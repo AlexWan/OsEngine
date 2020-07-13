c10fbb437c8ac64b6c3e59b6ac4dadc144bb84d5
Artem Atroshchenko <actpo4kun@gmail.com>
2020-07-14 01:08:43 +0300

    Cleaned code.

--- project/OsEngine/Market/Servers/FTX/EntityCreators/FTXPortfolioCreator.cs
+++ project/OsEngine/Market/Servers/FTX/EntityCreators/FTXPortfolioCreator.cs
@@ -18,22 +18,22 @@ namespace OsEngine.Market.Servers.FTX.EntityCreators
 
         public Portfolio CreatePortfolio(JToken jt, string portfolioName)
         {
-            var defaultPortfolio = new Portfolio() { Number = portfolioName };
+            var portfolio = new Portfolio() { Number = portfolioName };
 
             var result = jt.SelectToken(ResultPath);
             var collateral = result.SelectToken(CollateralPath).Value<decimal>();
             var freeCollateral = result.SelectToken(FreeCollateralPath).Value<decimal>();
             var positions = result.SelectTokens(PositionsPath).Children();
 
-            defaultPortfolio.ValueCurrent = result.SelectToken(TotalAccountValuePath).Value<decimal>();
-            defaultPortfolio.ValueBlocked = collateral - freeCollateral;
+            portfolio.ValueCurrent = result.SelectToken(TotalAccountValuePath).Value<decimal>();
+            portfolio.ValueBlocked = collateral - freeCollateral;
 
 
             foreach (var jtPosition in positions)
             {
                 PositionOnBoard pos = new PositionOnBoard();
 
-                pos.PortfolioName = defaultPortfolio.Number;
+                pos.PortfolioName = portfolio.Number;
                 pos.SecurityNameCode = jtPosition.SelectToken(NamePath).ToString();
                 var cost = jtPosition.SelectToken(CostPath).Value<decimal>();
                 var realizedPnl = jtPosition.SelectToken(PnlPricePath).Value<decimal>();
@@ -43,11 +43,11 @@ namespace OsEngine.Market.Servers.FTX.EntityCreators
 
                 if (pos.ValueCurrent > 0 || pos.ValueBlocked > 0)
                 {
-                    defaultPortfolio.SetNewPosition(pos);
+                    portfolio.SetNewPosition(pos);
                 }
             }
 
-            return defaultPortfolio;
+            return portfolio;
         }
 
         public Portfolio CreatePortfolio(string portfolioName)
