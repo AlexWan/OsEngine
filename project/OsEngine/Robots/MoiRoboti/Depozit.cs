09bd0836938c13dfc76ca17f7bb2e2ea4f1f1628
vv <4590955@list.ru>
2020-09-16 19:54:38 +0300

    навел лоск

--- project/OsEngine/Robots/MoiRoboti/Depozit.cs
+++ project/OsEngine/Robots/MoiRoboti/Depozit.cs
@@ -11,10 +11,9 @@ namespace OsEngine.Robots.MoiRoboti
     public class Depozit : BotPanel
     {
         // поля для хранения данных 
-
         public MovingAverage _ma; // поле хранения индикатора МА
         private BotTabSimple _vklad; // поле хранения вкладки робота 
-        private StrategyParameterInt slippage; // величина проскальзывание при установки ордеров  
+        private StrategyParameterDecimal slippage; // величина проскальзывание при установки ордеров  
         private StrategyParameterBool vkl_Robota; // поле включения бокса 
         private StrategyParameterInt zn_stop_los;  // через сколько позиций усреднения фиксировать убыток
         private StrategyParameterBool vkl_stop_los; // отключение функции стоплоса
@@ -32,10 +31,9 @@ namespace OsEngine.Robots.MoiRoboti
         private StrategyParameterDecimal volum;  //  объем входа 
         private StrategyParameterInt vel_ma; // какое значение индикатора махa использовать
         private StrategyParameterInt n_min; // количество минут для метода подсчета объема
-        private StrategyParameterBool vol_trade; // выключение подсчета объемов торгов
+        private StrategyParameterBool vkl_vol_trade; // выключение подсчета объемов торгов
 
         // глобальные переменные используемые в логике
-
         public decimal last_hi_candl; // значение хая последних свечей
         public decimal tek_bal_potfela; // текущий баланс портфеля квотируемой валюты
         public decimal Depo; // текущий баланс портфеля базовой 
@@ -48,73 +46,50 @@ namespace OsEngine.Robots.MoiRoboti
              : base(name, startProgram)
         {
             // инициализация переменных содержащих параметры стратегий 
-
-            vkl_Robota = CreateParameter("включение робота", false);
-            slippage = CreateParameter("проскальзывание при устан. ордеров", 5, 5, 50, 5);
-            zn_stop_los = CreateParameter("через сколько усред включить стоплос", 10, 5, 50, 5);
-            vkl_stop_los = CreateParameter("отключение функции стоплоса", true);
-            profit = CreateParameter("через сколько выставить профит", 10, 5, 50, 5);
-            vkl_profit = CreateParameter("отключение функции выставления профита", true);
-            komis_birgi = CreateParameter("сколько комиссии считать ", 0.2m, 1m, 1m, 1m);
-            veli4_usrednen = CreateParameter("во сколько увеличить усреднение ", 1m, 1m, 5m, 0.1m);
-            vkl_usrednen = CreateParameter("отключить  функцию усреднения", false);
-            dola_depa = CreateParameter("какой частью депозита входить", 10, 5, 100, 1);
-            deltaVerx = CreateParameter("через сколько пирамидиться ", 10m, 5m, 50m, 5m);
-            vkl_piramid = CreateParameter("отключение функции пирамиды", false);
-            deltaUsredn = CreateParameter(" через сколько усреднять ", 10m, 5m, 50m, 5m);
-            count_candels_hi = CreateParameter("сколько хаев свечей учитывать", 1, 1, 50, 1);
-            stop = CreateParameter("через сколько пунктов выставить стоп", 25, 5, 100, 5);
-            volum = CreateParameter("каким объемом заходить", 0.001m, 0.001m, 0.05m, 0.001m);
-            n_min = CreateParameter("сколько минут считать объем", 1, 1, 20, 1);
-            vol_trade = CreateParameter("считать ли объем торгов", true);
+            vkl_Robota = CreateParameter("РОБОТ Включен?", false);
+            slippage = CreateParameter("Велич. проскаль.у ордеров", 0.1m, 1m, 50m, 5m);
+            profit = CreateParameter("ТЭЙКПРОФИТ от рынка На ", 10, 5, 50, 5);
+            vkl_profit = CreateParameter("ТЕЙКПРОФИТ включен ЛИ ?", true);
+            veli4_usrednen = CreateParameter("ОБЪ.усред Уваел.НА ", 1m, 1m, 5m, 0.1m);
+            vkl_usrednen = CreateParameter("УСРЕДНЕНИЕ включено ЛИ?", true);
+            deltaUsredn = CreateParameter("УСРЕДнять через", 10m, 5m, 50m, 5m);
+            dola_depa = CreateParameter("Часть депозита на вход", 10, 5, 100, 1);
+            vkl_piramid = CreateParameter("ПИРАМИДА Включена ЛИ?", false);
+            deltaVerx = CreateParameter("ПИРАМИДИТЬСЯ через ", 10m, 5m, 50m, 5m);
+            vkl_stop_los = CreateParameter("Включен ЛИ СТОПЛОС ?", false);
+            zn_stop_los = CreateParameter("СТОП после скок УСРЕД ", 10, 5, 50, 5);
+            stop = CreateParameter("СТОПЛОС ниже на", 25, 5, 100, 5);
+            vel_ma = CreateParameter("MA", 7, 3, 50, 1);  // записываем в переменную параметры 
+            komis_birgi = CreateParameter("Биржа ЕСТ % учесть", 0.2m, 1m, 1m, 1m);
+            volum = CreateParameter("МИН объ.орд у биржи(базовой)", 0.001m, 0.001m, 0.05m, 0.001m);
+            vkl_vol_trade = CreateParameter("СЧИТАТЬ ЛИ объем торгов", true);
+            n_min = CreateParameter("скок минут считать объем", 1, 1, 20, 1);
+            count_candels_hi = CreateParameter("Скок.Хаев.св.читать(вход)", 2, 1, 50, 1);
 
             TabCreate(BotTabType.Simple);       // создание простой вкладки
             _vklad = TabsSimple[0]; // записываем первую вкладку в поле
 
             // создание и инициализация индикатора МА
-            vel_ma = CreateParameter("MA", 10, 5, 50, 5);  // записываем в переменную параметры 
             _ma = new MovingAverage(name + "Ma", false);
             _ma = (MovingAverage)_vklad.CreateCandleIndicator(_ma, "Prime");
             _ma.Lenght = vel_ma.ValueInt; // присвоение значения 
             _ma.Save();
 
-
             _vklad.CandleFinishedEvent += _vklad_CandleFinishedEvent; // подписались на событие завершения новой свечи
             _vklad.NewTickEvent += _vklad_NewTickEvent;     // тики
             _vklad.MarketDepthUpdateEvent += _vklad_MarketDepthUpdateEvent; // событие пришел стакан
             _vklad.PositionOpeningSuccesEvent += _vklad_PositionOpeningSuccesEvent; // событие успешного закрытия позиции 
-            _vklad.PositionNetVolumeChangeEvent += _vklad_PositionNetVolumeChangeEvent;
+            _vklad.PositionNetVolumeChangeEvent += _vklad_PositionNetVolumeChangeEvent; // изменился объем в позиции
 
         }
-
-        private void _vklad_PositionNetVolumeChangeEvent(Position position)
+        private void _vklad_PositionNetVolumeChangeEvent(Position position) // изменился объем в позиции
         {
             Price_kon_trade();
             Kol_Trad();    
         }
-
         private void _vklad_PositionOpeningSuccesEvent(Position position) // успешное закрытие позиции
         {
             _mnog = 1;
-        }
-        public decimal Price_kon_trade()  // получает значение цены последних трейдов, если их нет возвращает цену рынка 
-        {
-            if (_vklad.PositionsLast.MyTrades.Count != 0)
-            {
-                int asd = _vklad.PositionsLast.MyTrades.Count;
-                return _vklad.PositionsLast.MyTrades[asd - 1].Price;
-            }
-            else return _vklad.MarketDepth.Asks[0].Price;
-        }
-        void Save_prifit() // для выставления профита портфеля 
-        {
-            Percent_birgi(); // расчет величины в пунктах  процента биржи 
-            List<Position> positions = _vklad.PositionsOpenAll;
-
-            if (_vklad.MarketDepth.Bids[0].Price > _vklad.PositionsLast.EntryPrice + profit.ValueInt + _kom + slippage.ValueInt)
-            {
-                _vklad.CloseAtTrailingStop(positions[0], _vklad.MarketDepth.Bids[0].Price - _kom , _vklad.MarketDepth.Asks[0].Price - _kom);
-            }
         }
         private void _vklad_MarketDepthUpdateEvent(MarketDepth marketDepth) // логика работы запускается тут 
         {
@@ -125,7 +100,7 @@ namespace OsEngine.Robots.MoiRoboti
                     Save_prifit();
                 }
             }
-            if (vkl_Robota.ValueBool == false)
+            if (vkl_Robota.ValueBool == false) // выключаем работу робота
             {
                 return;
             }
@@ -136,12 +111,10 @@ namespace OsEngine.Robots.MoiRoboti
                     Usrednenie();
                 }
             }
-
             if (volum_ma > 0)
             {
                 if (_vklad.PositionsOpenAll.Count == 0) // если  не в рынке, открываемся по рынку 
                 {
-
                     if (volum_ma < _vklad.MarketDepth.Asks[0].Price) // если цена выше уровня машки
                     {
                         if (last_hi_candl < _vklad.MarketDepth.Asks[0].Price) // если цена выше последнего хая 
@@ -161,12 +134,9 @@ namespace OsEngine.Robots.MoiRoboti
         decimal bid_vol_tr;  // объем покупок
         decimal ask_vol_tr; // объем продаж
         decimal all_volum_trade_min; //все объемы за N минуту
-        public void Сount_volum()  // счетчик объема торгов по тикам 
-        {
-        }
         private void _vklad_NewTickEvent(Trade trade) // событие новых тиков для счета объема торгов
         {
-            if (vol_trade.ValueBool == false)
+            if (vkl_vol_trade.ValueBool == false)
             {
                 return;
             }
@@ -220,36 +190,23 @@ namespace OsEngine.Robots.MoiRoboti
                 }
             }
         }
-        decimal Okreglenie(decimal vol) // округляет децимал до 6 чисел после запятой 
+        void Save_prifit() // для выставления профита портфеля 
         {
-            decimal value = vol;
-            int N = 6;
-            decimal chah = decimal.Round(value, N, MidpointRounding.ToEven);
-            return chah;
-        }
-        int Kol_Trad() // вычисляет количество трейдов в позиции
-        {
-            int b = _vklad.PositionsLast.MyTrades.Count;
-            return b;
-        }
-        decimal Mnog() // используется для подсчета входов методом Piramida 
-        {
-            _mnog = _mnog + 1;
-            return _mnog;
-        }
-        int Dola_depa() // уменьшает значение  доли входа на количество осуществленных трейдов  
-        {
-            vol_dv = dola_depa.ValueInt;
-            int a = vol_dv - Kol_Trad();
-            if (a >= 1)
+            if (vkl_profit.ValueBool == false)
             {
-                return vol_dv = a;
+                return;
             }
-            return 1;
+            Percent_birgi(); // расчет величины в пунктах  процента биржи 
+            List<Position> positions = _vklad.PositionsOpenAll;
+
+            if (_vklad.MarketDepth.Bids[0].Price > _vklad.PositionsLast.EntryPrice + profit.ValueInt + _kom + slippage.ValueDecimal)
+            {
+                _vklad.CloseAtTrailingStop(positions[0], _vklad.MarketDepth.Bids[0].Price - profit.ValueInt, _vklad.MarketDepth.Asks[0].Price - profit.ValueInt - slippage.ValueDecimal);
+            }
         }
         void Usrednenie() // усреднение позиций при снижении рынка 
         {
-            if (vkl_usrednen.ValueBool == true)
+            if (vkl_usrednen.ValueBool == false)
             {
                 return;
             }
@@ -274,12 +231,50 @@ namespace OsEngine.Robots.MoiRoboti
                 }
             }
         }
+        decimal Okreglenie(decimal vol) // округляет децимал до 6 чисел после запятой 
+        {
+            decimal value = vol;
+            int N = 6;
+            decimal chah = decimal.Round(value, N, MidpointRounding.ToEven);
+            return chah;
+        }
+        public decimal Price_kon_trade()  // получает значение цены последних трейдов, если их нет возвращает цену рынка 
+        {
+            if (_vklad.PositionsLast.MyTrades.Count != 0)
+            {
+                int asd = _vklad.PositionsLast.MyTrades.Count;
+                return _vklad.PositionsLast.MyTrades[asd - 1].Price;
+            }
+            else return _vklad.MarketDepth.Asks[0].Price;
+        }
+        int Kol_Trad() // вычисляет количество трейдов в позиции
+        {
+            int b = _vklad.PositionsLast.MyTrades.Count;
+            return b;
+        }
+        decimal Mnog() // используется для подсчета входов методом Piramida 
+        {
+            _mnog = _mnog + 1;
+            return _mnog;
+        }
+        public void Сount_volum()  // счетчик объема торгов по тикам 
+        {
+        }
+        int Dola_depa() // уменьшает значение  доли входа на количество осуществленных трейдов  
+        {
+            vol_dv = dola_depa.ValueInt;
+            int a = vol_dv - Kol_Trad();
+            if (a >= 1)
+            {
+                return vol_dv = a;
+            }
+            return 1;
+        }
         decimal ZaprosBalahca()   // запрос квотируемых средств в портфеле (в USDT) 
         {
             List<PositionOnBoard> poses = _vklad.Portfolio.GetPositionOnBoard();
 
             decimal vol_usdt = 0;
-
             for (int i = 0; i < poses.Count; i++)
             {
                 if (poses[i].SecurityNameCode == "USDT")
