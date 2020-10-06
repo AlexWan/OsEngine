ae101d5be5c0159a69be490a8ceedb9057590a30
alex <alexrix777@gmail.com>
2020-10-06 11:04:12 +0300

    Implement Bot Attribute for applying bot to terminal

---
+++ project/OsEngine/OsTrader/Panels/Attributes/BotAttribute.cs
@@ -0,0 +1,17 @@
+﻿﻿namespace OsEngine.OsTrader.Panels.Attributes
+{
+    /// <summary>
+    /// Attribute for applying bot to terminal
+    /// Артрибут для добавления бота в терминал
+    /// </summary>
+    [System.AttributeUsage(System.AttributeTargets.Class)]
+    public class BotAttribute : System.Attribute
+    {
+        public string Name { get; }
+
+        public BotAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
