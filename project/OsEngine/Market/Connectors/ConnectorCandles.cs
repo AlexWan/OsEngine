1eb8ea5ef84543f45037befa7f54d3581a0816f4
Vlad Kredov <karuzzo@live.ru>
2021-03-04 00:28:46 +0300

    Minor fixies

--- project/OsEngine/Market/Connectors/ConnectorCandles.cs
+++ project/OsEngine/Market/Connectors/ConnectorCandles.cs
@@ -1101,7 +1101,7 @@ namespace OsEngine.Market.Connectors
             catch
             {
                 // it's hard to catch the error here. Who will understand what is wrong - well done 
-                // ошибка здесь была трудноуловимая. Кто понял что не так - молодец
+                // ошибка здесь трудноуловимая. Кто понял что не так - молодец
                 return;
             }
 
