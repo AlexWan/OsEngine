3e45e99ff8cf919a2d475d467b6e1f72d8eff652
vv <4590955@list.ru>
2021-07-22 17:44:33 +0300

    добавил ветвь для разработки

--- project/OsEngine/Robots/MoiRoboti/Breakdown.cs
+++ project/OsEngine/Robots/MoiRoboti/Breakdown.cs
@@ -12,6 +12,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using OsEngine.Indicators;
 using System.Drawing;
+using OsEngine.Logging;
 
 namespace OsEngine.Robots.MoiRoboti
 {
@@ -37,6 +38,16 @@ namespace OsEngine.Robots.MoiRoboti
             set => Set(ref _price, value);
         }
 
+        // настройки публичные
+        /// <summary>
+        /// объём для входа
+        /// </summary>
+        public decimal Volume = 1.1m; // тестовое присвоение значения
+        /// <summary>
+        /// проскальзывание на открытие первый ордер
+        /// </summary>
+        public int SlipageOpenFirst;
+
         /// <summary>
         /// вкладка робота для торговли
         /// </summary>
@@ -50,11 +61,10 @@ namespace OsEngine.Robots.MoiRoboti
             TabCreate(BotTabType.Simple);  // создание простой вкладки
             _tab = TabsSimple[0]; // записываем первую вкладку в поле
 
-            _tab.BestBidAskChangeEvent += _tab_BestBidAskChangeEvent; // для тестовой логики
+            _tab.MarketDepthUpdateEvent += _tab_MarketDepthUpdateEvent; // для тестовой логики
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
 
-            // создаем индикатор дампинга и машки для расчета его индекса 
-
+                                                             // создаем индикатор дампинга и машки для расчета его индекса 
             _dampIndex = new Line(name + "dampIndex", false)
             {
                 ColorBase = Color.DodgerBlue,
@@ -144,9 +154,55 @@ namespace OsEngine.Robots.MoiRoboti
             _smaLow4Period.Save();
         }
 
-        private void _tab_CandleFinishedEvent(List<Candle> candles) // тест
+        /// <summary>
+        ///  событие получения цены, использую для обновления переменных
+        /// </summary>
+        private void _tab_MarketDepthUpdateEvent(MarketDepth marketDepth)
         {
-            ReloadDampIndex(); // перегрузка 
+            Price = _tab.MarketDepth.Bids[0].Price; // обновляем значение цены
+            if (_index != null)
+            {
+                Index = ((Line)_dampIndex).Values[((Line)_dampIndex).Values.Count - 1]; // обновляем значение в индексе
+                ReloadDampIndex(); // перегрузка  
+            }
+        }
+
+        private void _tab_CandleFinishedEvent(List<Candle> candles) // событие создания свечи 
+        {
+            ReloadDampIndex(); // первая перегрузка индекса
+            TryOpenPosition(candles);
+            Strateg(candles);
+        }
+        /// <summary>
+        /// главный вход в логику робота. Вызывается когда на рынке заканчивается свеча
+        /// </summary>
+        void Strateg(List<Candle> candles)
+        {
+            ReloadDampIndex();
+
+            if (candles.Count < 14)
+            {
+                return;
+            }
+
+            List<Position> positions = _tab.PositionsOpenAll;
+
+            {
+                for (int i = 0; positions != null && i < positions.Count; i++)
+                {
+                    positions[i].StopOrderIsActiv = false;
+                    positions[i].ProfitOrderIsActiv = false;
+                }
+            }
+
+            if (positions == null || positions.Count == 0)
+            {
+                TryOpenPosition(candles);
+            }
+            else
+            {
+                TryClosePosition(positions[0], candles);
+            }
         }
 
         /// <summary>
@@ -225,17 +281,144 @@ namespace OsEngine.Robots.MoiRoboti
             }
             return result;
         }
+        /// <summary>
+        /// взять цену для стопПриказа
+        /// </summary>
+        /// <param name="side">сторона входа</param>
+        /// <param name="index">индекс свечи</param>
+        /// <returns>цена стопПриказа</returns>
+        private decimal GetPriseStop(Side side, int index)
+        {
+            List<Candle> myCandles = _tab.CandlesAll;
+
+            if (myCandles == null)
+            {
+                return 0;
+            }
+
+
+            if (side == Side.Buy)
+            { // стоп для лонга
+                decimal min = decimal.MaxValue;
+                for (int i = index; i > -1 && i > index; i--)
+                {
+                    if (myCandles[i].Low < min)
+                    {
+                        min = myCandles[i].Low;
+                    }
+                }
+                return min;// + _tab.Securiti.PriceStep;
+            }
+            else
+            { // стоп для шорта
+                decimal max = decimal.MinValue;
+                for (int i = index; i > -1 && i > index; i--)
+                {
+                    if (myCandles[i].High > max)
+                    {
+                        max = myCandles[i].High;
+                    }
+                }
+                return max;// - _tab.Securiti.PriceStep;
+            }
+        }
+
 
         /// <summary>
-        ///  событие обновления лучшей цены создано для обновления переменных
+        /// проверить условия на вход и войти в позицию
         /// </summary>
-        public void _tab_BestBidAskChangeEvent(decimal arg1, decimal arg2) 
+        private void TryOpenPosition(List<Candle> candles)
         {
-            Price = _tab.MarketDepth.Bids[0].Price; // обновляем значение цены
-            if (_index != null)
+            decimal damping;
+            decimal avgHigh2;
+            decimal avgLow3;
+            decimal avgLow2;
+            decimal avgHigh3;
+            decimal avgHighShifted;
+            decimal avgLowShifted;
+            try
             {
-               Index = ((Line)_dampIndex).Values[((Line)_dampIndex).Values.Count - 1]; // обновляем значение в индексе
-               ReloadDampIndex(); // перегрузка  
+                if (((MovingAverage)_smaHigh4Period).Values.Count - 1 - 10 < 0)
+                {
+                    return;
+                }
+                damping = ((Line)_dampIndex).Values[((Line)_dampIndex).Values.Count - 1];
+                avgHigh2 = ((MovingAverage)_smaHigh2Period).Values[((MovingAverage)_smaHigh2Period).Values.Count - 1]; // SMA.Series(High, 2);
+                avgLow3 = ((MovingAverage)_smaLow3Period).Values[((MovingAverage)_smaLow3Period).Values.Count - 1]; //SMA.Series(Low, 3);
+                avgLow2 = ((MovingAverage)_smaLow3Period).Values[((MovingAverage)_smaLow3Period).Values.Count - 1]; //SMA.Series(Low, 3);
+                avgHigh3 = ((MovingAverage)_smaHigh3Period).Values[((MovingAverage)_smaHigh3Period).Values.Count - 1];//SMA.Series(High, 3);
+                avgHighShifted = ((MovingAverage)_smaHigh4Period).Values[((MovingAverage)_smaHigh4Period).Values.Count - 1 - 10];//SMA.Series(High, 4) >> 10;
+                avgLowShifted = ((MovingAverage)_smaLow4Period).Values[((MovingAverage)_smaLow4Period).Values.Count - 1 - 10]; //SMA.Series(Low, 4) >> 10;
+            }
+            catch (Exception error)
+            {
+                _tab.SetNewLogMessage(error.ToString(), LogMessageType.Error);
+                return;
+            }
+ 
+            if (damping < 1 && (candles[candles.Count - 1].Close > avgHigh2) && (avgLow3 > avgHighShifted))
+            {
+                _tab.SetNewLogMessage("Системный вход в лонг. Время: " + candles[candles.Count - 1].TimeStart, LogMessageType.Signal);
+
+                if (StartProgram == StartProgram.IsTester)
+                {
+                    _tab.BuyAtMarket(Volume);
+                }
+                else
+                {
+                    _tab.BuyAtLimit(Volume, candles[candles.Count - 1].Close + _tab.Securiti.PriceStep * SlipageOpenFirst); // ЗДЕСЬ!!!!!!!!!!!!!!
+                }
+                return;
+            }
+            else if (damping < 1 && (candles[candles.Count - 1].Close < avgLow2) && (avgHigh3 < avgLowShifted))
+            {
+                if (StartProgram == StartProgram.IsTester)
+                {
+                    _tab.SellAtMarket(Volume);
+                }
+                else
+                {
+                    _tab.SellAtLimit(Volume, candles[candles.Count - 1].Close - _tab.Securiti.PriceStep * SlipageOpenFirst); // ЗДЕСЬ!!!!!!!!!!!!!!
+                }
+                return;
+            }
+            else if (damping <= 1)
+            {            }
+        }
+        /// <summary>
+        /// выставить стоп приказ по открытой позиции
+        /// </summary>
+        private void TryClosePosition(Position position, List<Candle> candles)
+        {
+            if (position.Direction == Side.Buy)
+            {
+                decimal priceEtalon = GetPriseStop(Side.Buy, candles.Count - 1);
+
+                decimal priceOrder = priceEtalon - _tab.Securiti.PriceStep; // ЗДЕСЬ!!!!!!!!!!!!!!
+                decimal priceRedLine = priceEtalon * _tab.Securiti.PriceStep;
+
+                if (priceRedLine - _tab.Securiti.PriceStep * 10 > _tab.PriceBestAsk)
+                {
+                    _tab.CloseAtLimit(position, _tab.PriceBestAsk, position.OpenVolume);
+                    return;
+                }
+
+            }
+
+            if (position.Direction == Side.Sell)
+            {
+                decimal priceEtalon = GetPriseStop(Side.Sell, candles.Count - 1);
+
+                decimal priceOrder = priceEtalon + _tab.Securiti.PriceStep; // ЗДЕСЬ!!!!!!!!!!!!!!
+                decimal priceRedLine = priceEtalon * _tab.Securiti.PriceStep;
+
+                if (priceRedLine + _tab.Securiti.PriceStep * 10 < _tab.PriceBestAsk)
+                {
+                    _tab.CloseAtLimit(position, _tab.PriceBestAsk, position.OpenVolume);
+                    return;
+                }
+
+                _tab.CloseAtStop(position, priceRedLine, priceOrder);
             }
         }
 
