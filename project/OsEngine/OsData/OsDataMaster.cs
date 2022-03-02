31a697bb1346b44eaa0973f9ebcc0f58dfcf4512
AlexWan <alexey@o-s-a.net>
2022-03-02 23:02:27 +0300

    vertical scroll bar in osData Source grid

--- project/OsEngine/OsData/OsDataMaster.cs
+++ project/OsEngine/OsData/OsDataMaster.cs
@@ -219,6 +219,7 @@ namespace OsEngine.OsData
         private void CreateSourceGrid()
         {
             DataGridView newGrid = DataGridFactory.GetDataGridView(DataGridViewSelectionMode.FullRowSelect, DataGridViewAutoSizeRowsMode.None);
+            newGrid.ScrollBars = ScrollBars.Vertical;
 
             DataGridViewTextBoxCell cell0 = new DataGridViewTextBoxCell();
             cell0.Style = newGrid.DefaultCellStyle;
