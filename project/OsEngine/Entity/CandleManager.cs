458637f0af5e3325ae575d9a86cae758deae6f2a
AlexWan <alexey@o-s-a.net>
2022-06-28 06:15:36 +0000

    candle manager delete procedure fix

--- project/OsEngine/Entity/CandleManager.cs
+++ project/OsEngine/Entity/CandleManager.cs
@@ -787,12 +787,25 @@ namespace OsEngine.Entity
                     return;
                 }
 
+                if(series == null)
+                {
+                    return;
+                }
+
                 series.СandleUpdeteEvent -= series_СandleUpdeteEvent;
                 series.СandleFinishedEvent -= series_СandleFinishedEvent;
 
                 for(int i = 0;i < _activSeries.Count;i++)
                 {
-                    if(_activSeries[i].UID == series.UID)
+                    CandleSeries curSeries = _activSeries[i];
+
+                    if(curSeries == null ||
+                        curSeries.UID == null)
+                    {
+                        return;
+                    }
+
+                    if (curSeries.UID == series.UID)
                     {
                         _activSeries.RemoveAt(i);
                         break;
