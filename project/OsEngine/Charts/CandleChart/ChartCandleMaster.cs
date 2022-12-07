021bdd766917e50cf2db8b438eb7748650a0d93f
AlexWan <alexey@o-s-a.net>
2022-12-07 12:42:48 +0000

    исправлено контекстное меню настройки индикаторов срабатывающее не с первого раза

--- project/OsEngine/Charts/CandleChart/ChartCandleMaster.cs
+++ project/OsEngine/Charts/CandleChart/ChartCandleMaster.cs
@@ -868,8 +868,6 @@ namespace OsEngine.Charts.CandleChart
                     }
                 }
 
-                LoadIndicatorOnChart(indicator);
-
                 if (_indicators == null)
                 {
                     _indicators = new List<IIndicator>();
@@ -879,6 +877,8 @@ namespace OsEngine.Charts.CandleChart
                 {
                     _indicators.Add(indicator);
                 }
+
+                LoadIndicatorOnChart(indicator);
 
                 indicator.NeadToReloadEvent += indicator_NeadToReloadEvent;
                 indicator_NeadToReloadEvent(indicator);
