cd3c8ed8489c0ff39691c34870a0a260a1637c7f
Skugar Denis <skugar.denis@mail.ru>
2023-04-18 08:43:28 +0000

    Время отправки timestamp увеличенно

--- project/OsEngine/Market/Servers/Bybit/Utilities/BybitWsRequestBuilder.cs
+++ project/OsEngine/Market/Servers/Bybit/Utilities/BybitWsRequestBuilder.cs
@@ -14,7 +14,7 @@ namespace OsEngine.Market.Servers.Bybit.Utilities
         public static string GetAuthRequest(Client client)
         {
             var now_UTC = TimeManager.GetExchangeTime("UTC");
-            var expires = Utils.GetMillisecondsFromEpochStart(now_UTC) + 1000;
+            var expires = Utils.GetMillisecondsFromEpochStart(now_UTC) + 2000;
             var signature = BybitSigner.CreateSignature(client, "GET/realtime" + expires.ToString());
             var sign = $"{{\"op\":\"auth\",\"args\":[\"{client.ApiKey}\",\"{expires}\", \"{signature}\"]}}";
             return sign;
