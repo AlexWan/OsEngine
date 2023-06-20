95db06f75267d4365fb34c7c560e4d03f2fbc419
unknown <alexey@o-s-a.net>
2023-06-20 14:05:04 +0300

    Бот для валидации Securities из сервера

--- project/OsEngine/Entity/Security.cs
+++ project/OsEngine/Entity/Security.cs
@@ -283,6 +283,12 @@ namespace OsEngine.Entity
     public enum SecurityStateType
     {
         /// <summary>
+        /// we don't know if the bidding's going on
+        /// неизвестно, идут ли торги
+        /// </summary>
+        UnKnown,
+
+        /// <summary>
         /// trading on the paper is active
         /// торги по бумаге активны
         /// </summary>
@@ -292,13 +298,7 @@ namespace OsEngine.Entity
         /// paper auction is closed.
         /// торги по бумаге закрыты
         /// </summary>
-        Close,
-
-        /// <summary>
-        /// we don't know if the bidding's going on
-        /// неизвестно, идут ли торги
-        /// </summary>
-        UnKnown
+        Close
     }
 
     /// <summary>
