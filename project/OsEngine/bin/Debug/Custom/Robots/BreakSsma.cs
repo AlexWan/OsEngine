1fa66788c4df89c8b2df994d7557557b9b08ab4c
JChinaM <137157160+JChinaM@users.noreply.github.com>
2023-07-08 13:37:48 +0300

    Update BreakSsma.cs

--- project/OsEngine/bin/Debug/Custom/Robots/BreakSsma.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/BreakSsma.cs
@@ -193,7 +193,7 @@ namespace OsEngine.Robots.MyRobots
                    
                     if (lastPrice > _lastSsma)
                     {
-                        _tab.CloseAtLimit(openPositions[0], lastPrice - _slippage, openPositions[0].OpenVolume);
+                        _tab.CloseAtLimit(openPositions[0], lastPrice + _slippage, openPositions[0].OpenVolume);
                     }
                 }
             }
