f1be06a2c9f106423717c58dfb1ba519afbca65d
unknown <alexey@o-s-a.net>
2023-07-27 10:53:54 +0300

    fix test layer

---
+++ project/OsEngine/Robots/AutoTestBots/ServerTests/Various_1_Securities.cs
@@ -0,0 +1,189 @@
+﻿using OsEngine.Entity;
+using OsEngine.Market;
+using System;
+using System.Collections.Generic;
+
+namespace OsEngine.Robots.AutoTestBots.ServerTests
+{
+    public class Various_1_Securities : AServerTester
+    {
+        public override void Process()
+        {
+            List<Security> securities = Server.Securities;
+
+            if (securities != null &&
+                securities.Count > 0)
+            {
+                CheckSecurities(securities, Server.ServerType);
+                SetNewServiceInfo("Securities count: " + securities.Count);
+            }
+            else
+            {
+                SetNewError("Security Error 0. No securities found");
+            }
+
+            TestEnded();
+        }
+
+        private void CheckSecurities(List<Security> securities, ServerType serverType)
+        {
+            for (int i = 0; i < securities.Count; i++)
+            {
+                if (securities[i] == null)
+                {
+                    SetNewError(serverType + " Security Error 1 . One of Securities in array is null!");
+                }
+
+                try
+                {
+                    Check(securities[i], serverType);
+                }
+                catch (Exception ex)
+                {
+                    SetNewError(serverType + " Security Error Unknown" + ex.ToString());
+                }
+            }
+        }
+
+        private void Check(Security security, ServerType serverType)
+        {
+            // Data Validation
+
+            /*
+            2.1.	НЕ обязательные поля
+            2.1.1.	Go – это гарантированное обеспечение для фьючерсной площадки МОЕКС. Не нужное
+            2.1.2.	OptionType – тип опциона. Не нужно нам пока
+            2.1.3.	Strike – тоже опционная тематика. Не нужно
+            2.1.4.	Expiration – опционы. Не нужно
+            2.1.5.	MinTradeAmount – Минимальный объём возможный для входа. Подаётся в очень малом кол-ве бирж. Можно игнорировать. Но если есть – надо добавлять.
+            2.1.6.	PriceLimitLow – минимальная цена для выставления ордера. Подаётся в очень малом кол-ве бирж.
+            2.1.7.	PriceLimitHigh – максимальная цена для выставления ордера. Подаётся в очень малом кол-ве бирж.
+
+
+            2.2.	Обязательные поля
+            2.2.1.	Name – имя бумаги повсеместно используемое в платформе
+            2.2.2.	NameFull – имя бумаги на случай если оно отличается от Name и имеет какие-то странные префиксы.Нужно на классических площадках и особенно на Американских.
+            2.2.3.	NameClass – Класс бумаги. Подробное описание есть ниже. С картинками
+            2.2.4.	NameId – Нужно для Международных рынков и некоторых типов брокеров РФ.Например Транзак и Тинькофф. В случае если по бирже нет 
+            2.2.5.	Exchange – Биржа по которой эта бумага торгуется
+            2.2.6.	State – Торговый статус бумаги.Надо вытаскивать из сообщения это.Очень важно. Если статус не Active – никуда не сохранять и не добавлять.
+
+            2.2.7.	Price Step – шаг цены инструмента.На классических площадках это обычно 1. На число с запятой, вида: 0.1 / 0.0001. 
+            2.2.8.	Lot – шаг объёма инструмента.На классических площадках это обычно 1. На крипте число с запятой, вида: 0.1 / 0.0001.
+            2.2.9.	PriceStepCost – цена шага цены инструмента. Актуально для фьючерсной секции Московской Биржи. Если на площадке такого понятия нет – то там должно быть указано Price Step.
+            2.2.10.	SecurityType.Тип инструмента
+            2.2.11.	Decimals  - количество знаков после запятой у цены инструмента
+            2.2.12.	DecimalsVolume – количество знаков после запятой у объёма инструмента
+
+            */
+
+            if (string.IsNullOrEmpty(security.Name))
+            {
+
+                SetNewError(serverType + " Security Error 2 . Name is Empty!");
+            }
+            if (string.IsNullOrEmpty(security.NameFull))
+            {
+
+                SetNewError(serverType + " Security Error 3 . Security Name is Empty!");
+            }
+            if (string.IsNullOrEmpty(security.NameClass))
+            {
+
+                SetNewError(serverType + " Security Error 4 . Security Name is Empty!");
+            }
+            if (string.IsNullOrEmpty(security.NameId))
+            {
+                SetNewError(serverType + " Security Error 5 . Security Name is Empty!");
+            }
+
+            if (string.IsNullOrEmpty(security.Exchange))
+            {
+                SetNewError(serverType + " Security Error 6 . Exchange is Empty!");
+            }
+            else
+            {
+                if (security.Exchange.Equals(serverType.ToString()) == false)
+                {
+                    SetNewError(serverType + " Security Error 7 . Exchange naming Error!");
+                }
+            }
+
+            if (string.IsNullOrEmpty(security.State.ToString()))
+            {
+                SetNewError(serverType + " Security Error 9. State is Empty!");
+            }
+            else
+            {
+                if (security.State == SecurityStateType.UnKnown)
+                {
+                    SetNewError(serverType + " Security Error 10. Sec State is UnKnown!");
+                }
+            }
+
+            if (security.SecurityType == SecurityType.None)
+            {
+                SetNewError(serverType + " Security Error 11. SecurityType is None ");
+            }
+
+            if (security.PriceStep <= 0)
+            {
+                SetNewError(serverType + " Security Error 12. PriceState is 0 ");
+            }
+            if (security.Lot <= 0)
+            {
+                SetNewError(serverType + " Security Error 13. Lot is 0 ");
+            }
+            if (security.PriceStepCost <= 0)
+            {
+                SetNewError(serverType + " Security Error 14. PriceStepCost is 0 ");
+            }
+
+            if (security.Decimals < 0)
+            {
+                SetNewError(serverType + " Security Error 15. Decimals is 0 ");
+            }
+
+            if (security.DecimalsVolume < 0)
+            {
+                SetNewError(serverType + " Security Error 16. DecimalsVolume is 0 ");
+            }
+
+            if (security.PriceStep != 0)
+            {
+                if (IsCompairDecimalsAndStep(security.Decimals, security.PriceStep) == false)
+                {
+                    SetNewError(serverType + " Security Error 17. PriceStep and Decimals in conflict ");
+                }
+            }
+
+            if (security.Lot != 0)
+            {
+                if (IsCompairDecimalsAndStep(security.DecimalsVolume, security.Lot) == false)
+                {
+                    SetNewError(serverType + " Security Error 18. Lot and DecimalsVolume in conflict ");
+                }
+            }
+
+        }
+
+        private bool IsCompairDecimalsAndStep(int decimals, decimal step)
+        {
+            int realDecimals = 0;
+
+            string stepInStr = step.ToStringWithNoEndZero().Replace(",", ".");
+
+            if (stepInStr.Split('.').Length > 1)
+            {
+                realDecimals = stepInStr.Split('.')[1].Length;
+
+                if (realDecimals != decimals)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ project/OsEngine/Robots/AutoTestBots/ServerTests/Various_2_MarketDepth.cs
@@ -0,0 +1,293 @@
+﻿using OsEngine.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OsEngine.Market.Servers.Hitbtc;
+
+namespace OsEngine.Robots.AutoTestBots.ServerTests
+{
+    public class Various_2_MarketDepth : AServerTester
+    {
+
+        public int CountSecuritiesToConnect;
+
+        public int MinutesToTest;
+
+        public override void Process()
+        {
+            List<Security> securities = Server.Securities;
+
+            if (securities != null &&
+                securities.Count > 0)
+            {
+                Server.NewMarketDepthEvent += Server_NewMarketDepthEvent;
+                int step = securities.Count / CountSecuritiesToConnect;
+
+                for(int i = 0;i < securities.Count;i++)
+                {
+                    Server.ServerRealization.Subscrible(securities[i]);
+                    _securities.Add(securities[i]);
+                    SetNewServiceInfo("Start sec: " + securities[i].Name);
+                    i += step - 1;
+                }
+            }
+            else
+            {
+                SetNewError("MD Error 0. No securities found");
+            }
+
+            DateTime timeEndTest = DateTime.Now.AddMinutes(MinutesToTest);
+
+            while (timeEndTest > DateTime.Now)
+            {
+                Thread.Sleep(1000);
+            }
+            Server.NewMarketDepthEvent -= Server_NewMarketDepthEvent;
+
+            SetNewServiceInfo("md сount analyzed: " + mdCount);
+            SetNewServiceInfo("test time minutes: " + MinutesToTest);
+
+            TestEnded();
+        }
+
+        List<Security> _securities = new List<Security>();
+
+        int mdCount = 0;
+
+        List<MarketDepth> _md = new List<MarketDepth>();
+
+        private void Server_NewMarketDepthEvent(MarketDepth md)
+        {
+            mdCount++;
+            /*  5.1.	Требования к данным MarketDepth
+5.1.1.	 Главный объект стакана котировок
+5.1.2.	Bid никогда не должен быть равен Ask
+5.1.3.	Bid не должен быть выше Ask
+5.1.4.	SecurityNameCode – обязательное поле. Не может быть равен null или содержать пустые строки. 
+5.1.5.	Time – поле содержащее в себе время обновления стакана. Каждый стакан должен быть маркирован временем. Либо своим, либо временем сервера
+5.1.6.	В истории стакана не должно быть больше 25 уровней
+5.1.7.	Стаканы не должны вызываться без смены времени стакана. Каждый должен быть уникальным.
+5.1.8.	Bids – 0 индекс самый высокий. И далее, чем больше индекс тем меньше цена
+5.1.9.	Asks  – 0 индекс самый низкий. И далее, чем больше индекс тем выше цена
+5.1.10.	С одинаковой ценой не может быть несколько уровней
+5.1.11.	Массивы не могут содержать в ячейках null
+
+
+
+5.2.MarketDepthLevel
+5.2.1.Объект одного уровня в стакане
+5.2.2.Price – обязательное поле цены
+5.2.3.Bid – назначается если это покупка
+5.2.4.Ask – назначается если это продажа
+5.2.5.Id – не обязательное сервисное поле. Применяется в некоторых коннекторах при сборке стакана.
+5.2.6. Находясь в массиве bids - Ask должен быть равен 0
+5.2.7. Находясь в массиве asks - Bid должен быть равен 0
+*/
+
+            // Базовые проверки
+            if (md.Bids == null ||
+                md.Asks == null)
+            {
+                SetNewError("MD Error 1. null in bids or asks array");
+                return;
+            }
+            if (md.Bids.Count == 0 ||
+                md.Asks.Count == 0)
+            {
+                SetNewError("MD Error 2. Zero count in bids or asks array");
+                return;
+            }
+
+            if (md.Bids.Count > 25 ||
+                md.Asks.Count > 25)
+            {
+                SetNewError("MD Error 3. Count in bids or asks more 25 lines");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(md.SecurityNameCode))
+            {
+                SetNewError("MD Error 4. Security name is null or empty");
+                return;
+            }
+
+            for (int i = 0; i < md.Bids.Count; i++)
+            {
+                if (md.Bids[i] == null)
+                {
+                    SetNewError("MD Error 5. Bids array have null level");
+                    return;
+                }
+                if (md.Bids[i].Ask != 0)
+                {
+                    SetNewError("MD Error 6. Ask in bids array is note zero");
+                    return;
+                }
+                if (md.Bids[i].Bid == 0)
+                {
+                    SetNewError("MD Error 7. Bid in bids array is zero");
+                    return;
+                }
+            }
+
+            for (int i = 0; i < md.Asks.Count; i++)
+            {
+                if (md.Asks[i] == null)
+                {
+                    SetNewError("MD Error 8. Asks array have null level");
+                    return;
+                }
+                if (md.Asks[i].Bid != 0)
+                {
+                    SetNewError("MD Error 9. Bid in asks array is note zero");
+                    return;
+                }
+                if (md.Asks[i].Ask == 0)
+                {
+                    SetNewError("MD Error 10. Ask in asks array is zero");
+                    return;
+                }
+            }
+
+            // проверка времени
+
+            if (md.Time == DateTime.MinValue)
+            {
+                SetNewError("MD Error 11. Time is min value");
+            }
+
+            MarketDepth oldDepth = null;
+
+            for (int i = 0; i < _md.Count; i++)
+            {
+                if (_md[i].SecurityNameCode == md.SecurityNameCode)
+                {
+                    oldDepth = _md[i];
+                }
+            }
+
+            if (oldDepth != null && oldDepth.Time == md.Time)
+            {
+                SetNewError("MD Error 12. Time in md is note change");
+            }
+
+            bool isSaved = false;
+
+            for (int i = 0; i < _md.Count; i++)
+            {
+                if (_md[i].SecurityNameCode == md.SecurityNameCode)
+                {
+                    _md[i] = md;
+                    isSaved = true;
+                    break;
+                }
+            }
+
+            if (isSaved == false)
+            {
+                _md.Add(md);
+            }
+
+            for (int i = 1; i < md.Bids.Count; i++)
+            {
+                if (md.Bids[i].Price == 0)
+                {
+                    SetNewError("MD Error 13. Bibs[i] price == 0");
+                    return;
+                }
+            }
+
+            for (int i = 1; i < md.Asks.Count; i++)
+            {
+                if (md.Asks[i].Price == 0)
+                {
+                    SetNewError("MD Error 14. Asks[i] price == 0");
+                    return;
+                }
+            }
+
+            // проверка массивов Bids и Asks на запутанность
+
+            if (md.Bids[0].Price >= md.Asks[0].Price)
+            {
+                SetNewError("MD Error 15. Bib price >= Ask price");
+                return;
+            }
+
+            for (int i = 1; i < md.Bids.Count; i++)
+            {
+                // Bids – уровни заявок на покупку.
+                // 0 индекс самый высокий.И далее, чем больше индекс тем меньше цена
+
+                if (md.Bids[i].Price == md.Bids[i - 1].Price)
+                {
+                    SetNewError("MD Error 16. Bibs[i] price == Bibs[i-1] price");
+                }
+
+                if (md.Bids[i].Price > md.Bids[i - 1].Price)
+                {
+                    SetNewError("MD Error 17. Bibs[i] price > Bibs[i-1] price");
+                }
+            }
+
+            for (int i = 1; i < md.Asks.Count; i++)
+            {
+                // Asks – уровни заявок на продажу.
+                // 0 индекс самый низкий.И далее, чем больше индекс тем выше цена
+
+                if (md.Asks[i].Price == md.Asks[i - 1].Price)
+                {
+                    SetNewError("MD Error 18. Asks[i] price == Asks[i-1] price");
+                }
+
+                if (md.Asks[i].Price < md.Asks[i - 1].Price)
+                {
+                    SetNewError("MD Error 19. Asks[i] price < Asks[i-1] price");
+                }
+            }
+
+            for (int i = 0; i < md.Bids.Count; i++)
+            {
+                // 5.1.10.	С одинаковой ценой не может быть несколько уровней
+
+                MarketDepthLevel curLevel = md.Bids[i];
+
+                for (int j = 0; j < md.Bids.Count; j++)
+                {
+                    if(j == i)
+                    {
+                        continue;
+                    }
+
+                    if (curLevel.Price == md.Bids[j].Price)
+                    {
+                        SetNewError("MD Error 20. bids with same price");
+                    }
+                }
+            }
+
+            for (int i = 0; i < md.Asks.Count; i++)
+            {
+                // 5.1.10.	С одинаковой ценой не может быть несколько уровней
+
+                MarketDepthLevel curLevel = md.Asks[i];
+
+                for (int j = 0; j < md.Asks.Count; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    if (curLevel.Price == md.Asks[j].Price)
+                    {
+                        SetNewError("MD Error 21. Asks with same price");
+                    }
+                }
+            }
+        }
+    }
+}
