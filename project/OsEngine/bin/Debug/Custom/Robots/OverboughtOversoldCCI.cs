69244cba75c1aae60d1e9a705a897a260e09535e
jchinam <jchinam@mail.ru>
2023-07-29 08:30:57 +0300

    new robot OverboughtOversoldCCI

---
+++ project/OsEngine/bin/Debug/Custom/Robots/OverboughtOversoldCCI.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using OsEngine.Charts.CandleChart.Indicators;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System.Linq;
+using OsEngine.Logging;
+using System.Runtime.CompilerServices;
+using OsEngine.Market.Servers.Tinkoff.TinkoffJsonSchema;
+
+/* Description
+trading robot for osengine
+
+The trend robot on Overbought Oversold CCI.
+
+Buy: CCI indicator values ​​below a certain value.
+
+Sell: CCI indicator values ​​are above a certain value.
+
+Exit: on the opposite signal.
+
+ */
+
+
+namespace OsEngine.Robots.AO
+{
+    [Bot("OverboughtOversoldCCI")] // We create an attribute so that we don't write anything to the BotFactory
+    public class OverboughtOversoldCCI : BotPanel
+    {
+        private BotTabSimple _tab;
+
+        // Basic Settings
+        private StrategyParameterString Regime;
+        private StrategyParameterString VolumeRegime;
+        private StrategyParameterDecimal VolumeOnPosition;
+        private StrategyParameterDecimal Slippage;
+        private StrategyParameterTimeOfDay StartTradeTime;
+        private StrategyParameterTimeOfDay EndTradeTime;
+
+        // Indicator setting 
+        private StrategyParameterDecimal BuyValue;
+        private StrategyParameterDecimal SellValue;
+        private StrategyParameterInt LengthCCI;
+
+        // Indicator
+        Aindicator _CCI;
+
+        // The last value of the indicator
+        private decimal _lastCCI;
+
+        public OverboughtOversoldCCI(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+            // Basic setting
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
+            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
+            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+            // Indicator setting
+            LengthCCI = CreateParameter("CCI Length", 21, 7, 48, 7, "Indicator");
+            BuyValue = CreateParameter("Buy Value", 10.0m, 10, 300, 10, "Indicator");
+            SellValue = CreateParameter("Sell Value", 10.0m, 10, 300, 10, "Indicator");
+
+            // Create indicator CCI
+            _CCI = IndicatorsFactory.CreateIndicatorByName("CCI", name + "CCI", false);
+            _CCI = (Aindicator)_tab.CreateCandleIndicator(_CCI, "NewArea");
+            ((IndicatorParameterInt)_CCI.Parameters[0]).ValueInt = LengthCCI.ValueInt;
+            _CCI.Save();
+
+            // Subscribe to the indicator update event
+            ParametrsChangeByUser += OverboughtOversoldCCI_ParametrsChangeByUser; ;
+
+            // Subscribe to the candle finished event
+            _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+            Description = "The trend robot on Overbought Oversold CCI. " +
+                "Buy: CCI indicator values ​​below a certain value. " +
+                "Sell: CCI indicator values ​​are above a certain value. " +
+                "Exit: on the opposite signal.";
+        }
+
+        private void OverboughtOversoldCCI_ParametrsChangeByUser()
+        {
+            ((IndicatorParameterInt)_CCI.Parameters[0]).ValueInt = LengthCCI.ValueInt;
+            _CCI.Save();
+            _CCI.Reload();
+        }
+
+        // The name of the robot in OsEngine
+        public override string GetNameStrategyType()
+        {
+            return "OverboughtOversoldCCI";
+        }
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+        // Candle Finished Event
+        private void _tab_CandleFinishedEvent(List<Candle> candles)
+        {
+            // If the robot is turned off, exit the event handler
+            if (Regime.ValueString == "Off")
+            {
+                return;
+            }
+
+            // If there are not enough candles to build an indicator, we exit
+            if (candles.Count < LengthCCI.ValueInt)
+            {
+                return;
+            }
+
+            // If the time does not match, we leave
+            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
+                EndTradeTime.Value < _tab.TimeServerCurrent)
+            {
+                return;
+            }
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            // If there are positions, then go to the position closing method
+            if (openPositions != null && openPositions.Count != 0)
+            {
+                LogicClosePosition(candles);
+            }
+
+            // If the position closing mode, then exit the method
+            if (Regime.ValueString == "OnlyClosePosition")
+            {
+                return;
+            }
+            // If there are no positions, then go to the position opening method
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                LogicOpenPosition(candles);
+            }
+        }
+
+        // Opening logic
+        private void LogicOpenPosition(List<Candle> candles)
+        {
+            // The last value of the indicator
+            _lastCCI = _CCI.DataSeries[0].Last;
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                decimal lastPrice = candles[candles.Count - 1].Close;
+
+                // Slippage
+                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+                // Long
+                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                {
+                    if (_lastCCI < -BuyValue.ValueDecimal)
+                    {
+                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                    }
+                }
+
+                // Short
+                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                {
+                    if (_lastCCI > SellValue.ValueDecimal)
+                    {
+                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                    }
+                }
+            }
+        }
+
+        // Logic close position
+        private void LogicClosePosition(List<Candle> candles)
+        {
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            // The last value of the indicator
+            _lastCCI = _CCI.DataSeries[0].Last;
+
+            decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+            decimal lastPrice = candles[candles.Count - 1].Close;
+
+            for (int i = 0; openPositions != null && i < openPositions.Count; i++)
+            {
+                Position positions = openPositions[i];
+
+                if (positions.State != PositionStateType.Open)
+                {
+                    continue;
+                }
+
+                if (openPositions[i].Direction == Side.Buy) // If the direction of the position is purchase
+                {
+                    if (_lastCCI > SellValue.ValueDecimal)
+                    {
+                        _tab.CloseAtLimit(openPositions[0], lastPrice - _slippage, openPositions[0].OpenVolume);
+                    }
+                }
+                else // If the direction of the position is sale
+                {
+                    if (_lastCCI < -BuyValue.ValueDecimal)
+                    {
+                        _tab.CloseAtLimit(openPositions[0], lastPrice + _slippage, openPositions[0].OpenVolume);
+                    }
+                }
+            }
+        }
+
+        // Method for calculating the volume of entry into a position
+        private decimal GetVolume()
+        {
+            decimal volume = 0;
+
+            if (VolumeRegime.ValueString == "Contract currency")
+            {
+                decimal contractPrice = _tab.PriceBestAsk;
+                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+            }
+            else if (VolumeRegime.ValueString == "Number of contracts")
+            {
+                volume = VolumeOnPosition.ValueDecimal;
+            }
+
+            // If the robot is running in the tester
+            if (StartProgram == StartProgram.IsTester)
+            {
+                volume = Math.Round(volume, 6);
+            }
+            else
+            {
+                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
+            }
+            return volume;
+        }
+    }
+}
+
