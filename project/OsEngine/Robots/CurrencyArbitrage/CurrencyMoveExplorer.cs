6a7c9455d83e9834118f1765ae18437fea0c40b1
unknown <alexey@o-s-a.net>
2023-10-06 05:18:43 +0000

    add currency arbitrage explorer bot

---
+++ project/OsEngine/Robots/CurrencyArbitrage/CurrencyMoveExplorer.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System;
+using System.Threading;
+
+namespace OsEngine.Robots.CurrencyArbitrage
+{
+    [Bot("CurrencyMoveExplorer")]
+    public class CurrencyMoveExplorer : BotPanel
+    {
+        public CurrencyMoveExplorer(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Polygon);
+            _tabPolygon = this.TabsPolygon[0];
+            _tabPolygon.ProfitGreaterThanSignalValueEvent += _tabPolygon_ProfitGreaterThanSignalValueEvent;
+
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On" });
+
+            Thread worker = new Thread(ThreadWorkerArea);
+            worker.Start();
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return "CurrencyMoveExplorer";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+        private BotTabPolygon _tabPolygon;
+
+        public StrategyParameterString Regime;
+
+        private void _tabPolygon_ProfitGreaterThanSignalValueEvent(decimal profit, PolygonToTrade sequence)
+        {
+            if (Regime.ValueString == "Off")
+            {
+                return;
+            }
+
+            if(_polygonToWatch != null)
+            {
+                return;
+            }
+
+            _lastTimePolygonStartWatch = DateTime.Now;
+
+            _polygonToWatch = sequence;
+
+            DateTime time = DateTime.Now;
+
+            string message = "Start watch " + _polygonToWatch.SecuritiesInSequence + "\n";
+            message += time.ToLongTimeString() + "." + time.Millisecond + "\n";
+            message += "Start profit  " + profit.ToString();
+
+            SendNewLogMessage(message, Logging.LogMessageType.System);
+
+            
+        }
+
+        PolygonToTrade _polygonToWatch;
+
+        DateTime _lastTimePolygonStartWatch;
+
+        private void ThreadWorkerArea()
+        {
+            while(true)
+            {
+                Thread.Sleep(200);
+
+                if(_polygonToWatch == null)
+                {
+                    continue;
+                }
+
+                if(_lastTimePolygonStartWatch.AddSeconds(3) < DateTime.Now)
+                {
+                    SendNewLogMessage("End watch " + _polygonToWatch.SecuritiesInSequence, Logging.LogMessageType.System);
+                    _polygonToWatch = null;
+                    continue;
+                }
+
+                DateTime time = DateTime.Now;
+
+                string message = time.ToLongTimeString() + "." + time.Millisecond;
+                message += "Profit: " + _polygonToWatch.ProfitToDealPercent.ToString();
+
+                SendNewLogMessage(message, Logging.LogMessageType.System);
+
+            }
+
+
+        }
+    }
+}
