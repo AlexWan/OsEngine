0599d9dfef659c750a2f0a6ecd3d34b95be3329b
JchinaM <jchinam@mail.ru>
2023-12-08 07:26:17 +0300

    new robot

---
+++ project/OsEngine/bin/Debug/Custom/Robots/BreakMomentum.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using OsEngine.Charts.CandleChart.Indicators;
+using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System.Linq;
+using OsEngine.Logging;
+using System.Windows.Media.Animation;
+using System.Security.Cryptography;
+
+/* Description
+trading robot for osengine
+
+The trend robot on Break Momentum.
+
+Buy: The Momentum indicator value crosses the 100 level from bottom to top and has been growing for two candles.
+
+Sell: The Momentum indicator value crosses the 100 level from top to bottom and has been falling for two candles.
+
+Exit: stop and profit in % of the entry price.
+ */
+
+
+namespace OsEngine.Robots.AO
+{
+    [Bot("BreakMomentum")] // We create an attribute so that we don't write anything to the BotFactory
+    public class BreakMomentum : BotPanel
+    {
+        private BotTabSimple _tab;
+
+        // Basic Settings
+        private StrategyParameterString Regime;
+        private StrategyParameterString VolumeRegime;
+        private StrategyParameterDecimal VolumeOnPosition;
+        private StrategyParameterDecimal Slippage;
+        private StrategyParameterTimeOfDay StartTradeTime;
+        private StrategyParameterTimeOfDay EndTradeTime;
+
+        // Indicator setting 
+        private StrategyParameterInt _MomentumPeriod;
+
+        // Indicator
+        Aindicator _Momentum;
+
+        // The last value of the indicator
+        private decimal _lastMomentum;
+
+        // Exit
+        private StrategyParameterDecimal StopValue;
+        private StrategyParameterDecimal ProfitValue;
+
+        public BreakMomentum(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+            // Basic setting
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
+            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
+            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+            // Indicator setting
+            _MomentumPeriod = CreateParameter("Momentum Period", 16, 10, 300, 7, "Indicator");
+
+            // Create indicator MACD
+            _Momentum = IndicatorsFactory.CreateIndicatorByName("Momentum", name + "Momentum", false);
+            _Momentum = (Aindicator)_tab.CreateCandleIndicator(_Momentum, "NewArea");
+            ((IndicatorParameterInt)_Momentum.Parameters[0]).ValueInt = _MomentumPeriod.ValueInt;
+            _Momentum.Save();
+            
+            // Exit
+            StopValue = CreateParameter("Stop Value", 1.0m, 5, 200, 5, "Exit");
+            ProfitValue = CreateParameter("Profit Value", 1.0m, 5, 200, 5, "Exit");
+
+            // Subscribe to the indicator update event
+            ParametrsChangeByUser += BreakMomentum_ParametrsChangeByUser; ;
+
+            // Subscribe to the candle finished event
+            _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+            Description = "The trend robot on Break Momentum. " +
+                "Buy: The Momentum indicator value crosses the 100 level from bottom to top and has been growing for two candles. " +
+                "Sell: The Momentum indicator value crosses the 100 level from top to bottom and has been falling for two candles. " +
+                "Exit: stop and profit in % of the entry price.";
+        }
+
+        private void BreakMomentum_ParametrsChangeByUser()
+        {
+            ((IndicatorParameterInt)_Momentum.Parameters[0]).ValueInt = _MomentumPeriod.ValueInt;
+            _Momentum.Save();
+            _Momentum.Reload();
+        }
+
+        // The name of the robot in OsEngine
+        public override string GetNameStrategyType()
+        {
+            return "BreakMomentum";
+        }
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+        // Candle Finished Event
+        private void _tab_CandleFinishedEvent(List<Candle> candles)
+        {
+            // If the robot is turned off, exit the event handler
+            if (Regime.ValueString == "Off")
+            {
+                return;
+            }
+
+            // If there are not enough candles to build an indicator, we exit
+            if (candles.Count < _MomentumPeriod.ValueInt)
+            {
+                return;
+            }
+
+            // If the time does not match, we leave
+            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
+                EndTradeTime.Value < _tab.TimeServerCurrent)
+            {
+                return;
+            }
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            // If there are positions, then go to the position closing method
+            if (openPositions != null && openPositions.Count != 0)
+            {
+                LogicClosePosition(candles);
+            }
+
+            // If the position closing mode, then exit the method
+            if (Regime.ValueString == "OnlyClosePosition")
+            {
+                return;
+            }
+            // If there are no positions, then go to the position opening method
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                LogicOpenPosition(candles);
+            }
+        }
+
+        // Opening logic
+        private void LogicOpenPosition(List<Candle> candles)
+        {
+            // The last value of the indicator
+            _lastMomentum = _Momentum.DataSeries[0].Last;
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                decimal lastPrice = candles[candles.Count - 1].Close;
+
+                // Slippage
+                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+                // Long
+                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                {
+                    if (_lastMomentum > 100)
+                    {
+                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                    }
+                }
+
+                // Short
+                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                {
+                    if (_lastMomentum < 100)
+                    {
+                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                    }
+                }
+            }
+        }
+
+        // Logic close position
+        private void LogicClosePosition(List<Candle> candles)
+        {
+            List<Position> openPositions = _tab.PositionsOpenAll;
+            Position pos = openPositions[0];
+
+            decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+            decimal lastPrice = candles[candles.Count - 1].Close;
+
+            for (int i = 0; openPositions != null && i < openPositions.Count; i++)
+            {
+                Position positions = openPositions[i];
+
+                if (positions.State != PositionStateType.Open)
+                {
+                    continue;
+                }
+
+                if (openPositions[i].Direction == Side.Buy) // If the direction of the position is purchase
+                {
+                    decimal profitActivation = pos.EntryPrice + pos.EntryPrice * ProfitValue.ValueDecimal / 100;
+                    decimal stopActivation = pos.EntryPrice - pos.EntryPrice * StopValue.ValueDecimal / 100;
+
+                    _tab.CloseAtProfit(pos, profitActivation, profitActivation + _slippage);
+                    _tab.CloseAtStop(pos, stopActivation, stopActivation - _slippage);
+                }
+                else // If the direction of the position is sale
+                {
+                    decimal profitActivation = pos.EntryPrice - pos.EntryPrice * ProfitValue.ValueDecimal / 100;
+                    decimal stopActivation = pos.EntryPrice + pos.EntryPrice * StopValue.ValueDecimal / 100;
+
+                    _tab.CloseAtProfit(pos, profitActivation, profitActivation - _slippage);
+                    _tab.CloseAtStop(pos, stopActivation, stopActivation + _slippage);
+                }
+
+            }
+        }
+
+        // Method for calculating the volume of entry into a position
+        private decimal GetVolume()
+        {
+            decimal volume = 0;
+
+            if (VolumeRegime.ValueString == "Contract currency")
+            {
+                decimal contractPrice = _tab.PriceBestAsk;
+                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+            }
+            else if (VolumeRegime.ValueString == "Number of contracts")
+            {
+                volume = VolumeOnPosition.ValueDecimal;
+            }
+
+            // If the robot is running in the tester
+            if (StartProgram == StartProgram.IsTester)
+            {
+                volume = Math.Round(volume, 6);
+            }
+            else
+            {
+                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
+            }
+            return volume;
+        }
+    }
+}
