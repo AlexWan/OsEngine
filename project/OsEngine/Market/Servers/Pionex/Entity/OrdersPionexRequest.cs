ccbd9dae69b81f00c29c6f80c78859e6a51e7cf5
Sergey Tsiunel <tsiunels@yandex.ru>
2024-03-06 01:20:55 +0500

    Обновил файлы коннектора Pionex

---
+++ project/OsEngine/Market/Servers/Pionex/Entity/OrdersPionexRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Market.Servers.Pionex.Entity
+{
+    public class SendNewOrder
+    {
+        public string symbol;
+        public string side;
+        public string type;
+        public string clientOrderId;
+        public string size;            // Quantity. Required in limit order and market sell order.
+        public string price;           // Required in limit order.
+        public string amount;          // Buying amount. Required in market buy order.
+        public bool IOC;               // Immediate or Cancel (IOC) Order
+    }
+}
---
+++ project/OsEngine/Market/Servers/Pionex/Entity/ResponseWebSocketMessage.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+
+namespace OsEngine.Market.Servers.Pionex.Entity
+{
+    public class ResponseWebSocketMessage<T>
+    {
+        public string result;
+        public string topic;
+        public string symbol;
+        public string type;
+        public string timestamp;
+        public T data;
+    }
+
+    public class ResponseWebSocketTrades
+    {
+        public TradeElements[] trades;
+    }
+
+    public class TradeElements
+    {
+        public string symbol;
+        public string tradeId;
+        public string price;
+        public string size;
+        public string side;
+        public string timestamp;
+    }
+
+    public class ResponseWebSocketDepthItem
+    {
+        public List<List<string>> asks;
+        public List<List<string>> bids;
+    }
+
+    public class ResponceWSBalance
+    {
+        public BalanceWS[] balances;
+    }
+
+    public class BalanceWS
+    {
+        public string coin;
+        public string free;    // Available balance, 8 decimal digits.
+        public string frozen;  // Frozen balance, 8 decimal digits.
+    }
+
+    public class MyTrades
+    {
+        public string id;
+        public string orderId;
+        public string symbol;
+        public string side;
+        public string role;
+        public string price;
+        public string size;
+        public string fee;
+        public string feeCoin;
+        public string timestamp;
+    }
+    public class MyOrders
+    {
+        public string orderId;
+        public string symbol; 
+        public string type;         // LIMIT / MARKET.
+        public string side;
+        public string price;
+        public string size;         // Order quantity.
+        public string filledSize;   // Filled quantity of order.
+        public string filledAmount; // Filled amount of order.
+        public string fee;          // Transaction fee.
+        public string feeCoin;      // Currency of transaction fee.
+        public string IOC;
+        public string status;       // OPEN / CLOSED.
+        public string clientOrderId;
+        public string createTime;
+        public string updateTime;
+    }
+
+
+}
