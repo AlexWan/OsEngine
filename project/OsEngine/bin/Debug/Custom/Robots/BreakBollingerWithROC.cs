37bf5446763c9848f41082f9b908ce2080b745a5
JchinaM <jchinam@mail.ru>
2024-03-16 14:21:14 +0300

    new robot

---
+++ project/OsEngine/bin/Debug/Custom/Robots/BreakBollingerWithROC.cs
@@ -0,0 +1,268 @@
+﻿using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System;
+using System.Collections.Generic;
+
+/* Description
+trading robot for osengine
+
+The trend robot on Break Bollinger With ROC.
+
+Buy:
+1. The candle closed above the upper Bollinger line.
+2. RoC is above 0.
+Sell:
+1. The candle closed below the lower Bollinger line.
+2. RoC is below 0.
+Exit from buy: trailing stop in % of the loy of the candle on which you entered.
+Exit from sell: trailing stop in% of the high of the candle on which you entered.
+
+ */
+
+
+namespace OsEngine.Robots.AO
+{
+    [Bot("BreakBollingerWithROC")] // We create an attribute so that we don't write anything to the BotFactory
+    public class BreakBollingerWithROC : BotPanel
+    {
+        private BotTabSimple _tab;
+
+        // Basic Settings
+        private StrategyParameterString Regime;
+        private StrategyParameterString VolumeRegime;
+        private StrategyParameterDecimal VolumeOnPosition;
+        private StrategyParameterDecimal Slippage;
+        private StrategyParameterTimeOfDay StartTradeTime;
+        private StrategyParameterTimeOfDay EndTradeTime;
+
+        // Indicator setting 
+        private StrategyParameterInt BollingerLength;
+        private StrategyParameterDecimal BollingerDeviation;
+        private StrategyParameterInt LengthROC;
+
+        // Indicator
+        Aindicator _Bollinger;
+        Aindicator _ROC;
+
+        // The last value of the indicator
+        private decimal _lastUpLine;
+        private decimal _lastDownLine;
+        private decimal _lastROC;
+
+        // Exit
+        private StrategyParameterDecimal TrailingValue;
+
+        public BreakBollingerWithROC(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+            // Basic setting
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
+            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
+            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+            // Indicator setting
+            BollingerLength = CreateParameter("Bollinger Length", 21, 7, 48, 7, "Indicator");
+            BollingerDeviation = CreateParameter("Bollinger Deviation", 1.0m, 1, 5, 0.1m, "Indicator");
+            LengthROC = CreateParameter("ROC Length", 14, 7, 48, 7, "Indicator");
+
+            // Create indicator Bollinger
+            _Bollinger = IndicatorsFactory.CreateIndicatorByName("Bollinger", name + "Bollinger", false);
+            _Bollinger = (Aindicator)_tab.CreateCandleIndicator(_Bollinger, "Prime");
+            ((IndicatorParameterInt)_Bollinger.Parameters[0]).ValueInt = BollingerLength.ValueInt;
+            ((IndicatorParameterDecimal)_Bollinger.Parameters[1]).ValueDecimal = BollingerDeviation.ValueDecimal;
+            _Bollinger.Save();
+
+            // Create indicator RSI
+            _ROC = IndicatorsFactory.CreateIndicatorByName("ROC", name + "ROC", false);
+            _ROC = (Aindicator)_tab.CreateCandleIndicator(_ROC, "NewArea");
+            ((IndicatorParameterInt)_ROC.Parameters[0]).ValueInt = LengthROC.ValueInt;
+            _ROC.Save();
+
+            // Exit
+            TrailingValue = CreateParameter("Stop Value", 1.0m, 5, 200, 5, "Exit");
+
+            // Subscribe to the indicator update event
+            ParametrsChangeByUser += BreakBollingerWithROC_ParametrsChangeByUser; ;
+
+            // Subscribe to the candle finished event
+            _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+            Description = "The trend robot on Break Bollinger With ROC. " +
+                "Buy: " +
+                "1. The candle closed above the upper Bollinger line. " +
+                "2. RoC is above 0. " +
+                "Sell: " +
+                "1. The candle closed below the lower Bollinger line. " +
+                "2. RoC is below 0. " +
+                "Exit from buy: trailing stop in % of the loy of the candle on which you entered. " +
+                "Exit from sell: trailing stop in% of the high of the candle on which you entered.";
+        }
+
+        private void BreakBollingerWithROC_ParametrsChangeByUser()
+        {
+            ((IndicatorParameterInt)_Bollinger.Parameters[0]).ValueInt = BollingerLength.ValueInt;
+            ((IndicatorParameterDecimal)_Bollinger.Parameters[1]).ValueDecimal = BollingerDeviation.ValueDecimal;
+            _Bollinger.Save();
+            _Bollinger.Reload();
+            ((IndicatorParameterInt)_ROC.Parameters[0]).ValueInt = LengthROC.ValueInt;
+            _ROC.Save();
+            _ROC.Reload();
+        }
+
+        // The name of the robot in OsEngine
+        public override string GetNameStrategyType()
+        {
+            return "BreakBollingerWithROC";
+        }
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+        // Candle Finished Event
+        private void _tab_CandleFinishedEvent(List<Candle> candles)
+        {
+            // If the robot is turned off, exit the event handler
+            if (Regime.ValueString == "Off")
+            {
+                return;
+            }
+
+            // If there are not enough candles to build an indicator, we exit
+            if (candles.Count < LengthROC.ValueInt ||
+                candles.Count < BollingerLength.ValueInt)
+            {
+                return;
+            }
+
+            // If the time does not match, we leave
+            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
+                EndTradeTime.Value < _tab.TimeServerCurrent)
+            {
+                return;
+            }
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            // If there are positions, then go to the position closing method
+            if (openPositions != null && openPositions.Count != 0)
+            {
+                LogicClosePosition(candles);
+            }
+
+            // If the position closing mode, then exit the method
+            if (Regime.ValueString == "OnlyClosePosition")
+            {
+                return;
+            }
+            // If there are no positions, then go to the position opening method
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                LogicOpenPosition(candles);
+            }
+        }
+
+        // Opening logic
+        private void LogicOpenPosition(List<Candle> candles)
+        {
+            // The last value of the indicator
+            _lastUpLine = _Bollinger.DataSeries[0].Last;
+            _lastDownLine = _Bollinger.DataSeries[1].Last;
+            _lastROC = _ROC.DataSeries[0].Last;
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                decimal lastPrice = candles[candles.Count - 1].Close;
+
+                // Slippage
+                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+                // Long
+                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                {
+                    if (lastPrice > _lastUpLine && _lastROC > 0)
+                    {
+                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                    }
+                }
+
+                // Short
+                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                {
+                    if (lastPrice < _lastDownLine && _lastROC < 0)
+                    {
+                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                    }
+                }
+            }
+        }
+
+        // Logic close position
+        private void LogicClosePosition(List<Candle> candles)
+        {
+            List<Position> openPositions = _tab.PositionsOpenAll;
+            Position pos = openPositions[0];
+
+            decimal stopPrice;
+
+            for (int i = 0; openPositions != null && i < openPositions.Count; i++)
+            {
+                Position positions = openPositions[i];
+
+                if (positions.State != PositionStateType.Open)
+                {
+                    continue;
+                }
+
+                if (openPositions[i].Direction == Side.Buy) // If the direction of the position is purchase
+                {
+                    decimal lov = candles[candles.Count - 1].Low;
+                    stopPrice = lov - lov * TrailingValue.ValueDecimal / 100;
+                }
+                else // If the direction of the position is sale
+                {
+                    decimal high = candles[candles.Count - 1].High;
+                    stopPrice = high + high * TrailingValue.ValueDecimal / 100;
+                }
+                _tab.CloseAtTrailingStop(pos, stopPrice, stopPrice);
+            }
+        }
+
+        // Method for calculating the volume of entry into a position
+        private decimal GetVolume()
+        {
+            decimal volume = 0;
+
+            if (VolumeRegime.ValueString == "Contract currency")
+            {
+                decimal contractPrice = _tab.PriceBestAsk;
+                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+            }
+            else if (VolumeRegime.ValueString == "Number of contracts")
+            {
+                volume = VolumeOnPosition.ValueDecimal;
+            }
+
+            // If the robot is running in the tester
+            if (StartProgram == StartProgram.IsTester)
+            {
+                volume = Math.Round(volume, 6);
+            }
+            else
+            {
+                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
+            }
+            return volume;
+        }
+    }
+}
