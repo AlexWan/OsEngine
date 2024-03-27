02ed0c3fffbcbb198452894f84f81056731cf468
JchinaM <jchinam@mail.ru>
2024-03-27 10:57:00 +0300

    new robot

---
+++ project/OsEngine/bin/Debug/Custom/Robots/BreakPriceChannelWithVolume.cs
@@ -0,0 +1,403 @@
+﻿using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using QuikSharp;
+using System;
+using System.Collections.Generic;
+
+/* Description
+trading robot for osengine
+
+The trend robot on Break PriceChannel With Volume.
+
+Buy:
+1. The candle closed above the upper PC line
+2. The volume is above the average volume for the period (the number of candles back) by a factor of several.
+Sell:
+1. The candle closed below the lower PC line.
+2. The volume is above the average volume for the period (the number of candles back) by a factor of several.
+
+Exit from buy: Stop and profit.
+The stop is placed at the minimum for the period specified for the stop (StopCandles). 
+Profit is equal to the size of the stop * CoefProfit (CoefProfit – how many times the size of the profit is greater than the size of the stop).
+
+Exit from sell: Stop and profit.
+The stop is placed at the maximum for the period specified for the stop (StopCandles). 
+Profit is equal to the size of the stop * CoefProfit (CoefProfit – how many times the size of the profit is greater than the size of the stop).
+
+ */
+
+
+namespace OsEngine.Robots.AO
+{
+    [Bot("BreakPriceChannelWithVolume")] // We create an attribute so that we don't write anything to the BotFactory
+    public class BreakPriceChannelWithVolume : BotPanel
+    {
+        private BotTabSimple _tab;
+
+        // Basic Settings
+        private StrategyParameterString Regime;
+        private StrategyParameterString VolumeRegime;
+        private StrategyParameterDecimal VolumeOnPosition;
+        private StrategyParameterDecimal Slippage;
+        private StrategyParameterTimeOfDay StartTradeTime;
+        private StrategyParameterTimeOfDay EndTradeTime;
+
+        // Indicator setting 
+        private StrategyParameterDecimal MultVolume;
+        private StrategyParameterInt CandlesCountVolume;
+        private StrategyParameterInt PcUpLength;
+        private StrategyParameterInt PcDownLength;
+
+        // Indicator
+        Aindicator _Volume;
+        Aindicator _PC;
+
+        // The last value of the indicator
+        private decimal _lastVolume;
+
+        // The prev value of the indicator
+        private decimal _prevUpPC;
+        private decimal _prevDownPC;
+
+        // Exit
+        private StrategyParameterDecimal CoefProfit;
+        private StrategyParameterInt StopCandles;
+
+        public BreakPriceChannelWithVolume(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+            // Basic setting
+            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
+            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
+            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+            // Indicator setting
+            CandlesCountVolume = CreateParameter("CandlesCountVolume", 13, 10, 300, 1, "Indicator");
+            MultVolume = CreateParameter("MultVolume", 10.0m, 10, 300, 10, "Indicator");
+            PcUpLength = CreateParameter("Up Line Length", 21, 7, 48, 7, "Indicator");
+            PcDownLength = CreateParameter("Down Line Length", 21, 7, 48, 7, "Indicator");
+
+            // Create indicator PC
+            _PC = IndicatorsFactory.CreateIndicatorByName("PriceChannel", name + "PC", false);
+            _PC = (Aindicator)_tab.CreateCandleIndicator(_PC, "Prime");
+            ((IndicatorParameterInt)_PC.Parameters[0]).ValueInt = PcUpLength.ValueInt;
+            ((IndicatorParameterInt)_PC.Parameters[1]).ValueInt = PcDownLength.ValueInt;
+            _PC.Save();
+
+            // Create indicator Volume
+            _Volume = IndicatorsFactory.CreateIndicatorByName("Volume", name + "Volume", false);
+            _Volume = (Aindicator)_tab.CreateCandleIndicator(_Volume, "NewArea");
+            _Volume.Save();
+
+            // Exit
+            CoefProfit = CreateParameter("Coef Profit", 1, 1m, 10, 1, "Exit settings");
+            StopCandles = CreateParameter("Stop Candles", 1, 2, 10, 1, "Exit settings");
+
+            // Subscribe to the indicator update event
+            ParametrsChangeByUser += BreakPriceChannelWithVolume_ParametrsChangeByUser; ;
+
+            // Subscribe to the candle finished event
+            _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
+
+            Description = "The trend robot on Break PriceChannel With Volume. " +
+                "Buy: " +
+                "1. The candle closed above the upper PC line " +
+                "2. The volume is above the average volume for the period (the number of candles back) by a factor of several. " +
+                "Sell: " +
+                "1. The candle closed below the lower PC line. " +
+                "2. The volume is above the average volume for the period (the number of candles back) by a factor of several. " +
+                "Exit from buy: Stop and profit. " +
+                "The stop is placed at the minimum for the period specified for the stop (StopCandles).  " +
+                "Profit is equal to the size of the stop * CoefProfit (CoefProfit – how many times the size of the profit is greater than the size of the stop). " +
+                "Exit from sell: Stop and profit. " +
+                "The stop is placed at the maximum for the period specified for the stop (StopCandles).  " +
+                "Profit is equal to the size of the stop * CoefProfit (CoefProfit – how many times the size of the profit is greater than the size of the stop).";
+        }
+
+        private void BreakPriceChannelWithVolume_ParametrsChangeByUser()
+        {
+            ((IndicatorParameterInt)_PC.Parameters[0]).ValueInt = PcUpLength.ValueInt;
+            ((IndicatorParameterInt)_PC.Parameters[1]).ValueInt = PcDownLength.ValueInt;
+            _PC.Save();
+            _PC.Reload();
+        }
+
+        // The name of the robot in OsEngine
+        public override string GetNameStrategyType()
+        {
+            return "BreakPriceChannelWithVolume";
+        }
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+        // Candle Finished Event
+        private void _tab_CandleFinishedEvent(List<Candle> candles)
+        {
+            // If the robot is turned off, exit the event handler
+            if (Regime.ValueString == "Off")
+            {
+                return;
+            }
+
+            // If there are not enough candles to build an indicator, we exit
+            if (candles.Count < CandlesCountVolume.ValueInt ||
+                candles.Count < PcUpLength.ValueInt ||
+                candles.Count < PcDownLength.ValueInt)
+            {
+                return;
+            }
+
+            // If the time does not match, we leave
+            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
+                EndTradeTime.Value < _tab.TimeServerCurrent)
+            {
+                return;
+            }
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            // If there are positions, then go to the position closing method
+            if (openPositions != null && openPositions.Count != 0)
+            {
+                LogicClosePosition(candles);
+            }
+
+            // If the position closing mode, then exit the method
+            if (Regime.ValueString == "OnlyClosePosition")
+            {
+                return;
+            }
+            // If there are no positions, then go to the position opening method
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                LogicOpenPosition(candles);
+            }
+        }
+
+        // Opening logic
+        private void LogicOpenPosition(List<Candle> candles)
+        {
+            // The last value of the indicator
+            _lastVolume = _Volume.DataSeries[0].Last;
+
+            // The prev value of the indicator
+            _prevUpPC = _PC.DataSeries[0].Values[_PC.DataSeries[0].Values.Count - 2];
+            _prevDownPC = _PC.DataSeries[1].Values[_PC.DataSeries[1].Values.Count - 2];
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+
+            if (openPositions == null || openPositions.Count == 0)
+            {
+                decimal lastPrice = candles[candles.Count - 1].Close;
+
+                List<decimal> VolumeValues = _Volume.DataSeries[0].Values;
+
+                // Slippage
+                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+                // Long
+                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                {
+                    if (GetAverage(VolumeValues, CandlesCountVolume.ValueInt) * MultVolume.ValueDecimal < _lastVolume && lastPrice > _prevUpPC)
+                    {
+                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                    }
+                }
+
+                // Short
+                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                {
+                    if ((GetAverage(VolumeValues, CandlesCountVolume.ValueInt) * MultVolume.ValueDecimal) < _lastVolume && lastPrice < _prevDownPC)
+                    {
+                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                    }
+                }
+            }
+        }
+
+        //  Logic close position
+        private void LogicClosePosition(List<Candle> candles)
+        {
+
+            List<Position> openPositions = _tab.PositionsOpenAll;
+            Position pos = openPositions[0];
+
+            decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+
+            decimal profitActivation;
+            decimal price;
+
+            for (int i = 0; openPositions != null && i < openPositions.Count; i++)
+            {
+                if (openPositions[i].State != PositionStateType.Open)
+                {
+                    continue;
+                }
+
+                if (openPositions[i].Direction == Side.Buy) // If the direction of the position is purchase
+                {
+
+                    decimal stopActivation = GetPriceStop(openPositions[i].TimeCreate, Side.Buy, candles, candles.Count - 1);
+
+                    if (stopActivation == 0)
+                    {
+                        return;
+                    }
+
+                    price = stopActivation;
+                    profitActivation = pos.EntryPrice + (pos.EntryPrice - price) * CoefProfit.ValueDecimal;
+                    _tab.CloseAtProfit(pos, profitActivation, profitActivation + _slippage);
+
+                    _tab.CloseAtStop(pos, stopActivation, stopActivation - _slippage);
+                }
+                else // If the direction of the position is sale
+                {
+                    decimal stopActivation = GetPriceStop(openPositions[i].TimeCreate, Side.Sell, candles, candles.Count - 1);
+
+                    if (stopActivation == 0)
+                    {
+                        return;
+                    }
+
+                    price = stopActivation;
+                    profitActivation = pos.EntryPrice - (price - pos.EntryPrice) * CoefProfit.ValueDecimal;
+                    _tab.CloseAtProfit(pos, profitActivation, profitActivation - _slippage);
+
+
+                    _tab.CloseAtStop(pos, stopActivation, stopActivation + _slippage);
+                }
+
+            }
+
+        }
+
+        private decimal GetAverage(List<decimal> Volume, int period)
+        {
+            decimal sum = 0;
+
+            for (int i = 2; i < period; i++)
+            {
+                sum += Volume[Volume.Count - i];
+            }
+
+            return sum / period;
+        }
+
+        private decimal GetPriceStop(DateTime positionCreateTime, Side side, List<Candle> candles, int index)
+        {
+            if (candles == null || index < StopCandles.ValueInt)
+            {
+                return 0;
+            }
+
+            if (side == Side.Buy)
+            {
+                // We calculate the stop price at Long
+                // We find the minimum for the time from the opening of the transaction to the current one
+                decimal price = decimal.MaxValue; ;
+                int indexIntro = 0;
+                DateTime openPositionTime = positionCreateTime;
+
+                if (openPositionTime == DateTime.MinValue)
+                {
+                    openPositionTime = candles[index - 2].TimeStart;
+                }
+
+                for (int i = index; i > 0; i--)
+                {
+                    // Look at the index of the candle, after which the opening of the pose occurred
+                    if (candles[i].TimeStart <= openPositionTime)
+                    {
+                        indexIntro = i;
+                        break;
+                    }
+                }
+
+                for (int i = indexIntro; i > 0 && i > indexIntro - StopCandles.ValueInt; i--)
+                {
+                    // Looking at the minimum after opening
+                    if (candles[i].Low < price)
+                    {
+                        price = candles[i].Low;
+                    }
+                }
+
+                return price;
+            }
+
+            if (side == Side.Sell)
+            {
+                //  We find the maximum for the time from the opening of the transaction to the current one
+                decimal price = 0;
+                int indexIntro = 0;
+                DateTime openPositionTime = positionCreateTime;
+
+                if (openPositionTime == DateTime.MinValue)
+                {
+                    openPositionTime = candles[index - 1].TimeStart;
+                }
+
+                for (int i = index; i > 0; i--)
+                {
+                    // Look at the index of the candle, after which the opening of the pose occurred
+                    if (candles[i].TimeStart <= openPositionTime)
+                    {
+                        indexIntro = i;
+                        break;
+                    }
+                }
+
+                for (int i = indexIntro; i > 0 && i > indexIntro - StopCandles.ValueInt; i--)
+                {
+                    // Looking at the maximum high
+                    if (candles[i].High > price)
+                    {
+                        price = candles[i].High;
+                    }
+                }
+
+                return price;
+            }
+
+            return 0;
+        }
+
+        // Method for calculating the volume of entry into a position
+        private decimal GetVolume()
+        {
+            decimal volume = 0;
+
+            if (VolumeRegime.ValueString == "Contract currency")
+            {
+                decimal contractPrice = _tab.PriceBestAsk;
+                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+            }
+            else if (VolumeRegime.ValueString == "Number of contracts")
+            {
+                volume = VolumeOnPosition.ValueDecimal;
+            }
+
+            // If the robot is running in the tester
+            if (StartProgram == StartProgram.IsTester)
+            {
+                volume = Math.Round(volume, 6);
+            }
+            else
+            {
+                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
+            }
+            return volume;
+        }
+    }
+}
+
