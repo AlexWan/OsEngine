5bdcd0156a8d3a679ccaecbe23542cf5bd8cf56d
unknown <alexey@o-s-a.net>
2024-03-29 20:38:09 +0300

    update version

---
+++ project/OsEngine/Robots/AutoTestBots/ServerTests/Orders_6_ChangePriceError.cs
@@ -0,0 +1,28 @@
+﻿using OsEngine.Market.Servers;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using OsEngine.Entity;
+using OsEngine.Market;
+
+namespace OsEngine.Robots.AutoTestBots.ServerTests
+{
+    public class Orders_6_ChangePriceError : AServerTester
+    {
+        public string SecurityNameToTrade = "ETHUSDT";
+
+        public string SecurityClassToTrade = "Futures";
+
+        public decimal VolumeToTrade;
+
+        public string PortfolioName;
+
+        public int CountOrders;
+
+        public override void Process()
+        {
+            TestEnded();
+            return;
+        }
+    }
+}
