d1fde1967469bd4996e47e00d45162faa12bc6ef
Sergey Knyazev <knyazev@bk.ru>
2024-08-11 15:10:18 +0500

    upd

---
+++ project/OsEngine/Robots/HomeWork/Test1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using OsEngine.Entity;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.OsTrader.Panels.Attributes;
+using System.Threading;
+
+namespace OsEngine.Robots.HomeWork
+{
+    [Bot("Test1")]
+    public class Test1 : BotPanel
+    {
+        private BotTabSimple _tab;
+       
+
+        public Test1(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Simple);
+            _tab = TabsSimple[0];
+
+           
+
+
+            StartThread();
+        }
+
+       
+
+        private void StartThread()
+        {
+            Thread worker = new Thread(StartPaintChart) { IsBackground = true };
+            worker.Start();
+        }
+
+        private void StartPaintChart()
+        {
+            
+        }
+
+        
+
+        public override string GetNameStrategyType()
+        {
+            return "Test1";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+
+        }
+
+    }
+}
+
