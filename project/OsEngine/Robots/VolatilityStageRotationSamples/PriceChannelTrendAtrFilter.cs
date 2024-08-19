0d00a4e177164cb5668b84065066d92bf70ae1dc
unknown <alexey@o-s-a.net>
2024-08-19 17:21:54 +0100

    Volatility Stages Samples. Init commit

---
+++ project/OsEngine/Robots/VolatilityStageRotationSamples/PriceChannelTrendAtrFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Robots.VolatilityStageRotationSamples
+{
+    public class PriceChannelTrendAtrFilter
+    {
+
+
+
+
+
+    }
+}
