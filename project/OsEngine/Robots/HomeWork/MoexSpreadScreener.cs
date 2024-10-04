9a8ed9e75a324c9799f63853a44b13ab91632073
Sergey Knyazev <knyazev@bk.ru>
2024-10-04 15:08:16 +0500

    upd

---
+++ project/OsEngine/Robots/HomeWork/MoexSpreadScreener.cs
@@ -0,0 +1,371 @@
+﻿using OsEngine;
+using OsEngine.Entity;
+using OsEngine.Market.Connectors;
+using OsEngine.OsTrader.Panels;
+using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Robots.HomeWork;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace OsEngine.Robots.HomeWork
+{
+    [Bot("MoexSpreadScreener")]
+    public class MoexSpreadScreener : BotPanel
+    {
+        private BotTabScreener _tab;
+        private StrategyParameterString _regime;
+        private List<string> _listSecurities;
+
+        public MoexSpreadScreener(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            TabCreate(BotTabType.Screener);
+            _tab = TabsScreener[0];
+
+            _regime = CreateParameter("Regime", "Off", new string[] { "Off", "On" });
+
+            this.ParamGuiSettings.Title = "TableBot Parameters";
+            this.ParamGuiSettings.Height = 300;
+            this.ParamGuiSettings.Width = 600;
+
+            CustomTabToParametersUi customTab = ParamGuiSettings.CreateCustomTab("Table Parameters");
+
+            CreateTable();
+            customTab.AddChildren(_host);
+            //LoadTable();
+            CreateButtonAddRow();
+
+            _tab.CandleUpdateEvent += _tab_CandleUpdateEvent;
+
+        }
+
+        private void _tab_CandleUpdateEvent(List<Candle> candles, BotTabSimple tab)
+        {
+            SetSpreadInTable(candles, tab);
+            
+        }
+
+        private void SetSpreadInTable(List<Candle> candles, BotTabSimple tab)
+        {
+            if (MainWindow.GetDispatcher.CheckAccess() == false)
+            {
+                MainWindow.GetDispatcher.Invoke((Action<List<Candle>, BotTabSimple>)SetSpreadInTable, candles, tab);
+                return;
+            }
+
+            if (_grid == null)
+            {
+                return;
+            }
+
+            if (_grid.Rows.Count <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < _grid.Rows.Count - 1; i++)
+            {
+                if (_grid.Rows[i].Cells[0].Value == null ||
+                    _grid.Rows[i].Cells[1].Value == null)
+                {
+                    continue;
+                }
+
+                if (_grid.Rows[i].Cells[0].Value.ToString() == tab.Securiti.NameFull)
+                {
+                    _grid.Rows[i].Cells[2].Value = candles[candles.Count - 1].Close.ToString();
+                }
+                if (_grid.Rows[i].Cells[1].Value.ToString() == tab.Securiti.NameFull)
+                {
+                    _grid.Rows[i].Cells[3].Value = candles[candles.Count - 1].Close.ToString();
+                }
+
+                if (_grid.Rows[i].Cells[2].Value != null &&
+                    _grid.Rows[i].Cells[3].Value != null)
+                {
+                    _grid.Rows[i].Cells[4].Value = GetSpread(Convert.ToDecimal(_grid.Rows[i].Cells[2].Value), Convert.ToDecimal(_grid.Rows[i].Cells[3].Value));                   
+                }
+            }
+
+        }
+
+        private object GetSpread(decimal price1, decimal price2)
+        {
+            return price1 / price2;
+        }
+
+        public override string GetNameStrategyType()
+        {
+            return "MoexSpreadScreener";
+        }
+
+        public override void ShowIndividualSettingsDialog()
+        {
+        }
+
+        private WindowsFormsHost _host;
+
+        private DataGridView _grid;
+
+        private void CreateTable()
+        {
+            if (MainWindow.GetDispatcher.CheckAccess() == false)
+            {
+                MainWindow.GetDispatcher.Invoke(new Action(CreateTable));
+                return;
+            }
+
+            _host = new WindowsFormsHost();
+
+            DataGridView newGrid =
+                DataGridFactory.GetDataGridView(DataGridViewSelectionMode.FullRowSelect,
+                DataGridViewAutoSizeRowsMode.AllCells);
+
+            //newGrid.CellValidating += NewGrid_CellValidating;
+            newGrid.CellClick += NewGrid_CellClick;
+
+            newGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+            DataGridViewTextBoxCell cell0 = new DataGridViewTextBoxCell();
+            cell0.Style = newGrid.DefaultCellStyle;
+
+            DataGridViewComboBoxColumn colum0 = new DataGridViewComboBoxColumn();
+            //colum0.CellTemplate = cell0;
+            colum0.HeaderText = "First Security";
+            colum0.ReadOnly = false;
+            colum0.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            newGrid.Columns.Add(colum0);
+
+            DataGridViewComboBoxColumn colum01 = new DataGridViewComboBoxColumn();
+            //colum01.CellTemplate = cell0;
+            colum01.HeaderText = "Second Security";
+            colum01.ReadOnly = false;
+            colum01.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            newGrid.Columns.Add(colum01);
+
+            DataGridViewColumn colum02 = new DataGridViewColumn();
+            colum02.CellTemplate = cell0;
+            colum02.HeaderText = "Price First Security";
+            colum02.ReadOnly = false;
+            colum02.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            newGrid.Columns.Add(colum02);
+
+            DataGridViewColumn colum03 = new DataGridViewColumn();
+            colum03.CellTemplate = cell0;
+            colum03.HeaderText = "Price Second Security";
+            colum03.ReadOnly = false;
+            colum03.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            newGrid.Columns.Add(colum03);
+
+            DataGridViewColumn colum04 = new DataGridViewColumn();
+            colum04.CellTemplate = cell0;
+            colum04.HeaderText = "Spread";
+            colum04.ReadOnly = false;
+            colum04.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            newGrid.Columns.Add(colum04);
+
+            DataGridViewButtonColumn colum05 = new DataGridViewButtonColumn();
+            colum05.ReadOnly = true;
+            colum05.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            colum05.UseColumnTextForButtonValue = true;
+            colum05.Text = "Delete row";
+            newGrid.Columns.Add(colum05);
+
+            _host.Child = newGrid;
+            _grid = newGrid;
+        }
+
+        private void CreateButtonAddRow()
+        {
+            DataGridViewRow newRow = new DataGridViewRow();
+
+            DataGridViewButtonCell buttonCell = new DataGridViewButtonCell();
+            buttonCell.Value = "Add row";
+
+            newRow.Cells.Add(buttonCell);
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+
+            _grid.Rows.Add(newRow);
+
+            _grid.Rows[_grid.Rows.Count - 1].Cells[1].ReadOnly = true;
+            _grid.Rows[_grid.Rows.Count - 1].Cells[2].ReadOnly = true;
+            _grid.Rows[_grid.Rows.Count - 1].Cells[3].ReadOnly = true;
+            _grid.Rows[_grid.Rows.Count - 1].Cells[4].ReadOnly = true;
+
+        }
+
+        private void NewGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 5 && e.RowIndex != _grid.Rows.Count - 1)
+            {
+                _grid.Rows.RemoveAt(e.RowIndex);
+                //SaveTable();
+            }
+
+            if (e.ColumnIndex == 0 && e.RowIndex == _grid.Rows.Count - 1)
+            {
+                SetNewRowsSpread();                
+            }
+
+            if (e.ColumnIndex == 0 && e.RowIndex != _grid.Rows.Count - 1)
+            {
+                GetLastPriceSecurityInTable(e.ColumnIndex, e.RowIndex);
+            }
+            if (e.ColumnIndex == 1 && e.RowIndex != _grid.Rows.Count - 1)
+            {
+                GetLastPriceSecurityInTable(e.ColumnIndex, e.RowIndex);
+            }
+
+        }
+
+        private void GetLastPriceSecurityInTable(int columnIndex, int rowIndex)
+        {            
+            if (_grid[columnIndex, rowIndex].Value == null)
+            {
+                return;
+            }
+
+            string securuty = _grid[columnIndex, rowIndex].Value.ToString();
+
+            for (int i = 0; i < _tab.Tabs.Count; i++)
+            {
+                if (_tab.Tabs[i].Securiti.NameFull == securuty)
+                {
+                    if (columnIndex == 0)
+                    {
+                        _grid[2, rowIndex].Value = _tab.Tabs[i].CandlesAll[_tab.Tabs[i].CandlesAll.Count - 1].Close;
+                    }
+                    if (columnIndex == 1)
+                    {
+                        _grid[3, rowIndex].Value = _tab.Tabs[i].CandlesAll[_tab.Tabs[i].CandlesAll.Count - 1].Close;
+                    }
+                }
+            }
+            
+        }
+                
+        private void SetNewRowsSpread()
+        {
+            _listSecurities = GetListSecuruties();
+
+            DataGridViewRow newRow = new DataGridViewRow();
+
+            newRow.Cells.Add(new DataGridViewComboBoxCell()
+            { DataSource = _listSecurities }
+            );
+
+            newRow.Cells.Add(new DataGridViewComboBoxCell()
+            { DataSource = _listSecurities }
+            );
+
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+            newRow.Cells.Add(new DataGridViewTextBoxCell());
+
+            newRow.Cells[2].ReadOnly = true;
+            newRow.Cells[3].ReadOnly = true;
+            newRow.Cells[4].ReadOnly = true;
+
+            _grid.Rows.Insert(_grid.Rows.Count - 1, newRow);
+        }
+
+        private void NewGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            //SaveTable();
+        }
+
+        private void SaveTable()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(@"Engine\" + NameStrategyUniq + @"SpreadScreener.txt", false)
+                    )
+                {
+                    for (int i = 0; i < _grid.Rows.Count - 1; i++)
+                    {
+                        string saveString = "";
+
+                        for (int j = 0; j < _grid.Columns.Count - 1; j++)
+                        {
+                            saveString += _grid.Rows[i].Cells[j].FormattedValue.ToString() + " ";
+                        }
+                        writer.WriteLine(saveString);
+                    }
+                    writer.Close();
+                }
+            }
+            catch (Exception)
+            {
+                // ignore
+            }
+        }
+
+        private void LoadTable()
+        {
+            _grid.Rows.Clear();
+
+            if (!File.Exists(@"Engine\" + NameStrategyUniq + @"SpreadScreener.txt"))
+            {
+                return;
+            }
+            try
+            {
+                using (StreamReader reader = new StreamReader(@"Engine\" + NameStrategyUniq + @"SpreadScreener.txt"))
+                {
+                    string line;
+                    int counter = 0;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (!string.IsNullOrEmpty(line))
+                        {
+                            string[] split = line.Split(' ');
+                            _grid.Rows.Add(split);
+                            counter++;
+                        }
+                    }
+                    reader.Close();
+                }
+                _grid.CellValueChanged += NewGrid_CellValueChanged;
+            }
+            catch (Exception e)
+            {
+                SendNewLogMessage(e.ToString(), Logging.LogMessageType.Error);
+            }
+        }
+
+        public List<string> GetListSecuruties()
+        {
+            if (_tab.Tabs.Count == 0)
+            {
+                return null;
+            }
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < _tab.Tabs.Count; i++)
+            {
+                if (_tab.Tabs[i].Securiti != null)
+                {
+                    list.Add(_tab.Tabs[i].Securiti.NameFull);
+                }                
+            }
+            return list;
+        }
+        
+    }
+}
+
