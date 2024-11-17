72ae974702d107fe7375848b0b3104b52e03f2f2
VovcheG1 <aiwa1953@yandex.ru>
2024-11-17 22:06:57 +0300

    Fix orders

--- project/OsEngine/Market/Servers/BitMex/BitMexEntity/BitMexPortfolio.cs
+++ project/OsEngine/Market/Servers/BitMex/BitMexEntity/BitMexPortfolio.cs
@@ -7,29 +7,6 @@ using System.Collections.Generic;
 
 namespace OsEngine.Market.Servers.BitMex.BitMexEntity
 {
-    public class Types
-    {
-        public string account { get; set; }
-
-        public string unrealisedProfit { get; set; }
-
-        public string walletBalance { get; set; }
-        public string marginBalance { get; set; }
-
-        public string availableMargin { get; set; }
-
-    }
-
-    public class ForeignKeys
-    {
-    }
-
-    public class Attributes
-    {
-        public string account { get; set; }
-        public string currency { get; set; }
-    }
-
     public class Datum
     {
         public string account { get; set; }
@@ -46,25 +23,12 @@ namespace OsEngine.Market.Servers.BitMex.BitMexEntity
         public string availableMargin { get; set; }
         public string withdrawableMargin { get; set; }
         public string timestamp { get; set; }
-        
-
-    }
-
-    public class Filter
-    {
-        public string account { get; set; }
     }
 
     public class BitMexPortfolio
     {
         public string table { get; set; }
-        public List<string> keys { get; set; }
-        public Types types { get; set; }
-        public ForeignKeys foreignKeys { get; set; }
-        public Attributes attributes { get; set; }
         public string action { get; set; }
         public List<Datum> data { get; set; }
-        public Filter filter { get; set; }
-
     }
 }
--- project/OsEngine/Market/Servers/BitMex/BitMexEntity/BitMexSecurity.cs
+++ project/OsEngine/Market/Servers/BitMex/BitMexEntity/BitMexSecurity.cs
@@ -24,53 +24,7 @@ namespace OsEngine.Market.Servers.BitMex.BitMexEntity
         public string takerFee; // 0
         public string fundingRat; // 0
         public string indicativeFundingRate; // 0
-
         public string underlyingToSettleMultiplier;
         public string underlyingToPositionMultiplier;
-
-        //"rebalanceTimestamp": "2024-07-27T07:02:30.608Z",
-        //"rebalanceInterval": "2024-07-27T07:02:30.608Z",
-        //"prevClosePrice": 0,
-        //"limitDownPrice": 0,
-        //"limitUpPrice": 0,
-        //"totalVolume": 0,
-        //"volume": 0,
-        //"volume24h": 0,
-        //"prevTotalTurnover": 0,
-        //"totalTurnover": 0,
-        //"turnover": 0,
-        //"turnover24h": 0,
-        //"homeNotional24h": 0,
-        //"foreignNotional24h": 0,
-        //"prevPrice24h": 0,
-        //"vwap": 0,
-        //"highPrice": 0,
-        //"lowPrice": 0,
-        //"lastPrice": 0,
-        //"lastPriceProtected": 0,
-        //"lastTickDirection": "string",
-        //"lastChangePcnt": 0,
-        //"bidPrice": 0,
-        //"midPrice": 0,
-        //"askPrice": 0,
-        //"impactBidPrice": 0,
-        //"impactMidPrice": 0,
-        //"impactAskPrice": 0,
-        //"hasLiquidity": true,
-        //"openInterest": 0,
-        //"openValue": 0,
-        //"fairMethod": "string",
-        //"fairBasisRate": 0,
-        //"fairBasis": 0,
-        //"fairPrice": 0,
-        //"markMethod": "string",
-        //"markPrice": 0,
-        //"indicativeSettlePrice": 0,
-        //"settledPriceAdjustmentRate": 0,
-        //"settledPrice": 0,
-        //"instantPnl": true,
-        //"minTick": 0,
-        //"timestamp": "2024-07-27T07:02:30.608Z"
-
     }
 }
