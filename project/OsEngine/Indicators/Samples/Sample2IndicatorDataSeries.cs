877e93fd413cda567d06402a4376ba437cb19093
unknown <alexey@o-s-a.net>
2024-12-19 08:05:16 +0000

    Indicators layer refactoring

---
+++ project/OsEngine/Indicators/Samples/Sample2IndicatorDataSeries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Indicators.Samples
+{
+    internal class Sample2IndicatorDataSeries
+    {
+    }
+}
