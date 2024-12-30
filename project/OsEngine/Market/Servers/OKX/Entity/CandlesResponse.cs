68a9365bf0f15a14ebcafc8b236dae3ead62d541
Nikita Buida <nikita.buida@gmail.com>
2024-12-30 16:39:45 +0300

    Переименованные json

---
+++ project/OsEngine/Market/Servers/OKX/Entity/CandlesResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace OsEngine.Market.Servers.OKX.Entity
+{
+    public class CandlesResponse
+    {
+        public string code;
+        public string msg;
+        public List<List<string>> data;
+    }
+}
---
+++ project/OsEngine/Market/Servers/OKX/Entity/TradeDetailsResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+
+namespace OsEngine.Market.Servers.OKX.Entity
+{
+    public class TradeDetailsResponse
+    {
+        public string code;
+        public string msg;
+        public List<TradeDetailsObject> data;
+    }
+
+
+    public class TradeDetailsObject
+    {
+        public string instType;
+        public string instId;
+        public string tradeId;
+        public string ordId;
+        public string clOrdId;
+        public string billId;
+        public string tag;
+        public string fillPx;
+        public string fillSz;
+        public string side;
+        public string posSide;
+        public string execType;
+        public string feeCcy;
+        public string fee;
+        public string ts;
+
+    }
+}
