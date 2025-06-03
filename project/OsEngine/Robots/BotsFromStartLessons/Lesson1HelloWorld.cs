cf1b722bf98c0a44a16d9b786b3b0dec3c67563e
Roman <razziroma6@gmail.com>
2025-06-03 13:30:07 +0300

    Added Description

--- project/OsEngine/Robots/BotsFromStartLessons/Lesson1HelloWorld.cs
+++ project/OsEngine/Robots/BotsFromStartLessons/Lesson1HelloWorld.cs
@@ -1,17 +1,24 @@
 ﻿using OsEngine.Entity;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
-
+/* Description
+Robot-example from the course of lectures "C# for algotreader".
+This script creates a button in the parameters of the robot. When you press the button, the Log error with the text "Hello world!"
+ */
 namespace OsEngine.Robots.BotsFromStartLessons
 {
     [Bot("Lesson1HelloWorld")]
     public class Lesson1HelloWorld : BotPanel
     {
+
         public Lesson1HelloWorld(string name, StartProgram startProgram) : base(name, startProgram)
         {
             StrategyParameterButton button = CreateParameterButton("Hello world button");
 
             button.UserClickOnButtonEvent += Button_UserClickOnButtonEvent;
+            Description = "Robot-example from the course of lectures C# for algotreader." +
+                "This script creates a button in the parameters of the robot." +
+                "When you press the button, the Log error with the text \"Hello world!\"";
         }
 
         private void Button_UserClickOnButtonEvent()
@@ -28,5 +35,6 @@ namespace OsEngine.Robots.BotsFromStartLessons
         {
 
         }
+        
     }
 }
