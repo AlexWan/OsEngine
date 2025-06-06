72b5219c067d5ec097bed5cd9c9767e6037245fe
Tati <orkipes@gmail.com>
2025-06-06 22:27:54 +0200

    add BitfinexFutures

---
+++ project/OsEngine/Market/Servers/Bitfinex/BitfinexFutures/Json/BitfinexFuturesWebsocketTrades.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace OsEngine.Market.Servers.Bitfinex.Json
+{
+    public class BitfinexFuturesSubscriptionResponse
+    {
+        public string Event { get; set; }
+        public string Channel { get; set; }
+        public string ChanId { get; set; }
+        public string Symbol { get; set; }
+        public string Pair { get; set; }
+    }
+
+    class BitfinexFuturesAuthResponseWebSocket
+    {
+        public string Event { get; set; }
+        public string Status { get; set; }
+        public string ChanId { get; set; }
+        public string UserId { get; set; }
+        public string AuthId { get; set; }
+        public string Msg { get; set; }
+    }
+}
