88dd80029657307ef9c93cfe56435ee353add59c
JChinaM <jchinam@mail.ru>
2025-06-09 18:21:26 +0300

    IntersectionRsiAndCCI, IntersectionSencouAWithSencouB and  IntersectionTenkanAndKijun Refactoring Bots

--- project/OsEngine/bin/Debug/Custom/Robots/IntersectionTenkanAndKijun.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/IntersectionTenkanAndKijun.cs
@@ -1,10 +1,17 @@
-﻿using OsEngine.Entity;
+﻿/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using OsEngine.Entity;
 using OsEngine.Indicators;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.OsTrader.Panels.Tab;
 using System;
 using System.Collections.Generic;
+using OsEngine.Market.Servers;
+using OsEngine.Market;
 
 /* Description
 trading robot for osengine
@@ -20,7 +27,6 @@ If the intersection occurs in the cloud (between the lines Senkou A and Senkou B
 Exit: on the opposite signal.
  */
 
-
 namespace OsEngine.Robots
 {
     [Bot("IntersectionTenkanAndKijun")] // We create an attribute so that we don't write anything to the BotFactory
@@ -29,22 +35,25 @@ namespace OsEngine.Robots
         private BotTabSimple _tab;
 
         // Basic Settings
-        private StrategyParameterString Regime;
-        private StrategyParameterString VolumeRegime;
-        private StrategyParameterDecimal VolumeOnPosition;
-        private StrategyParameterDecimal Slippage;
-        private StrategyParameterTimeOfDay StartTradeTime;
-        private StrategyParameterTimeOfDay EndTradeTime;
+        private StrategyParameterString _regime;
+        private StrategyParameterDecimal _slippage;
+        private StrategyParameterTimeOfDay _startTradeTime;
+        private StrategyParameterTimeOfDay _endTradeTime;
+
+        // GetVolume Settings
+        private StrategyParameterString _volumeType;
+        private StrategyParameterDecimal _volume;
+        private StrategyParameterString _tradeAssetInPortfolio;
 
         // Indicator setting 
-        private StrategyParameterInt TenkanLength;
-        private StrategyParameterInt KijunLength;
-        private StrategyParameterInt SenkouLength;
-        private StrategyParameterInt ChinkouLength;
-        private StrategyParameterInt Offset;
+        private StrategyParameterInt _tenkanLength;
+        private StrategyParameterInt _kijunLength;
+        private StrategyParameterInt _senkouLength;
+        private StrategyParameterInt _chinkouLength;
+        private StrategyParameterInt _offset;
 
         // Indicator
-        Aindicator _Ichomoku;
+        private Aindicator _ichomoku;
 
         // The last value of the indicator
         private decimal _lastTenkan;
@@ -62,29 +71,32 @@ namespace OsEngine.Robots
             _tab = TabsSimple[0];
 
             // Basic setting
-            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
-            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
-            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
-            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
-            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
-            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+            _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            _slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+            // GetVolume Settings
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
 
             // Indicator setting
-            TenkanLength = CreateParameter("Tenkan Length", 9, 1, 50, 3, "Indicator");
-            KijunLength = CreateParameter("Kijun Length", 26, 1, 50, 4, "Indicator");
-            SenkouLength = CreateParameter("Senkou Length", 52, 1, 100, 8, "Indicator");
-            ChinkouLength = CreateParameter("Chinkou Length", 26, 1, 50, 4, "Indicator");
-            Offset = CreateParameter("Offset", 26, 1, 50, 4, "Indicator");
+            _tenkanLength = CreateParameter("Tenkan Length", 9, 1, 50, 3, "Indicator");
+            _kijunLength = CreateParameter("Kijun Length", 26, 1, 50, 4, "Indicator");
+            _senkouLength = CreateParameter("Senkou Length", 52, 1, 100, 8, "Indicator");
+            _chinkouLength = CreateParameter("Chinkou Length", 26, 1, 50, 4, "Indicator");
+            _offset = CreateParameter("Offset", 26, 1, 50, 4, "Indicator");
 
             // Create indicator _Ichomoku
-            _Ichomoku = IndicatorsFactory.CreateIndicatorByName("Ichimoku", name + "Ichimoku", false);
-            _Ichomoku = (Aindicator)_tab.CreateCandleIndicator(_Ichomoku, "Prime");
-            ((IndicatorParameterInt)_Ichomoku.Parameters[0]).ValueInt = TenkanLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[1]).ValueInt = KijunLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[2]).ValueInt = SenkouLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[3]).ValueInt = ChinkouLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[4]).ValueInt = Offset.ValueInt;
-            _Ichomoku.Save();
+            _ichomoku = IndicatorsFactory.CreateIndicatorByName("Ichimoku", name + "Ichimoku", false);
+            _ichomoku = (Aindicator)_tab.CreateCandleIndicator(_ichomoku, "Prime");
+            ((IndicatorParameterInt)_ichomoku.Parameters[0]).ValueInt = _tenkanLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[1]).ValueInt = _kijunLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[2]).ValueInt = _senkouLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[3]).ValueInt = _chinkouLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[4]).ValueInt = _offset.ValueInt;
+            _ichomoku.Save();
 
             // Subscribe to the indicator update event
             ParametrsChangeByUser += BreakChaikin_ParametrsChangeByUser; ;
@@ -101,13 +113,13 @@ namespace OsEngine.Robots
 
         private void BreakChaikin_ParametrsChangeByUser()
         {
-            ((IndicatorParameterInt)_Ichomoku.Parameters[0]).ValueInt = TenkanLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[1]).ValueInt = KijunLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[2]).ValueInt = SenkouLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[3]).ValueInt = ChinkouLength.ValueInt;
-            ((IndicatorParameterInt)_Ichomoku.Parameters[4]).ValueInt = Offset.ValueInt;
-            _Ichomoku.Save();
-            _Ichomoku.Reload();
+            ((IndicatorParameterInt)_ichomoku.Parameters[0]).ValueInt = _tenkanLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[1]).ValueInt = _kijunLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[2]).ValueInt = _senkouLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[3]).ValueInt = _chinkouLength.ValueInt;
+            ((IndicatorParameterInt)_ichomoku.Parameters[4]).ValueInt = _offset.ValueInt;
+            _ichomoku.Save();
+            _ichomoku.Reload();
         }
 
         // The name of the robot in OsEngine
@@ -124,24 +136,24 @@ namespace OsEngine.Robots
         private void _tab_CandleFinishedEvent(List<Candle> candles)
         {
             // If the robot is turned off, exit the event handler
-            if (Regime.ValueString == "Off")
+            if (_regime.ValueString == "Off")
             {
                 return;
             }
 
             // If there are not enough candles to build an indicator, we exit
-            if (candles.Count < TenkanLength.ValueInt ||
-                candles.Count < KijunLength.ValueInt ||
-                candles.Count < SenkouLength.ValueInt ||
-                candles.Count < ChinkouLength.ValueInt ||
-                candles.Count < Offset.ValueInt)
+            if (candles.Count < _tenkanLength.ValueInt ||
+                candles.Count < _kijunLength.ValueInt ||
+                candles.Count < _senkouLength.ValueInt ||
+                candles.Count < _chinkouLength.ValueInt ||
+                candles.Count < _offset.ValueInt)
             {
                 return;
             }
 
             // If the time does not match, we leave
-            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
-                EndTradeTime.Value < _tab.TimeServerCurrent)
+            if (_startTradeTime.Value > _tab.TimeServerCurrent ||
+                _endTradeTime.Value < _tab.TimeServerCurrent)
             {
                 return;
             }
@@ -155,10 +167,11 @@ namespace OsEngine.Robots
             }
 
             // If the position closing mode, then exit the method
-            if (Regime.ValueString == "OnlyClosePosition")
+            if (_regime.ValueString == "OnlyClosePosition")
             {
                 return;
             }
+
             // If there are no positions, then go to the position opening method
             if (openPositions == null || openPositions.Count == 0)
             {
@@ -170,14 +183,14 @@ namespace OsEngine.Robots
         private void LogicOpenPosition(List<Candle> candles)
         {
             // The last value of the indicator
-            _lastTenkan = _Ichomoku.DataSeries[0].Last;
-            _lastKijun = _Ichomoku.DataSeries[1].Last;
-            _lastSenkouA = _Ichomoku.DataSeries[3].Last;
-            _lastSenkouB = _Ichomoku.DataSeries[4].Last;
+            _lastTenkan = _ichomoku.DataSeries[0].Last;
+            _lastKijun = _ichomoku.DataSeries[1].Last;
+            _lastSenkouA = _ichomoku.DataSeries[3].Last;
+            _lastSenkouB = _ichomoku.DataSeries[4].Last;
 
             // The prev value of the indicator
-            _prevTenkan = _Ichomoku.DataSeries[0].Values[_Ichomoku.DataSeries[0].Values.Count - 2];
-            _prevKijun = _Ichomoku.DataSeries[1].Values[_Ichomoku.DataSeries[1].Values.Count - 2];
+            _prevTenkan = _ichomoku.DataSeries[0].Values[_ichomoku.DataSeries[0].Values.Count - 2];
+            _prevKijun = _ichomoku.DataSeries[1].Values[_ichomoku.DataSeries[1].Values.Count - 2];
 
             List<Position> openPositions = _tab.PositionsOpenAll;
 
@@ -186,10 +199,10 @@ namespace OsEngine.Robots
                 decimal lastPrice = candles[candles.Count - 1].Close;
 
                 // Slippage
-                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+                decimal _slippage = this._slippage.ValueDecimal * _tab.Securiti.PriceStep;
 
                 // Long
-                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                if (_regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
                 {
                     if (_lastTenkan > _lastKijun)
                     {
@@ -199,12 +212,12 @@ namespace OsEngine.Robots
                             return;
                         }
 
-                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                        _tab.BuyAtLimit(GetVolume(_tab), _tab.PriceBestAsk + _slippage);
                     }
                 }
 
                 // Short
-                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                if (_regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
                 {
                     if (_lastTenkan < _lastKijun)
                     {
@@ -214,7 +227,7 @@ namespace OsEngine.Robots
                             return;
                         }
 
-                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                        _tab.SellAtLimit(GetVolume(_tab), _tab.PriceBestBid - _slippage);
                     }
                 }
             }
@@ -226,16 +239,16 @@ namespace OsEngine.Robots
             List<Position> openPositions = _tab.PositionsOpenAll;
             
             // The last value of the indicator
-            _lastTenkan = _Ichomoku.DataSeries[0].Last;
-            _lastKijun = _Ichomoku.DataSeries[1].Last;
-            _lastSenkouA = _Ichomoku.DataSeries[3].Last;
-            _lastSenkouB = _Ichomoku.DataSeries[4].Last;
+            _lastTenkan = _ichomoku.DataSeries[0].Last;
+            _lastKijun = _ichomoku.DataSeries[1].Last;
+            _lastSenkouA = _ichomoku.DataSeries[3].Last;
+            _lastSenkouB = _ichomoku.DataSeries[4].Last;
 
             // The prev value of the indicator
-            _prevTenkan = _Ichomoku.DataSeries[0].Values[_Ichomoku.DataSeries[0].Values.Count - 2];
-            _prevKijun = _Ichomoku.DataSeries[1].Values[_Ichomoku.DataSeries[1].Values.Count - 2];
+            _prevTenkan = _ichomoku.DataSeries[0].Values[_ichomoku.DataSeries[0].Values.Count - 2];
+            _prevKijun = _ichomoku.DataSeries[1].Values[_ichomoku.DataSeries[1].Values.Count - 2];
 
-            decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+            decimal _slippage = this._slippage.ValueDecimal * _tab.Securiti.PriceStep;
 
             decimal lastPrice = candles[candles.Count - 1].Close;
 
@@ -248,14 +261,14 @@ namespace OsEngine.Robots
                     continue;
                 }
 
-                if (pos.Direction == Side.Buy) // If the direction of the position is purchase
+                if (pos.Direction == Side.Buy) // If the direction of the position is long
                 {
                     if (_lastTenkan < _lastKijun)
                     {
                         _tab.CloseAtLimit(pos, lastPrice - _slippage, pos.OpenVolume);
                     }
                 }
-                else // If the direction of the position is sale
+                else // If the direction of the position is short
                 {
                     if (_lastTenkan > _lastKijun)
                     {
@@ -266,29 +279,94 @@ namespace OsEngine.Robots
         }
 
         // Method for calculating the volume of entry into a position
-        private decimal GetVolume()
+        private decimal GetVolume(BotTabSimple tab)
         {
             decimal volume = 0;
 
-            if (VolumeRegime.ValueString == "Contract currency")
+            if (_volumeType.ValueString == "Contracts")
             {
-                decimal contractPrice = _tab.PriceBestAsk;
-                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+                volume = _volume.ValueDecimal;
             }
-            else if (VolumeRegime.ValueString == "Number of contracts")
+            else if (_volumeType.ValueString == "Contract currency")
             {
-                volume = VolumeOnPosition.ValueDecimal;
+                decimal contractPrice = tab.PriceBestAsk;
+                volume = _volume.ValueDecimal / contractPrice;
+
+                if (StartProgram == StartProgram.IsOsTrader)
+                {
+                    IServerPermission serverPermission = ServerMaster.GetServerPermission(tab.Connector.ServerType);
+
+                    if (serverPermission != null &&
+                        serverPermission.IsUseLotToCalculateProfit &&
+                    tab.Security.Lot != 0 &&
+                        tab.Security.Lot > 1)
+                    {
+                        volume = _volume.ValueDecimal / (contractPrice * tab.Security.Lot);
+                    }
+
+                    volume = Math.Round(volume, tab.Security.DecimalsVolume);
+                }
+                else // Tester or Optimizer
+                {
+                    volume = Math.Round(volume, 6);
+                }
+            }
+            else if (_volumeType.ValueString == "Deposit percent")
+            {
+                Portfolio myPortfolio = tab.Portfolio;
+
+                if (myPortfolio == null)
+                {
+                    return 0;
+                }
+
+                decimal portfolioPrimeAsset = 0;
+
+                if (_tradeAssetInPortfolio.ValueString == "Prime")
+                {
+                    portfolioPrimeAsset = myPortfolio.ValueCurrent;
+                }
+                else
+                {
+                    List<PositionOnBoard> positionOnBoard = myPortfolio.GetPositionOnBoard();
+
+                    if (positionOnBoard == null)
+                    {
+                        return 0;
+                    }
+
+                    for (int i = 0; i < positionOnBoard.Count; i++)
+                    {
+                        if (positionOnBoard[i].SecurityNameCode == _tradeAssetInPortfolio.ValueString)
+                        {
+                            portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
+                            break;
+                        }
+                    }
+                }
+
+                if (portfolioPrimeAsset == 0)
+                {
+                    SendNewLogMessage("Can`t found portfolio " + _tradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
+                    return 0;
+                }
+
+                decimal moneyOnPosition = portfolioPrimeAsset * (_volume.ValueDecimal / 100);
+
+                decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
+
+                if (tab.StartProgram == StartProgram.IsOsTrader)
+                {
+                    qty = Math.Round(qty, tab.Security.DecimalsVolume);
+                }
+                else
+                {
+                    qty = Math.Round(qty, 7);
+                }
+
+                return qty;
             }
 
-            // If the robot is running in the tester
-            if (StartProgram == StartProgram.IsTester)
-            {
-                volume = Math.Round(volume, 6);
-            }
-            else
-            {
-                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
-            }
             return volume;
         }
     }
