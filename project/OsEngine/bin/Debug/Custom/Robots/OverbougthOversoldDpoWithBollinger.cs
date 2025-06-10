4f694cbce280835ae5a2e90f74bb05a2b5692fc7
JChinaM <jchinam@mail.ru>
2025-06-10 15:13:23 +0300

    OverbougthOversoldCMO, OverbougthOversoldDeMarker and OverbougthOversoldDpoWithBollinger Refactoring Bots

--- project/OsEngine/bin/Debug/Custom/Robots/OverbougthOversoldDpoWithBollinger.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/OverbougthOversoldDpoWithBollinger.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
 using System.Collections.Generic;
 using OsEngine.Entity;
 using OsEngine.Indicators;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Market.Servers;
+using OsEngine.Market;
 
 /* Description
 trading robot for osengine
@@ -22,7 +29,7 @@ Exit from buy: trailing stop in % of the loy of the candle on which you entered.
 Exit from sell: trailing stop in % of the high of the candle on which you entered.
  */
 
-namespace OsEngine.Robots.MyBots
+namespace OsEngine.Robots
 {
     [Bot("OverbougthOversoldDpoWithBollinger")] // We create an attribute so that we don't write anything to the BotFactory
     internal class OverbougthOversoldDpoWithBollinger : BotPanel
@@ -30,62 +37,68 @@ namespace OsEngine.Robots.MyBots
         private BotTabSimple _tab;
 
         // Basic Settings
-        private StrategyParameterString Regime;
-        private StrategyParameterString VolumeRegime;
-        private StrategyParameterDecimal VolumeOnPosition;
-        private StrategyParameterDecimal Slippage;
-        private StrategyParameterTimeOfDay StartTradeTime;
-        private StrategyParameterTimeOfDay EndTradeTime;
+        private StrategyParameterString _regime;
+        private StrategyParameterDecimal _slippage;
+        private StrategyParameterTimeOfDay _startTradeTime;
+        private StrategyParameterTimeOfDay _endTradeTime;
 
-        // Indicator setting  
-        private StrategyParameterInt DpoLength;
-        private StrategyParameterDecimal OverboughtLevel;
-        private StrategyParameterDecimal OversoldLevel;
-        private StrategyParameterInt BollingerLength;
-        private StrategyParameterDecimal BollingerDeviation;
+        // GetVolume Settings
+        private StrategyParameterString _volumeType;
+        private StrategyParameterDecimal _volume;
+        private StrategyParameterString _tradeAssetInPortfolio;
 
-        // Exit
-        private StrategyParameterDecimal TrailingValue;
+        // Indicator Settings  
+        private StrategyParameterInt _dpoLength;
+        private StrategyParameterDecimal _overboughtLevel;
+        private StrategyParameterDecimal _oversoldLevel;
+        private StrategyParameterInt _bollingerLength;
+        private StrategyParameterDecimal _bollingerDeviation;
+
+        // Exit Setting
+        private StrategyParameterDecimal _trailingValue;
 
         // Indicator
-        private Aindicator _Dpo;
-        private Aindicator _Bollinger;
+        private Aindicator _dpo;
+        private Aindicator _bollinger;
 
         public OverbougthOversoldDpoWithBollinger(string name, StartProgram startProgram) : base(name, startProgram)
         {
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
 
-            // Basic setting
-            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
-            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
-            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
-            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
-            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
-            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+            // Basic Settings
+            _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            _slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
 
-            // Indicator setting
-            DpoLength = CreateParameter("Dpo Length", 14, 5, 200, 10, "Indicator");
-            OverboughtLevel = CreateParameter("Overbought Level", 500m, 100, 1000, 100, "Indicator");
-            OversoldLevel = CreateParameter("Oversold Level", -500m, -1000, -100, 100, "Indicator");
-            BollingerLength = CreateParameter("Bollinger Length", 50, 10, 300, 10, "Indicator");
-            BollingerDeviation = CreateParameter("Bollinger Deviation", 1.0m, 1, 5, 0.1m, "Indicator");
+            // GetVolume Settings
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
 
-            // Exit
-            TrailingValue = CreateParameter("Trailing Value", 1.0m, 1, 20, 1, "Exit");
+            // Indicator Settings
+            _dpoLength = CreateParameter("Dpo Length", 14, 5, 200, 10, "Indicator");
+            _overboughtLevel = CreateParameter("Overbought Level", 0.5m, 100, 1000, 100, "Indicator");
+            _oversoldLevel = CreateParameter("Oversold Level", -0.5m, -1000, -100, 100, "Indicator");
+            _bollingerLength = CreateParameter("Bollinger Length", 50, 10, 300, 10, "Indicator");
+            _bollingerDeviation = CreateParameter("Bollinger Deviation", 1.0m, 1, 5, 0.1m, "Indicator");
+            
+            // Exit Setting
+            _trailingValue = CreateParameter("Trailing Value", 1.0m, 1, 20, 1, "Exit");
 
             // Create indicator Dpo
-            _Dpo = IndicatorsFactory.CreateIndicatorByName("DPO_Detrended_Price_Oscillator", name + "Dpo", false);
-            _Dpo = (Aindicator)_tab.CreateCandleIndicator(_Dpo, "DpoArea");
-            ((IndicatorParameterInt)_Dpo.Parameters[0]).ValueInt = DpoLength.ValueInt;
-            _Dpo.Save();
+            _dpo = IndicatorsFactory.CreateIndicatorByName("DPO_Detrended_Price_Oscillator", name + "Dpo", false);
+            _dpo = (Aindicator)_tab.CreateCandleIndicator(_dpo, "DpoArea");
+            ((IndicatorParameterInt)_dpo.Parameters[0]).ValueInt = _dpoLength.ValueInt;
+            _dpo.Save();
 
             // Create indicator Bollinger
-            _Bollinger = IndicatorsFactory.CreateIndicatorByName("Bollinger", name + "Bollinger", false);
-            _Bollinger = (Aindicator)_tab.CreateCandleIndicator(_Bollinger, "Prime");
-            ((IndicatorParameterInt)_Bollinger.Parameters[0]).ValueInt = BollingerLength.ValueInt;
-            ((IndicatorParameterDecimal)_Bollinger.Parameters[1]).ValueDecimal = BollingerDeviation.ValueDecimal;
-            _Bollinger.Save();
+            _bollinger = IndicatorsFactory.CreateIndicatorByName("Bollinger", name + "Bollinger", false);
+            _bollinger = (Aindicator)_tab.CreateCandleIndicator(_bollinger, "Prime");
+            ((IndicatorParameterInt)_bollinger.Parameters[0]).ValueInt = _bollingerLength.ValueInt;
+            ((IndicatorParameterDecimal)_bollinger.Parameters[1]).ValueDecimal = _bollingerDeviation.ValueDecimal;
+            _bollinger.Save();
 
             // Subscribe to the indicator update event
             ParametrsChangeByUser += OverbougthOversoldDpoWithBollinger_ParametrsChangeByUser;
@@ -104,45 +117,44 @@ namespace OsEngine.Robots.MyBots
 
         private void OverbougthOversoldDpoWithBollinger_ParametrsChangeByUser()
         {
-            ((IndicatorParameterInt)_Dpo.Parameters[0]).ValueInt = DpoLength.ValueInt;
-            _Dpo.Save();
-            _Dpo.Reload();
+            ((IndicatorParameterInt)_dpo.Parameters[0]).ValueInt = _dpoLength.ValueInt;
+            _dpo.Save();
+            _dpo.Reload();
 
-            ((IndicatorParameterInt)_Bollinger.Parameters[0]).ValueInt = BollingerLength.ValueInt;
-            ((IndicatorParameterDecimal)_Bollinger.Parameters[1]).ValueDecimal = BollingerDeviation.ValueDecimal;
-            _Bollinger.Save();
-            _Bollinger.Reload();
+            ((IndicatorParameterInt)_bollinger.Parameters[0]).ValueInt = _bollingerLength.ValueInt;
+            ((IndicatorParameterDecimal)_bollinger.Parameters[1]).ValueDecimal = _bollingerDeviation.ValueDecimal;
+            _bollinger.Save();
+            _bollinger.Reload();
         }
 
         public override string GetNameStrategyType()
         {
             return "OverbougthOversoldDpoWithBollinger";
         }
-
         public override void ShowIndividualSettingsDialog()
         {
+
         }
 
-        // Logic
         // Candle Finished Event
         private void _tab_CandleFinishedEvent(List<Candle> candles)
         {
             // If the robot is turned off, exit the event handler
-            if (Regime.ValueString == "Off")
+            if (_regime.ValueString == "Off")
             {
                 return;
             }
 
             // If there are not enough candles to build an indicator, we exit
-            if (candles.Count < DpoLength.ValueInt + 10 ||
-                candles.Count < BollingerLength.ValueInt)
+            if (candles.Count < _dpoLength.ValueInt + 10 ||
+                candles.Count < _bollingerLength.ValueInt)
             {
                 return;
             }
 
             // If the time does not match, we leave
-            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
-                EndTradeTime.Value < _tab.TimeServerCurrent)
+            if (_startTradeTime.Value > _tab.TimeServerCurrent ||
+                _endTradeTime.Value < _tab.TimeServerCurrent)
             {
                 return;
             }
@@ -156,10 +168,11 @@ namespace OsEngine.Robots.MyBots
             }
 
             // If the position closing mode, then exit the method
-            if (Regime.ValueString == "OnlyClosePosition")
+            if (_regime.ValueString == "OnlyClosePosition")
             {
                 return;
             }
+
             // If there are no positions, then go to the position opening method
             if (openPositions == null || openPositions.Count == 0)
             {
@@ -172,12 +185,12 @@ namespace OsEngine.Robots.MyBots
             List<Position> openPositions = _tab.PositionsOpenAll;
 
             // The value of the indicator
-            decimal lastDpo = _Dpo.DataSeries[0].Last;
-            decimal prevDpo = _Dpo.DataSeries[0].Values[_Dpo.DataSeries[0].Values.Count - 2];
-            decimal lastUpLineBol = _Bollinger.DataSeries[0].Last;
-            decimal lastDownLineBol = _Bollinger.DataSeries[1].Last;
-            decimal prevUpLineBol = _Bollinger.DataSeries[0].Values[_Bollinger.DataSeries[0].Values.Count - 2];
-            decimal prevDownLineBol = _Bollinger.DataSeries[1].Values[_Bollinger.DataSeries[1].Values.Count - 2];
+            decimal lastDpo = _dpo.DataSeries[0].Last;
+            decimal prevDpo = _dpo.DataSeries[0].Values[_dpo.DataSeries[0].Values.Count - 2];
+            decimal lastUpLineBol = _bollinger.DataSeries[0].Last;
+            decimal lastDownLineBol = _bollinger.DataSeries[1].Last;
+            decimal prevUpLineBol = _bollinger.DataSeries[0].Values[_bollinger.DataSeries[0].Values.Count - 2];
+            decimal prevDownLineBol = _bollinger.DataSeries[1].Values[_bollinger.DataSeries[1].Values.Count - 2];
 
             decimal lastPrice = candles[candles.Count - 1].Close;
             decimal prevPrice = candles[candles.Count - 2].Close;
@@ -190,25 +203,25 @@ namespace OsEngine.Robots.MyBots
             if (openPositions == null || openPositions.Count == 0)
             {
                 // Slippage
-                decimal _slippage = Slippage.ValueDecimal / 100 * _tab.Securiti.PriceStep;
+                decimal _slippage = this._slippage.ValueDecimal / 100 * _tab.Securiti.PriceStep;
 
                 // Long
-                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                if (_regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
                 {
-                    if (lastDpo > OversoldLevel.ValueDecimal && prevDpo < OversoldLevel.ValueDecimal && lastDownLineBol < lastPrice
+                    if (lastDpo > _oversoldLevel.ValueDecimal && prevDpo < _oversoldLevel.ValueDecimal && lastDownLineBol < lastPrice
                          && prevPrice < prevDownLineBol)
                     {
-                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                        _tab.BuyAtLimit(GetVolume(_tab), _tab.PriceBestAsk + _slippage);
                     }
                 }
 
                 // Short
-                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                if (_regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
                 {
-                    if (lastDpo < OverboughtLevel.ValueDecimal && prevDpo > OverboughtLevel.ValueDecimal && lastUpLineBol > lastPrice
+                    if (lastDpo < _overboughtLevel.ValueDecimal && prevDpo > _overboughtLevel.ValueDecimal && lastUpLineBol > lastPrice
                         && prevPrice > prevUpLineBol)
                     {
-                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                        _tab.SellAtLimit(GetVolume(_tab), _tab.PriceBestBid - _slippage);
                     }
                 }
             }
@@ -230,44 +243,109 @@ namespace OsEngine.Robots.MyBots
                     continue;
                 }
 
-                if (pos.Direction == Side.Buy) // If the direction of the position is purchase
+                if (pos.Direction == Side.Buy) // If the direction of the position is long
                 {
                     decimal lov = candles[candles.Count - 1].Low;
-                    stopPrice = lov - lov * TrailingValue.ValueDecimal / 100;
+                    stopPrice = lov - lov * _trailingValue.ValueDecimal / 100;
                 }
-                else // If the direction of the position is sale
+                else // If the direction of the position is short
                 {
                     decimal high = candles[candles.Count - 1].High;
-                    stopPrice = high + high * TrailingValue.ValueDecimal / 100;
+                    stopPrice = high + high * _trailingValue.ValueDecimal / 100;
                 }
                 _tab.CloseAtTrailingStop(pos, stopPrice, stopPrice);
             }
         }
 
         // Method for calculating the volume of entry into a position
-        private decimal GetVolume()
+        private decimal GetVolume(BotTabSimple tab)
         {
             decimal volume = 0;
 
-            if (VolumeRegime.ValueString == "Contract currency")
+            if (_volumeType.ValueString == "Contracts")
             {
-                decimal contractPrice = _tab.PriceBestAsk;
-                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+                volume = _volume.ValueDecimal;
             }
-            else if (VolumeRegime.ValueString == "Number of contracts")
+            else if (_volumeType.ValueString == "Contract currency")
             {
-                volume = VolumeOnPosition.ValueDecimal;
+                decimal contractPrice = tab.PriceBestAsk;
+                volume = _volume.ValueDecimal / contractPrice;
+
+                if (StartProgram == StartProgram.IsOsTrader)
+                {
+                    IServerPermission serverPermission = ServerMaster.GetServerPermission(tab.Connector.ServerType);
+
+                    if (serverPermission != null &&
+                        serverPermission.IsUseLotToCalculateProfit &&
+                    tab.Security.Lot != 0 &&
+                        tab.Security.Lot > 1)
+                    {
+                        volume = _volume.ValueDecimal / (contractPrice * tab.Security.Lot);
+                    }
+
+                    volume = Math.Round(volume, tab.Security.DecimalsVolume);
+                }
+                else // Tester or Optimizer
+                {
+                    volume = Math.Round(volume, 6);
+                }
+            }
+            else if (_volumeType.ValueString == "Deposit percent")
+            {
+                Portfolio myPortfolio = tab.Portfolio;
+
+                if (myPortfolio == null)
+                {
+                    return 0;
+                }
+
+                decimal portfolioPrimeAsset = 0;
+
+                if (_tradeAssetInPortfolio.ValueString == "Prime")
+                {
+                    portfolioPrimeAsset = myPortfolio.ValueCurrent;
+                }
+                else
+                {
+                    List<PositionOnBoard> positionOnBoard = myPortfolio.GetPositionOnBoard();
+
+                    if (positionOnBoard == null)
+                    {
+                        return 0;
+                    }
+
+                    for (int i = 0; i < positionOnBoard.Count; i++)
+                    {
+                        if (positionOnBoard[i].SecurityNameCode == _tradeAssetInPortfolio.ValueString)
+                        {
+                            portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
+                            break;
+                        }
+                    }
+                }
+
+                if (portfolioPrimeAsset == 0)
+                {
+                    SendNewLogMessage("Can`t found portfolio " + _tradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
+                    return 0;
+                }
+
+                decimal moneyOnPosition = portfolioPrimeAsset * (_volume.ValueDecimal / 100);
+
+                decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
+
+                if (tab.StartProgram == StartProgram.IsOsTrader)
+                {
+                    qty = Math.Round(qty, tab.Security.DecimalsVolume);
+                }
+                else
+                {
+                    qty = Math.Round(qty, 7);
+                }
+
+                return qty;
             }
 
-            // If the robot is running in the tester
-            if (StartProgram == StartProgram.IsTester)
-            {
-                volume = Math.Round(volume, 6);
-            }
-            else
-            {
-                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
-            }
             return volume;
         }
     }
