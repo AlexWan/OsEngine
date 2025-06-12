5f2f37182b81b9f24ae637d5aa487807aa21a778
Roman <razziroma6@gmail.com>
2025-06-12 17:08:03 +0300

    BFSL L9B1, L9B2, L9B3, L9B4, L9B5 refactoring

--- project/OsEngine/Robots/BotsFromStartLessons/Lesson9Bot3.cs
+++ project/OsEngine/Robots/BotsFromStartLessons/Lesson9Bot3.cs
@@ -1,68 +1,80 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * Your rights to use code governed by this license http://o-s-a.net/doc/license_simple_engine.pdf
+ *Ваши права на использования кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using System;
+using System.Collections.Generic;
 using OsEngine.Entity;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.OsTrader.Panels.Tab;
-using System;
+
+/* Description
+Robot example from the lecture course "C# for algotreader".
+Stores examples of different methods for close position.
+*/
 
 namespace OsEngine.Robots.BotsFromStartLessons
 {
-    [Bot("Lesson9Bot3")]
+    [Bot("Lesson9Bot3")] // Instead of manually adding through BotFactory, we use an attribute to simplify the process.
     public class Lesson9Bot3 : BotPanel
     {
+        // Reference to the main trading tab
         BotTabSimple _tabToTrade;
 
         public Lesson9Bot3(string name, StartProgram startProgram) : base(name, startProgram)
         {
+            // Create and assign the main trading tab
             TabCreate(BotTabType.Simple);
             _tabToTrade = TabsSimple[0];
 
             // BuyAtMarket / SellAtMarket
-
             _buyMarketButton = CreateParameterButton("Market Buy", "Open");
             _buyMarketButton.UserClickOnButtonEvent += MarketBuy_UserClickOnButtonEvent;
+
             _sellMarketButton = CreateParameterButton("Market Sell", "Open");
             _sellMarketButton.UserClickOnButtonEvent += MarketSell_UserClickOnButtonEvent;
+
             _volumeOpenPosition = CreateParameter("Volume open position", 10m, 1, 20, 1, "Open");
 
             // CloseAllAtMarket
-
             _closeAllMarketButton = CreateParameterButton("Close all at market", "Market all");
             _closeAllMarketButton.UserClickOnButtonEvent += _closeAllMarketButton_UserClickOnButtonEvent;
+
             _closeAllAtMarketSignal = CreateParameter("Close all at market have signal", false, "Market all");
 
             // CloseAtFake
-
             _closeAtFakeButton = CreateParameterButton("Close at fake", "Fake");
             _closeAtFakeButton.UserClickOnButtonEvent += _closeAtFakeButton_UserClickOnButtonEvent;
 
             // CloseAtMarket
-
             _closeAtMarketButton = CreateParameterButton("Close at market", "Market one");
             _closeAtMarketButton.UserClickOnButtonEvent += _closeAtMarketButton_UserClickOnButtonEvent;
+
             _closeAtMarketSignal = CreateParameter("Close at market have signal", false, "Market one");
 
             // CloseAtLimit
-
             _closeAtLimitButton = CreateParameterButton("Close at Limit", "Limit");
             _closeAtLimitButton.UserClickOnButtonEvent += _closeAtLimitButton_UserClickOnButtonEvent;
+
             _closeAtLimitSignal = CreateParameter("Close at Limit have signal", false, "Limit");
 
             // CloseAtLimitUnsafe
-
             _closeAtLimitUnsafeButton = CreateParameterButton("Close at Limit Unsafe", "LimitUnsafe");
             _closeAtLimitUnsafeButton.UserClickOnButtonEvent += _closeAtLimitUnsafeButton_UserClickOnButtonEvent;
 
             // CloseAtIceberg
-
             _closeAtIcebergButton = CreateParameterButton("Close at Iceberg", "Iceberg");
             _closeAtIcebergButton.UserClickOnButtonEvent += _closeAtIcebergButton_UserClickOnButtonEvent;
+
             _closeAtIcebergSignal = CreateParameter("Close at Iceberg have signal", false, "Iceberg");
             _icebergCount = CreateParameter("Iceberg orders count", 2, 1, 10, 1, "Iceberg");
             _icebergMarket = CreateParameter("Iceberg market", false, "Iceberg");
             _icebergMarketMinMillisecondsDistance = CreateParameter("Iceberg market min milliseconds distance", 500, 1, 10, 1, "Iceberg");
 
-
+            Description = "Robot example from the lecture course \"C# for algotreader\"." +
+                "Stores examples of different methods for close position.";
         }
 
         #region BuyAtMarket / SellAtMarket
@@ -135,7 +147,7 @@ namespace OsEngine.Robots.BotsFromStartLessons
         {
             List<Position> openPositions = _tabToTrade.PositionsOpenAll;
 
-            if(openPositions.Count == 0)
+            if (openPositions.Count == 0)
             {
                 _tabToTrade.SetNewLogMessage("No positions", Logging.LogMessageType.Error);
                 return;
@@ -152,13 +164,13 @@ namespace OsEngine.Robots.BotsFromStartLessons
 
             DateTime time = _tabToTrade.TimeServerCurrent;
 
-            if(position.Direction == Side.Buy)
+            if (position.Direction == Side.Buy)
             {
                 decimal price = _tabToTrade.PriceBestAsk;
 
                 _tabToTrade.CloseAtFake(position, position.OpenVolume, price, time);
             }
-            else if(position.Direction == Side.Sell)
+            else if (position.Direction == Side.Sell)
             {
                 decimal price = _tabToTrade.PriceBestBid;
 
@@ -193,7 +205,7 @@ namespace OsEngine.Robots.BotsFromStartLessons
             Position position = openPositions[0];
             _tabToTrade.CloseAllOrderToPosition(position);
 
-            if(_closeAtMarketSignal.ValueBool == false)
+            if (_closeAtMarketSignal.ValueBool == false)
             {
                 _tabToTrade.CloseAtMarket(position, position.OpenVolume);
             }
@@ -232,11 +244,11 @@ namespace OsEngine.Robots.BotsFromStartLessons
 
             decimal price = 0;
 
-            if(position.Direction == Side.Buy)
+            if (position.Direction == Side.Buy)
             {
                 price = _tabToTrade.PriceBestAsk;
             }
-            else if(position.Direction == Side.Sell)
+            else if (position.Direction == Side.Sell)
             {
                 price = _tabToTrade.PriceBestBid;
             }
@@ -335,7 +347,7 @@ namespace OsEngine.Robots.BotsFromStartLessons
 
             int ordersCount = _icebergCount.ValueInt;
 
-            if(_icebergMarket.ValueBool == false)
+            if (_icebergMarket.ValueBool == false)
             { // Limit
 
                 if (_closeAtIcebergSignal.ValueBool == false)
@@ -348,7 +360,7 @@ namespace OsEngine.Robots.BotsFromStartLessons
                 }
 
             }
-            else if(_icebergMarket.ValueBool == true)
+            else if (_icebergMarket.ValueBool == true)
             { // Market
 
                 if (_closeAtIcebergSignal.ValueBool == false)
