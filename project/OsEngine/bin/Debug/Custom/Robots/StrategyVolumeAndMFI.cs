24f5a7e813915f772c0532d1f7cd43f06b0d26b0
JChinaM <jchinam@mail.ru>
2025-06-16 17:17:59 +0300

    StrategyVolumeAndMFI Refactoring bot

--- project/OsEngine/bin/Debug/Custom/Robots/StrategyVolumeAndMFI.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/StrategyVolumeAndMFI.cs
@@ -1,10 +1,18 @@
-﻿using OsEngine.Entity;
+﻿/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using OsEngine.Entity;
 using OsEngine.Indicators;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.OsTrader.Panels.Tab;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using OsEngine.Market.Servers;
+using OsEngine.Market;
 
 /* Description
 trading robot for osengine
@@ -22,8 +30,7 @@ Exit from buy: trailing stop in % of the loy of the candle on which you entered.
 Exit from sell: trailing stop in % of the high of the candle on which you entered.
  */
 
-
-namespace OsEngine.Robots.AO
+namespace OsEngine.Robots
 {
     [Bot("StrategyVolumeAndMFI")] // We create an attribute so that we don't write anything to the BotFactory
     public class StrategyVolumeAndMFI : BotPanel
@@ -31,62 +38,68 @@ namespace OsEngine.Robots.AO
         private BotTabSimple _tab;
 
         // Basic Settings
-        private StrategyParameterString Regime;
-        private StrategyParameterString VolumeRegime;
-        private StrategyParameterDecimal VolumeOnPosition;
-        private StrategyParameterDecimal Slippage;
-        private StrategyParameterTimeOfDay StartTradeTime;
-        private StrategyParameterTimeOfDay EndTradeTime;
+        private StrategyParameterString _regime;
+        private StrategyParameterDecimal _slippage;
+        private StrategyParameterTimeOfDay _startTradeTime;
+        private StrategyParameterTimeOfDay _endTradeTime;
 
-        // Indicator setting 
-        private StrategyParameterInt _MFIPeriod;
-        private StrategyParameterDecimal MfiMinValue;
-        private StrategyParameterDecimal MfiMaxValue;
-        private StrategyParameterDecimal VolumeValue;
+        // GetVolume Settings
+        private StrategyParameterString _volumeType;
+        private StrategyParameterDecimal _volume;
+        private StrategyParameterString _tradeAssetInPortfolio;
+
+        // Indicator settings
+        private StrategyParameterInt _mfiPeriod;
+        private StrategyParameterDecimal _mfiMinValue;
+        private StrategyParameterDecimal _mfiMaxValue;
+        private StrategyParameterDecimal _volumeValue;
 
         // Indicator
-        Aindicator _MFI;
-        Aindicator _Volume;
+        private Aindicator _mfi;
+        private Aindicator _volumeIndicator;
 
         // The last value of the indicator
         private decimal _lastMFI;
         private decimal _lastVolume;
 
-        // Exit
-        private StrategyParameterDecimal TrailingValue;
+        // Exit setting
+        private StrategyParameterDecimal _trailingValue;
 
         public StrategyVolumeAndMFI(string name, StartProgram startProgram) : base(name, startProgram)
         {
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
 
-            // Basic setting
-            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
-            VolumeRegime = CreateParameter("Volume type", "Number of contracts", new[] { "Number of contracts", "Contract currency" }, "Base");
-            VolumeOnPosition = CreateParameter("Volume", 1, 1.0m, 50, 4, "Base");
-            Slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
-            StartTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
-            EndTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+            // Basic settings
+            _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+            _slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
+            _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+            _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
 
-            // Indicator setting
-            _MFIPeriod = CreateParameter("MFI Length", 3, 3, 48, 7, "Indicator");
-            MfiMinValue = CreateParameter("MfiMinValue", 1.0m, 3, 48, 7, "Indicator");
-            MfiMaxValue = CreateParameter("MfiMaxValue", 3.0m, 3, 48, 7, "Indicator");
-            VolumeValue = CreateParameter("VolumeValue", 30.0m, 3, 48, 7, "Indicator");
+            // GetVolume Settings
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
+
+            // Indicator settings
+            _mfiPeriod = CreateParameter("MFI Length", 3, 3, 48, 7, "Indicator");
+            _mfiMinValue = CreateParameter("MfiMinValue", 1.0m, 3, 48, 7, "Indicator");
+            _mfiMaxValue = CreateParameter("MfiMaxValue", 3.0m, 3, 48, 7, "Indicator");
+            _volumeValue = CreateParameter("VolumeValue", 30.0m, 3, 48, 7, "Indicator");
 
             // Create indicator MFI
-            _MFI = IndicatorsFactory.CreateIndicatorByName("MFI", name + "MFI", false);
-            _MFI = (Aindicator)_tab.CreateCandleIndicator(_MFI, "NewArea");
-            ((IndicatorParameterInt)_MFI.Parameters[0]).ValueInt = _MFIPeriod.ValueInt;
-            _MFI.Save();
+            _mfi = IndicatorsFactory.CreateIndicatorByName("MFI", name + "MFI", false);
+            _mfi = (Aindicator)_tab.CreateCandleIndicator(_mfi, "NewArea");
+            ((IndicatorParameterInt)_mfi.Parameters[0]).ValueInt = _mfiPeriod.ValueInt;
+            _mfi.Save();
 
             // Create indicator Volume
-            _Volume = IndicatorsFactory.CreateIndicatorByName("Volume", name + "Volume", false);
-            _Volume = (Aindicator)_tab.CreateCandleIndicator(_Volume, "NewArea0");
-            _Volume.Save();
+            _volumeIndicator = IndicatorsFactory.CreateIndicatorByName("Volume", name + "Volume", false);
+            _volumeIndicator = (Aindicator)_tab.CreateCandleIndicator(_volumeIndicator, "NewArea0");
+            _volumeIndicator.Save();
 
-            // Exit
-            TrailingValue = CreateParameter("Stop Value", 1.0m, 5, 200, 5, "Exit");
+            // Exit setting
+            _trailingValue = CreateParameter("Stop Value", 1.0m, 5, 200, 5, "Exit");
 
             // Subscribe to the indicator update event
             ParametrsChangeByUser += StrategyVolumeAndMFI_ParametrsChangeByUser; ;
@@ -95,21 +108,21 @@ namespace OsEngine.Robots.AO
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
 
             Description = "The trend robot on Strategy Volume And MFI. " +
-                "Buy " +
+                "Buy: " +
                 "1. The volume is higher than the VolumeValue value. " +
                 "2. The MFI is lower than the MfiMinValue value. " +
-                "Sell " +
+                "Sell: " +
                 "1. The volume is higher than the VolumeValue value. " +
                 "2. The MFI is higher than the MfiMaxValue value. " +
-                "Exit from buy trailing stop in % of the loy of the candle on which you entered. " +
-                "Exit from sell trailing stop in % of the high of the candle on which you entered.";
+                "Exit from buy: trailing stop in % of the loy of the candle on which you entered. " +
+                "Exit from sell: trailing stop in % of the high of the candle on which you entered.";
         }
 
         private void StrategyVolumeAndMFI_ParametrsChangeByUser()
         {
-            ((IndicatorParameterInt)_MFI.Parameters[0]).ValueInt = _MFIPeriod.ValueInt;
-            _MFI.Save();
-            _MFI.Reload();
+            ((IndicatorParameterInt)_mfi.Parameters[0]).ValueInt = _mfiPeriod.ValueInt;
+            _mfi.Save();
+            _mfi.Reload();
         }
 
         // The name of the robot in OsEngine
@@ -126,20 +139,20 @@ namespace OsEngine.Robots.AO
         private void _tab_CandleFinishedEvent(List<Candle> candles)
         {
             // If the robot is turned off, exit the event handler
-            if (Regime.ValueString == "Off")
+            if (_regime.ValueString == "Off")
             {
                 return;
             }
 
             // If there are not enough candles to build an indicator, we exit
-            if (candles.Count < _MFIPeriod.ValueInt)
+            if (candles.Count < _mfiPeriod.ValueInt)
             {
                 return;
             }
 
             // If the time does not match, we leave
-            if (StartTradeTime.Value > _tab.TimeServerCurrent ||
-                EndTradeTime.Value < _tab.TimeServerCurrent)
+            if (_startTradeTime.Value > _tab.TimeServerCurrent ||
+                _endTradeTime.Value < _tab.TimeServerCurrent)
             {
                 return;
             }
@@ -153,10 +166,11 @@ namespace OsEngine.Robots.AO
             }
 
             // If the position closing mode, then exit the method
-            if (Regime.ValueString == "OnlyClosePosition")
+            if (_regime.ValueString == "OnlyClosePosition")
             {
                 return;
             }
+
             // If there are no positions, then go to the position opening method
             if (openPositions == null || openPositions.Count == 0)
             {
@@ -168,8 +182,8 @@ namespace OsEngine.Robots.AO
         private void LogicOpenPosition(List<Candle> candles)
         {
             // The last value of the indicator
-            _lastMFI = _MFI.DataSeries[0].Last;
-            _lastVolume = _Volume.DataSeries[0].Last;
+            _lastMFI = _mfi.DataSeries[0].Last;
+            _lastVolume = _volumeIndicator.DataSeries[0].Last;
 
             List<Position> openPositions = _tab.PositionsOpenAll;
 
@@ -178,23 +192,23 @@ namespace OsEngine.Robots.AO
                 decimal lastPrice = candles[candles.Count - 1].Close;
 
                 // Slippage
-                decimal _slippage = Slippage.ValueDecimal * _tab.Securiti.PriceStep;
+                decimal _slippage = this._slippage.ValueDecimal * _tab.Securiti.PriceStep;
 
                 // Long
-                if (Regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
+                if (_regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
                 {
-                    if (_lastMFI < MfiMinValue.ValueDecimal && _lastVolume > VolumeValue.ValueDecimal)
+                    if (_lastMFI < _mfiMinValue.ValueDecimal && _lastVolume > _volumeValue.ValueDecimal)
                     {
-                        _tab.BuyAtLimit(GetVolume(), _tab.PriceBestAsk + _slippage);
+                        _tab.BuyAtLimit(GetVolume(_tab), _tab.PriceBestAsk + _slippage);
                     }
                 }
 
                 // Short
-                if (Regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
+                if (_regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
                 {
-                    if (_lastMFI > MfiMaxValue.ValueDecimal && _lastVolume > VolumeValue.ValueDecimal)
+                    if (_lastMFI > _mfiMaxValue.ValueDecimal && _lastVolume > _volumeValue.ValueDecimal)
                     {
-                        _tab.SellAtLimit(GetVolume(), _tab.PriceBestBid - _slippage);
+                        _tab.SellAtLimit(GetVolume(_tab), _tab.PriceBestBid - _slippage);
                     }
                 }
             }
@@ -216,44 +230,109 @@ namespace OsEngine.Robots.AO
                     continue;
                 }
 
-                if (pos.Direction == Side.Buy) // If the direction of the position is purchase
+                if (pos.Direction == Side.Buy) // If the direction of the position is long
                 {
                     decimal lov = candles[candles.Count - 1].Low;
-                    stopPrice = lov - lov * TrailingValue.ValueDecimal / 100;
+                    stopPrice = lov - lov * _trailingValue.ValueDecimal / 100;
                 }
-                else // If the direction of the position is sale
+                else // If the direction of the position is short
                 {
                     decimal high = candles[candles.Count - 1].High;
-                    stopPrice = high + high * TrailingValue.ValueDecimal / 100;
+                    stopPrice = high + high * _trailingValue.ValueDecimal / 100;
                 }
                 _tab.CloseAtTrailingStop(pos, stopPrice, stopPrice);
             }
         }
 
         // Method for calculating the volume of entry into a position
-        private decimal GetVolume()
+        private decimal GetVolume(BotTabSimple tab)
         {
             decimal volume = 0;
 
-            if (VolumeRegime.ValueString == "Contract currency")
+            if (_volumeType.ValueString == "Contracts")
             {
-                decimal contractPrice = _tab.PriceBestAsk;
-                volume = VolumeOnPosition.ValueDecimal / contractPrice;
+                volume = _volume.ValueDecimal;
             }
-            else if (VolumeRegime.ValueString == "Number of contracts")
+            else if (_volumeType.ValueString == "Contract currency")
             {
-                volume = VolumeOnPosition.ValueDecimal;
+                decimal contractPrice = tab.PriceBestAsk;
+                volume = _volume.ValueDecimal / contractPrice;
+
+                if (StartProgram == StartProgram.IsOsTrader)
+                {
+                    IServerPermission serverPermission = ServerMaster.GetServerPermission(tab.Connector.ServerType);
+
+                    if (serverPermission != null &&
+                        serverPermission.IsUseLotToCalculateProfit &&
+                    tab.Security.Lot != 0 &&
+                        tab.Security.Lot > 1)
+                    {
+                        volume = _volume.ValueDecimal / (contractPrice * tab.Security.Lot);
+                    }
+
+                    volume = Math.Round(volume, tab.Security.DecimalsVolume);
+                }
+                else // Tester or Optimizer
+                {
+                    volume = Math.Round(volume, 6);
+                }
+            }
+            else if (_volumeType.ValueString == "Deposit percent")
+            {
+                Portfolio myPortfolio = tab.Portfolio;
+
+                if (myPortfolio == null)
+                {
+                    return 0;
+                }
+
+                decimal portfolioPrimeAsset = 0;
+
+                if (_tradeAssetInPortfolio.ValueString == "Prime")
+                {
+                    portfolioPrimeAsset = myPortfolio.ValueCurrent;
+                }
+                else
+                {
+                    List<PositionOnBoard> positionOnBoard = myPortfolio.GetPositionOnBoard();
+
+                    if (positionOnBoard == null)
+                    {
+                        return 0;
+                    }
+
+                    for (int i = 0; i < positionOnBoard.Count; i++)
+                    {
+                        if (positionOnBoard[i].SecurityNameCode == _tradeAssetInPortfolio.ValueString)
+                        {
+                            portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
+                            break;
+                        }
+                    }
+                }
+
+                if (portfolioPrimeAsset == 0)
+                {
+                    SendNewLogMessage("Can`t found portfolio " + _tradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
+                    return 0;
+                }
+
+                decimal moneyOnPosition = portfolioPrimeAsset * (_volume.ValueDecimal / 100);
+
+                decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
+
+                if (tab.StartProgram == StartProgram.IsOsTrader)
+                {
+                    qty = Math.Round(qty, tab.Security.DecimalsVolume);
+                }
+                else
+                {
+                    qty = Math.Round(qty, 7);
+                }
+
+                return qty;
             }
 
-            // If the robot is running in the tester
-            if (StartProgram == StartProgram.IsTester)
-            {
-                volume = Math.Round(volume, 6);
-            }
-            else
-            {
-                volume = Math.Round(volume, _tab.Securiti.DecimalsVolume);
-            }
             return volume;
         }
     }
