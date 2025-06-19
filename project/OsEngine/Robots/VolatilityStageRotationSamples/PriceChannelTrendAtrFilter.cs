55d3ed9fc4de002354aaaff7dbea03103276afcd
JChinaM <jchinam@mail.ru>
2025-06-19 17:19:52 +0300

    PriceChannelTrendAtrFilter Refactoring bot

--- project/OsEngine/Robots/VolatilityStageRotationSamples/PriceChannelTrendAtrFilter.cs
+++ project/OsEngine/Robots/VolatilityStageRotationSamples/PriceChannelTrendAtrFilter.cs
@@ -3,44 +3,62 @@
  * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
 */
 
-using System.Collections.Generic;
+using OsEngine.Charts.CandleChart.Indicators;
 using OsEngine.Entity;
 using OsEngine.Indicators;
-using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Market;
+using OsEngine.Market.Servers;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
-using OsEngine.Market.Servers;
-using OsEngine.Market;
+using OsEngine.OsTrader.Panels.Tab;
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+/* Description
+trading robot for osengine
+
+The trend robot on PriceChannel and AtrFilter.
+
+Buy:
+1.The price has broken above the upper line of the Price Channel
+2.ATR has grown compared to AtrGrowLookBack candles ago.
+Sell:
+1.The price has broken below the lower line of the Price Channel
+2.ATR has grown compared to AtrGrowLookBack candles ago.
+
+Exit for long:
+Exit using a trailing stop set to the lower line of the Price Channel.
+Exit for short:
+Exit using a trailing stop set to the upper line of the Price Channel.
+*/
 
 namespace OsEngine.Robots.VolatilityStageRotationSamples
 {
-    [Bot("PriceChannelTrendAtrFilter")]
+    [Bot("PriceChannelTrendAtrFilter")] // We create an attribute so that we don't write anything to the BotFactory
     public class PriceChannelTrendAtrFilter : BotPanel
     {
         private BotTabSimple _tab;
 
+        // Indicator
         private Aindicator _pc;
-
         private Aindicator _atr;
 
-        public StrategyParameterString Regime;
+        // Basic setting
+        private StrategyParameterString _regime;
 
-        public StrategyParameterInt PriceChannelLength;
+        // Indicator settings
+        private StrategyParameterInt _priceChannelLength;
+        private StrategyParameterInt _atrLength;
+        private StrategyParameterBool _atrFilterIsOn;
+        private StrategyParameterDecimal _atrGrowPercent;
+        private StrategyParameterInt _atrGrowLookBack;
 
-        public StrategyParameterInt AtrLength;
-
-        public StrategyParameterBool AtrFilterIsOn;
-
-        public StrategyParameterDecimal AtrGrowPercent;
-
-        public StrategyParameterInt AtrGrowLookBack;
-
-        public StrategyParameterString VolumeType;
-
-        public StrategyParameterDecimal Volume;
-
-        public StrategyParameterString TradeAssetInPortfolio;
+        // GetVolume settings
+        private StrategyParameterString _volumeType;
+        private StrategyParameterDecimal _volume;
+        private StrategyParameterString _tradeAssetInPortfolio;
 
         public PriceChannelTrendAtrFilter(string name, StartProgram startProgram)
             : base(name, startProgram)
@@ -48,78 +66,100 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
 
-            Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
+            // Basic setting
+            _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
 
-            VolumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
-            Volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
-            TradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
+            // GetVolume settings
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
 
-            PriceChannelLength = CreateParameter("Price channel length", 50, 10, 80, 3);
-            AtrLength = CreateParameter("Atr length", 25, 10, 80, 3);
+            // Indicator settings
+            _priceChannelLength = CreateParameter("Price channel length", 50, 10, 80, 3);
+            _atrLength = CreateParameter("Atr length", 25, 10, 80, 3);
+            _atrFilterIsOn = CreateParameter("Atr filter is on", false);
+            _atrGrowPercent = CreateParameter("Atr grow percent", 3, 1.0m, 50, 4);
+            _atrGrowLookBack = CreateParameter("Atr grow look back", 20, 1, 50, 4);
 
-            AtrFilterIsOn = CreateParameter("Atr filter is on", false);
-            AtrGrowPercent = CreateParameter("Atr grow percent", 3, 1.0m, 50, 4);
-            AtrGrowLookBack = CreateParameter("Atr grow look back", 20, 1, 50, 4);
-
+            // Create indicator PriceChannel
             _pc = IndicatorsFactory.CreateIndicatorByName("PriceChannel", name + "PriceChannel", false);
             _pc = (Aindicator)_tab.CreateCandleIndicator(_pc, "Prime");
-            _pc.ParametersDigit[0].Value = PriceChannelLength.ValueInt;
-            _pc.ParametersDigit[1].Value = PriceChannelLength.ValueInt;
+            _pc.ParametersDigit[0].Value = _priceChannelLength.ValueInt;
+            _pc.ParametersDigit[1].Value = _priceChannelLength.ValueInt;
             _pc.Save();
 
+            // Create indicator ATR
             _atr = IndicatorsFactory.CreateIndicatorByName("ATR", name + "Atr", false);
             _atr = (Aindicator)_tab.CreateCandleIndicator(_atr, "AtrArea");
-            _atr.ParametersDigit[0].Value = AtrLength.ValueInt;
+            _atr.ParametersDigit[0].Value = _atrLength.ValueInt;
 
+            // Subscribe to the candle finished event
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
 
+            // Subscribe to the indicator update event
             ParametrsChangeByUser += Event_ParametrsChangeByUser;
+
+            Description = "The trend robot on PriceChannel and AtrFilter." +
+                "Buy:" +
+                "1.The price has broken above the upper line of the Price Channel " +
+                "2.ATR has grown compared to AtrGrowLookBack candles ago. " +
+                "Sell: " +
+                "1.The price has broken below the lower line of the Price Channel " +
+                "2.ATR has grown compared to AtrGrowLookBack candles ago. " +
+                "Exit for long: " +
+                "Exit using a trailing stop set to the lower line of the Price Channel. " +
+                "Exit for short: " +
+                "Exit using a trailing stop set to the upper line of the Price Channel.";
         }
 
         void Event_ParametrsChangeByUser()
         {
-            if (PriceChannelLength.ValueInt != _pc.ParametersDigit[0].Value ||
-                PriceChannelLength.ValueInt != _pc.ParametersDigit[1].Value)
+            if (_priceChannelLength.ValueInt != _pc.ParametersDigit[0].Value ||
+                _priceChannelLength.ValueInt != _pc.ParametersDigit[1].Value)
             {
-                _pc.ParametersDigit[0].Value = PriceChannelLength.ValueInt;
-                _pc.ParametersDigit[1].Value = PriceChannelLength.ValueInt;
+                _pc.ParametersDigit[0].Value = _priceChannelLength.ValueInt;
+                _pc.ParametersDigit[1].Value = _priceChannelLength.ValueInt;
                 _pc.Reload();
                 _pc.Save();
             }
 
-            if(_atr.ParametersDigit[0].Value != AtrLength.ValueInt)
+            if(_atr.ParametersDigit[0].Value != _atrLength.ValueInt)
             {
-                _atr.ParametersDigit[0].Value = AtrLength.ValueInt;
+                _atr.ParametersDigit[0].Value = _atrLength.ValueInt;
                 _atr.Reload();
                 _atr.Save();
             }
         }
 
+        // The name of the robot in OsEngine
         public override string GetNameStrategyType()
         {
             return "PriceChannelTrendAtrFilter";
         }
 
+        // Show settings GUI
         public override void ShowIndividualSettingsDialog()
         {
 
         }
 
         // logic
-
         private void _tab_CandleFinishedEvent(List<Candle> candles)
         {
-            if (Regime.ValueString == "Off")
+            // If the robot is turned off, exit the event handler
+            if (_regime.ValueString == "Off")
             {
                 return;
             }
 
+            // If value indicator equals null, we exit
             if (_pc.DataSeries[0].Values == null 
                 || _pc.DataSeries[1].Values == null)
             {
                 return;
             }
 
+            // If there are not enough candles to build an indicator, we exit
             if (_pc.DataSeries[0].Values.Count < _pc.ParametersDigit[0].Value + 2 
                 || _pc.DataSeries[1].Values.Count < _pc.ParametersDigit[1].Value + 2)
             {
@@ -130,7 +170,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
             if (openPositions == null || openPositions.Count == 0)
             {
-                if (Regime.ValueString == "OnlyClosePosition")
+                if (_regime.ValueString == "OnlyClosePosition")
                 {
                     return;
                 }
@@ -142,8 +182,10 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
             }
         }
 
+        // Opening logic
         private void LogicOpenPosition(List<Candle> candles)
         {
+            // The last value of the indicator
             decimal lastPrice = candles[candles.Count - 1].Close;
             decimal lastPcUp = _pc.DataSeries[0].Values[_pc.DataSeries[0].Values.Count - 2];
             decimal lastPcDown = _pc.DataSeries[1].Values[_pc.DataSeries[1].Values.Count - 2];
@@ -155,18 +197,17 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
             }
 
             if (lastPrice > lastPcUp
-                && Regime.ValueString != "OnlyShort")
+                && _regime.ValueString != "OnlyShort") // If the mode is not only short, then we enter long
             {
-                if (AtrFilterIsOn.ValueBool == true)
+                if (_atrFilterIsOn.ValueBool == true)
                 {
-                    if (_atr.DataSeries[0].Values.Count - 1 - AtrGrowLookBack.ValueInt <= 0)
+                    if (_atr.DataSeries[0].Values.Count - 1 - _atrGrowLookBack.ValueInt <= 0)
                     {
                         return;
                     }
 
                     decimal atrLast = _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1];
-                    decimal atrLookBack =
-                        _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1 - AtrGrowLookBack.ValueInt];
+                    decimal atrLookBack = _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1 - _atrGrowLookBack.ValueInt];
 
                     if (atrLast == 0
                         || atrLookBack == 0)
@@ -176,7 +217,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
                     decimal atrGrowPercent = atrLast / (atrLookBack / 100) - 100;
 
-                    if (atrGrowPercent < AtrGrowPercent.ValueDecimal)
+                    if (atrGrowPercent < _atrGrowPercent.ValueDecimal)
                     {
                         return;
                     }
@@ -185,18 +226,18 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
                 _tab.BuyAtMarket(GetVolume(_tab));
             }
             if (lastPrice < lastPcDown
-                && Regime.ValueString != "OnlyLong")
+                && _regime.ValueString != "OnlyLong") // If the mode is not only long, then we enter short
             {
-                if (AtrFilterIsOn.ValueBool == true)
+                if (_atrFilterIsOn.ValueBool == true)
                 {
-                    if (_atr.DataSeries[0].Values.Count - 1 - AtrGrowLookBack.ValueInt <= 0)
+                    if (_atr.DataSeries[0].Values.Count - 1 - _atrGrowLookBack.ValueInt <= 0)
                     {
                         return;
                     }
 
                     decimal atrLast = _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1];
                     decimal atrLookBack =
-                        _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1 - AtrGrowLookBack.ValueInt];
+                        _atr.DataSeries[0].Values[_atr.DataSeries[0].Values.Count - 1 - _atrGrowLookBack.ValueInt];
 
                     if (atrLast == 0
                         || atrLookBack == 0)
@@ -206,7 +247,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
                     decimal atrGrowPercent = atrLast / (atrLookBack / 100) - 100;
 
-                    if (atrGrowPercent < AtrGrowPercent.ValueDecimal)
+                    if (atrGrowPercent < _atrGrowPercent.ValueDecimal)
                     {
                         return;
                     }
@@ -216,33 +257,36 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
             }
         }
 
+        // Logic close position
         private void LogicClosePosition(List<Candle> candles, Position position)
         {
+            // The last value of the indicator
             decimal lastPcUp = _pc.DataSeries[0].Values[_pc.DataSeries[0].Values.Count - 1];
             decimal lastPcDown = _pc.DataSeries[1].Values[_pc.DataSeries[1].Values.Count - 1];
 
-            if(position.Direction == Side.Buy)
+            if(position.Direction == Side.Buy) // If the direction of the position is long
             {
                 _tab.CloseAtTrailingStopMarket(position, lastPcDown);
             }
-            if (position.Direction == Side.Sell)
+            if (position.Direction == Side.Sell) // If the direction of the position is short
             {
                 _tab.CloseAtTrailingStopMarket(position, lastPcUp);
             }
         }
 
+        // Method for calculating the volume of entry into a position
         private decimal GetVolume(BotTabSimple tab)
         {
             decimal volume = 0;
 
-            if (VolumeType.ValueString == "Contracts")
+            if (_volumeType.ValueString == "Contracts")
             {
-                volume = Volume.ValueDecimal;
+                volume = _volume.ValueDecimal;
             }
-            else if (VolumeType.ValueString == "Contract currency")
+            else if (_volumeType.ValueString == "Contract currency")
             {
                 decimal contractPrice = tab.PriceBestAsk;
-                volume = Volume.ValueDecimal / contractPrice;
+                volume = _volume.ValueDecimal / contractPrice;
 
                 if (StartProgram == StartProgram.IsOsTrader)
                 {
@@ -253,7 +297,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
                     tab.Security.Lot != 0 &&
                         tab.Security.Lot > 1)
                     {
-                        volume = Volume.ValueDecimal / (contractPrice * tab.Security.Lot);
+                        volume = _volume.ValueDecimal / (contractPrice * tab.Security.Lot);
                     }
 
                     volume = Math.Round(volume, tab.Security.DecimalsVolume);
@@ -263,7 +307,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
                     volume = Math.Round(volume, 6);
                 }
             }
-            else if (VolumeType.ValueString == "Deposit percent")
+            else if (_volumeType.ValueString == "Deposit percent")
             {
                 Portfolio myPortfolio = tab.Portfolio;
 
@@ -274,7 +318,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
                 decimal portfolioPrimeAsset = 0;
 
-                if (TradeAssetInPortfolio.ValueString == "Prime")
+                if (_tradeAssetInPortfolio.ValueString == "Prime")
                 {
                     portfolioPrimeAsset = myPortfolio.ValueCurrent;
                 }
@@ -289,7 +333,7 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
                     for (int i = 0; i < positionOnBoard.Count; i++)
                     {
-                        if (positionOnBoard[i].SecurityNameCode == TradeAssetInPortfolio.ValueString)
+                        if (positionOnBoard[i].SecurityNameCode == _tradeAssetInPortfolio.ValueString)
                         {
                             portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
                             break;
@@ -299,11 +343,11 @@ namespace OsEngine.Robots.VolatilityStageRotationSamples
 
                 if (portfolioPrimeAsset == 0)
                 {
-                    SendNewLogMessage("Can`t found portfolio " + TradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
+                    SendNewLogMessage("Can`t found portfolio " + _tradeAssetInPortfolio.ValueString, Logging.LogMessageType.Error);
                     return 0;
                 }
 
-                decimal moneyOnPosition = portfolioPrimeAsset * (Volume.ValueDecimal / 100);
+                decimal moneyOnPosition = portfolioPrimeAsset * (_volume.ValueDecimal / 100);
 
                 decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
 
