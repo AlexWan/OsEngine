89296469b9827de3c2fdd5e6e83500ceedbb4878
JChinaM <137157160+JChinaM@users.noreply.github.com>
2025-06-26 15:42:20 +0300

    Update BbPowerTrade.cs

--- project/OsEngine/Robots/OnScriptIndicators/BbPowerTrade.cs
+++ project/OsEngine/Robots/OnScriptIndicators/BbPowerTrade.cs
@@ -1,6 +1,6 @@
 /*
  * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
- * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+ * Р’Р°С€Рё РїСЂР°РІР° РЅР° РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ РєРѕРґР° СЂРµРіСѓР»РёСЂСѓСЋС‚СЃСЏ РґР°РЅРЅРѕР№ Р»РёС†РµРЅР·РёРµР№ http://o-s-a.net/doc/license_simple_engine.pdf
 */
 
 using OsEngine.Entity;
