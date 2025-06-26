4c6ac9c75f0962bdd14e52dd8c38dcc06401f769
JChinaM <137157160+JChinaM@users.noreply.github.com>
2025-06-26 15:51:02 +0300

    Update TimeOfDayBot.cs

--- project/OsEngine/Robots/OnScriptIndicators/TimeOfDayBot.cs
+++ project/OsEngine/Robots/OnScriptIndicators/TimeOfDayBot.cs
@@ -1,3 +1,8 @@
+/*
+ * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
 ﻿using OsEngine.Charts.CandleChart.Indicators;
 using OsEngine.Entity;
 using OsEngine.Indicators;
