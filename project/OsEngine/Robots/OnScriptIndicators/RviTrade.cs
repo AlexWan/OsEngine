5f084e17d0f67b672c2629658bbee64794c58784
JChinaM <jchinam@mail.ru>
2025-06-26 15:37:49 +0300

    RviTrade Refactoring bot

--- project/OsEngine/Robots/OnScriptIndicators/RviTrade.cs
+++ project/OsEngine/Robots/OnScriptIndicators/RviTrade.cs
@@ -1,35 +1,78 @@
-using System.Collections.Generic;
+using OsEngine.Charts.CandleChart.Indicators;
 using OsEngine.Entity;
 using OsEngine.Indicators;
-using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Market;
+using OsEngine.Market.Servers;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
+using OsEngine.OsTrader.Panels.Tab;
+using System;
+using System.Collections.Generic;
 
-/// <summary>
-/// Trend strategy at the intersection of the indicator RVI
-/// Трендовая стратегия на пересечение индикатора RVI
-/// </summary>
-[Bot("RviTrade")]
+/* Description
+trading robot for osengine
+
+Trend strategy at the intersection of the indicator RVI.
+
+Buy: lastRviDown < 0 and lastRviUp > lastRviDown.
+
+Sell: lastRviDown > 0 and lastRviUp < lastRviDown.
+
+Exit: By return signal.
+*/
+
+[Bot("RviTrade")] // We create an attribute so that we don't write anything to the BotFactory
 public class RviTrade : BotPanel
 {
-    public RviTrade(string name, StartProgram startProgram)
-        : base(name, startProgram)
+    private BotTabSimple _tab;
+
+    // Indicator
+    private Aindicator _rvi;
+
+    // Basic settings
+    private StrategyParameterString _regime;
+    private StrategyParameterInt _slippage;
+
+    // GetVolume settings
+    private StrategyParameterString _volumeType;
+    private StrategyParameterDecimal _volume;
+    private StrategyParameterString _tradeAssetInPortfolio;
+
+    // Indicator setting
+    private StrategyParameterInt _rviLength;
+
+    // The last value of the indicator and price
+    private decimal _lastPrice;
+    private decimal _lastRviUp;
+    private decimal _lastRviDown;
+
+    public RviTrade(string name, StartProgram startProgram) : base(name, startProgram)
     {
         TabCreate(BotTabType.Simple);
         _tab = TabsSimple[0];
 
-        Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
-        Volume = CreateParameter("Volume", 3, 1.0m, 50, 4);
-        Slippage = CreateParameter("Slippage", 0, 0, 20, 1);
+        // Basic settings
+        _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
+        _slippage = CreateParameter("Slippage", 0, 0, 20, 1);
 
-        RviLength = CreateParameter("Rvi Length", 10, 10, 80, 3);
+        // GetVolume settings
+        _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
+        _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
+        _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
 
+        // Indicator setting
+        _rviLength = CreateParameter("Rvi Length", 10, 10, 80, 3);
+
+        // Create indicator Rvi
         _rvi = IndicatorsFactory.CreateIndicatorByName("RVI",name + "RviArea", false);
         _rvi = (Aindicator)_tab.CreateCandleIndicator(_rvi, "MacdArea");
-        _rvi.ParametersDigit[0].Value = RviLength.ValueInt;
+        _rvi.ParametersDigit[0].Value = _rviLength.ValueInt;
         _rvi.Save();
 
+        // Subscribe to the candle finished event
         _tab.CandleFinishedEvent += Strateg_CandleFinishedEvent;
+
+        // Subscribe to the indicator update event
         ParametrsChangeByUser += RviTrade_ParametrsChangeByUser;
 
         Description = "Trend strategy at the intersection of the indicator RVI" +
@@ -40,79 +83,29 @@ public class RviTrade : BotPanel
 
     void RviTrade_ParametrsChangeByUser()
     {
-        if (RviLength.ValueInt != _rvi.ParametersDigit[0].Value)
+        if (_rviLength.ValueInt != _rvi.ParametersDigit[0].Value)
         {
-            _rvi.ParametersDigit[0].Value = RviLength.ValueInt;
+            _rvi.ParametersDigit[0].Value = _rviLength.ValueInt;
             _rvi.Reload();
         }
     }
 
-    /// <summary>
-    /// strategy name
-    /// взять уникальное имя
-    /// </summary>
+    // The name of the robot in OsEngine
     public override string GetNameStrategyType()
     {
         return "RviTrade";
     }
 
-    /// <summary>
-    /// settings GUI
-    /// показать окно настроек
-    /// </summary>
+    // Show settings GUI
     public override void ShowIndividualSettingsDialog()
     {
+
     }
 
-    /// <summary>
-    /// tab to trade
-    /// вкладка для торговли
-    /// </summary>
-    private BotTabSimple _tab;
-
-    //indicators индикаторы
-
-    private Aindicator _rvi;
-
-    //settings настройки публичные
-
-    /// <summary>
-    /// slippage
-    /// проскальзывание
-    /// </summary>
-    public StrategyParameterInt Slippage;
-
-    /// <summary>
-    /// volume to inter
-    /// фиксированный объем для входа
-    /// </summary>
-    public StrategyParameterDecimal Volume;
-
-    /// <summary>
-    /// regime
-    /// режим работы
-    /// </summary>
-    public StrategyParameterString Regime;
-
-    /// <summary>
-    /// indicator length
-    /// длинна индикатора
-    /// </summary>
-    public StrategyParameterInt RviLength;
-
-    private decimal _lastPrice;
-    private decimal _lastRviUp;
-    private decimal _lastRviDown;
-
-    // logic / логика
-
-    /// <summary>
-    /// candle finished event
-    /// событие завершения свечи
-    /// </summary>
+    // Logic
     private void Strateg_CandleFinishedEvent(List<Candle> candles)
     {
-        if (Regime.ValueString == "Off")
+        if (_regime.ValueString == "Off")
         {
             return;
         }
@@ -137,48 +130,43 @@ public class RviTrade : BotPanel
             }
         }
 
-        if (Regime.ValueString == "OnlyClosePosition")
+        if (_regime.ValueString == "OnlyClosePosition")
         {
             return;
         }
+
         if (openPositions == null || openPositions.Count == 0)
         {
             LogicOpenPosition(candles, openPositions);
         }
     }
 
-    /// <summary>
-    /// open position logic
-    /// логика открытия позиции
-    /// </summary>
+    // Open position logic
     private void LogicOpenPosition(List<Candle> candles, List<Position> position)
     {
-        if (_lastRviDown < 0 && _lastRviUp > _lastRviDown && Regime.ValueString != "OnlyShort")
+        if (_lastRviDown < 0 && _lastRviUp > _lastRviDown && _regime.ValueString != "OnlyShort")
         {
-            _tab.BuyAtLimit(Volume.ValueDecimal, _lastPrice + Slippage.ValueInt * _tab.Security.PriceStep);
+            _tab.BuyAtLimit(GetVolume(_tab), _lastPrice + _slippage.ValueInt * _tab.Security.PriceStep);
         }
 
-        if (_lastRviDown > 0 && _lastRviUp < _lastRviDown && Regime.ValueString != "OnlyLong")
+        if (_lastRviDown > 0 && _lastRviUp < _lastRviDown && _regime.ValueString != "OnlyLong")
         {
-            _tab.SellAtLimit(Volume.ValueDecimal, _lastPrice - Slippage.ValueInt * _tab.Security.PriceStep);
+            _tab.SellAtLimit(GetVolume(_tab), _lastPrice - _slippage.ValueInt * _tab.Security.PriceStep);
         }
     }
 
-    /// <summary>
-    /// logic close position
-    /// логика зыкрытия позиции
-    /// </summary>
+    // Logic close position
     private void LogicClosePosition(List<Candle> candles, Position position)
     {
         if (position.Direction == Side.Buy && position.State == PositionStateType.Open)
         {
             if (_lastRviDown > 0 && _lastRviUp < _lastRviDown)
             {
-                _tab.CloseAtLimit(position, _lastPrice - Slippage.ValueInt, position.OpenVolume);
+                _tab.CloseAtLimit(position, _lastPrice - _slippage.ValueInt, position.OpenVolume);
 
-                if (Regime.ValueString != "OnlyLong" && Regime.ValueString != "OnlyClosePosition")
+                if (_regime.ValueString != "OnlyLong" && _regime.ValueString != "OnlyClosePosition")
                 {
-                    _tab.SellAtLimit(Volume.ValueDecimal, _lastPrice - Slippage.ValueInt * _tab.Security.PriceStep);
+                    _tab.SellAtLimit(GetVolume(_tab), _lastPrice - _slippage.ValueInt * _tab.Security.PriceStep);
                 }
             }
         }
@@ -187,13 +175,105 @@ public class RviTrade : BotPanel
         {
             if (_lastRviDown < 0 && _lastRviUp > _lastRviDown)
             {
-                _tab.CloseAtLimit(position, _lastPrice + Slippage.ValueInt, position.OpenVolume);
+                _tab.CloseAtLimit(position, _lastPrice + _slippage.ValueInt, position.OpenVolume);
 
-                if (Regime.ValueString != "OnlyShort" && Regime.ValueString != "OnlyClosePosition")
+                if (_regime.ValueString != "OnlyShort" && _regime.ValueString != "OnlyClosePosition")
                 {
-                    _tab.BuyAtLimit(Volume.ValueDecimal, _lastPrice + Slippage.ValueInt * _tab.Security.PriceStep);
+                    _tab.BuyAtLimit(GetVolume(_tab), _lastPrice + _slippage.ValueInt * _tab.Security.PriceStep);
                 }
             }
         }
+    }
+
+    // Method for calculating the volume of entry into a position
+    private decimal GetVolume(BotTabSimple tab)
+    {
+        decimal volume = 0;
+
+        if (_volumeType.ValueString == "Contracts")
+        {
+            volume = _volume.ValueDecimal;
+        }
+        else if (_volumeType.ValueString == "Contract currency")
+        {
+            decimal contractPrice = tab.PriceBestAsk;
+            volume = _volume.ValueDecimal / contractPrice;
+
+            if (StartProgram == StartProgram.IsOsTrader)
+            {
+                IServerPermission serverPermission = ServerMaster.GetServerPermission(tab.Connector.ServerType);
+
+                if (serverPermission != null &&
+                    serverPermission.IsUseLotToCalculateProfit &&
+                tab.Security.Lot != 0 &&
+                    tab.Security.Lot > 1)
+                {
+                    volume = _volume.ValueDecimal / (contractPrice * tab.Security.Lot);
+                }
+
+                volume = Math.Round(volume, tab.Security.DecimalsVolume);
+            }
+            else // Tester or Optimizer
+            {
+                volume = Math.Round(volume, 6);
+            }
+        }
+        else if (_volumeType.ValueString == "Deposit percent")
+        {
+            Portfolio myPortfolio = tab.Portfolio;
+
+            if (myPortfolio == null)
+            {
+                return 0;
+            }
+
+            decimal portfolioPrimeAsset = 0;
+
+            if (_tradeAssetInPortfolio.ValueString == "Prime")
+            {
+                portfolioPrimeAsset = myPortfolio.ValueCurrent;
+            }
+            else
+            {
+                List<PositionOnBoard> positionOnBoard = myPortfolio.GetPositionOnBoard();
+
+                if (positionOnBoard == null)
+                {
+                    return 0;
+                }
+
+                for (int i = 0; i < positionOnBoard.Count; i++)
+                {
+                    if (positionOnBoard[i].SecurityNameCode == _tradeAssetInPortfolio.ValueString)
+                    {
+                        portfolioPrimeAsset = positionOnBoard[i].ValueCurrent;
+                        break;
+                    }
+                }
+            }
+
+            if (portfolioPrimeAsset == 0)
+            {
+                SendNewLogMessage("Can`t found portfolio " + _tradeAssetInPortfolio.ValueString, OsEngine.Logging.LogMessageType.Error);
+                return 0;
+            }
+
+            decimal moneyOnPosition = portfolioPrimeAsset * (_volume.ValueDecimal / 100);
+
+            decimal qty = moneyOnPosition / tab.PriceBestAsk / tab.Security.Lot;
+
+            if (tab.StartProgram == StartProgram.IsOsTrader)
+            {
+                qty = Math.Round(qty, tab.Security.DecimalsVolume);
+            }
+            else
+            {
+                qty = Math.Round(qty, 7);
+            }
+
+            return qty;
+        }
+
+        return volume;
     }
 }
