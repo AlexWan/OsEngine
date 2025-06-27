c0a7882e4a05b68ee8f0581d7f836acc521f225c
TylerDerden17 <romamiro5hnichenko@yandex.ru>
2025-06-27 10:12:47 +0300

    StrategyEomWatcherAndEnvelopes.cs, StrategyLevermor.cs refactoring

--- project/OsEngine/bin/Debug/Custom/Robots/StrategyLevermor.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/StrategyLevermor.cs
@@ -3,104 +3,157 @@
  * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
 */
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using OsEngine.Entity;
 using OsEngine.Indicators;
-using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.Logging;
 using OsEngine.OsTrader.Panels;
-using System;
-using OsEngine.Market.Servers;
+using OsEngine.OsTrader.Panels.Tab;
+using OsEngine.OsTrader.Panels.Attributes;
 using OsEngine.Market;
+using OsEngine.Market.Servers;
 
+/* Description
+trading robot for osengine
+
+Trend strategy with pyramiding and optional exit by trailing stop or SMA.
+
+Buy:
+1. If the price is above the moving average (lastPrice >= SMA):
+   - If dont have position:
+     BuyAtStop at (upper channel border + slippage).
+   - If a long position exists and price > lastEntry + (lastEntry * PercentDopBuy/100):
+     BuyAtLimit (pyramiding).
+
+Sell:
+1. If the price is below the moving average (lastPrice <= SMA):
+   - If dont have position:
+     SellAtStop at (lower channel border - slippage).
+   - If a short position exists and price < lastEntry - (lastEntry * PercentDopSell/100):
+     SellAtLimit (pyramiding).
+
+Exit from a long position:
+1. Trailing stop at (close - close * TralingStopLength / 100), or
+2. Market exit if Close < SMA (if ExitType = "Sma").
+
+Exit from the short position:
+1. Trailing stop at (close + close * TralingStopLength / 100), or
+2. Market exit if Close > SMA (if ExitType = "Sma").
+*/
+
+[Bot("StrategyLevermor")]
 public class StrategyLevermor : BotPanel
 {
+    // Reference to the main trading tab
     private BotTabSimple _tab;
-    
+
     // Basic settings
-    public StrategyParameterInt Slippage;
-    public StrategyParameterString Regime;
-    public StrategyParameterInt MaximumPosition;
-    public StrategyParameterDecimal PersentDopBuy;
-    public StrategyParameterDecimal PersentDopSell;
-    public StrategyParameterInt ChannelLength;
-    public StrategyParameterInt SmaLength; 
+    private StrategyParameterInt _slippage;
+    private StrategyParameterString _regime;
+    private StrategyParameterInt _maximumPosition;
+    private StrategyParameterDecimal _percentDopBuy;
+    private StrategyParameterDecimal _percentDopSell;
+    private StrategyParameterTimeOfDay _startTradeTime;
+    private StrategyParameterTimeOfDay _endTradeTime;
+
 
     // GetVolume Settings
     private StrategyParameterString _volumeType;
     private StrategyParameterDecimal _volume;
     private StrategyParameterString _tradeAssetInPortfolio;
-    
-    // Indicator
+
+    // Indicators
     private Aindicator _sma;
     private Aindicator _channel;
 
+    //indicators settings
+    private StrategyParameterInt _channelLength;
+    private StrategyParameterInt _smaLength;
+
     // Exit settings
-    public StrategyParameterDecimal TralingStopLength;
-    public StrategyParameterString ExitType;
+    private StrategyParameterDecimal TralingStopLength;
+    private StrategyParameterString ExitType;
 
     public StrategyLevermor(string name, StartProgram startProgram) : base(name, startProgram)
     {
+        // Create and assign the main trading tab
         TabCreate(BotTabType.Simple);
         _tab = TabsSimple[0];
-        
+
         // Basic settings
-        Regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" });
-        ChannelLength = CreateParameter("ChannelLength", 10, 10, 400, 10);
-        SmaLength = CreateParameter("SmaLength", 10, 5, 150, 2);
-        MaximumPosition = CreateParameter("MaxPosition", 5, 1, 20, 3);
-        Slippage = CreateParameter("Slipage", 0, 0, 20, 1);
-        PersentDopBuy = CreateParameter("PersentDopBuy", 0.5m, 0.1m, 2, 0.1m);
-        PersentDopSell = CreateParameter("PersentDopSell", 0.5m, 0.1m, 2, 0.1m);
-        
+        _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
+        _maximumPosition = CreateParameter("MaxPosition", 5, 1, 20, 3, "Base");
+        _slippage = CreateParameter("Slippage", 0, 0, 20, 1, "Base");
+        _percentDopBuy = CreateParameter("PersentDopBuy", 0.5m, 0.1m, 2, 0.1m, "Base");
+        _percentDopSell = CreateParameter("PersentDopSell", 0.5m, 0.1m, 2, 0.1m, "Base");
+        _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
+        _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+
+
         // GetVolume Settings
         _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
         _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
         _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
 
+        // Indicators settings
+        _channelLength = CreateParameter("ChannelLength", 10, 10, 400, 10, "Indicators");
+        _smaLength = CreateParameter("SmaLength", 10, 5, 150, 2, "Indicators");
+
         // Exit settings
-        TralingStopLength = CreateParameter("TralingStopLength", 3, 3, 8, 0.5m);
-        ExitType = CreateParameter("ExitType", "Traling", new[] { "Traling", "Sma" });
+        TralingStopLength = CreateParameter("TralingStopLength", 3, 3, 8, 0.5m, "Exit");
+        ExitType = CreateParameter("ExitType", "Traling", new[] { "Traling", "Sma" }, "Exit");
 
         // Create indicator Sma
         _sma = IndicatorsFactory.CreateIndicatorByName("Sma", name + "MovingLong", false);
         _sma = (Aindicator)_tab.CreateCandleIndicator(_sma, "Prime");
-        _sma.ParametersDigit[0].Value = SmaLength.ValueInt;
+        _sma.ParametersDigit[0].Value = _smaLength.ValueInt;
         _sma.Save();
 
         // Create indicator PriceChannel
         _channel = IndicatorsFactory.CreateIndicatorByName("PriceChannel", name + "Chanel", false);
         _channel = (Aindicator)_tab.CreateCandleIndicator(_channel, "Prime");
-        _channel.ParametersDigit[0].Value = ChannelLength.ValueInt;
-        _channel.ParametersDigit[1].Value = ChannelLength.ValueInt;
+        _channel.ParametersDigit[0].Value = _channelLength.ValueInt;
+        _channel.ParametersDigit[1].Value = _channelLength.ValueInt;
         _channel.Save();
 
-        _tab.CandleFinishedEvent += Strateg_CandleFinishedEvent;
+        _tab.CandleFinishedEvent += _tabCandleFinishedEvent;
 
-        _tab.PositionOpeningSuccesEvent += PositionOpeningSuccesEvent;
-
-        DeleteEvent += Strategy_DeleteEvent;
+        _tab.PositionOpeningSuccesEvent += _tabPositionOpeningSuccesEvent;
 
         ParametrsChangeByUser += StrategyLevermor_ParametrsChangeByUser;
 
-        Description = "Buy: " +
-            "1) Close exits PriceChannel up and Close is above the moving average. " +
-            "2) We buy more every N percent in the direction of travel. (Pyramid). " +
-            "Sale: " +
-            "1) Close exits PriceChannel down and Close is below the moving average. " +
-            "2) We buy more every N percent in the direction of travel. " +
-            "Closure: " +
-            "1) Fixed Stop Loss. " +
-            "2) Crossing the moving average";
+        Description = "trading robot for osengine. " +
+            "Trend strategy with pyramiding and optional exit by trailing stop or SMA." +
+            "Buy: 1. If the price is above the moving average (lastPrice >= SMA): " +
+            " - If dont have position: " +
+            "BuyAtStop at (upper channel border + slippage). " +
+            " - If a long position exists and price > lastEntry + (lastEntry * PercentDopBuy/100): " +
+            "BuyAtLimit (pyramiding). " +
+            "Sell: 1. If the price is below the moving average (lastPrice <= SMA): " +
+            " - If dont have position: " +
+            "SellAtStop at (lower channel border - slippage)." +
+            " - If a short position exists and price < lastEntry - (lastEntry * PercentDopSell/100): " +
+            "SellAtLimit (pyramiding). " +
+            "Exit from a long position:" +
+            "1. Trailing stop at (close - close * TralingStopLength / 100), or " +
+            "2. Market exit if Close < SMA (if ExitType = Sma). " +
+            "Exit from the short position: " +
+            "1. Trailing stop at (close + close * TralingStopLength / 100), or " +
+            "2. Market exit if Close > SMA (if ExitType = Sma).";
     }
 
     // Indicator Update event
-    void StrategyLevermor_ParametrsChangeByUser()
+    private void StrategyLevermor_ParametrsChangeByUser()
     {
-        _channel.ParametersDigit[0].Value = ChannelLength.ValueInt;
-        _channel.ParametersDigit[1].Value = ChannelLength.ValueInt;
+        _channel.ParametersDigit[0].Value = _channelLength.ValueInt;
+        _channel.ParametersDigit[1].Value = _channelLength.ValueInt;
+        _channel.Save();
         _channel.Reload();
-        _sma.ParametersDigit[0].Value = SmaLength.ValueInt;
+
+        _sma.ParametersDigit[0].Value = _smaLength.ValueInt;
+        _sma.Save();
         _sma.Reload();
     }
 
@@ -109,22 +162,15 @@ public class StrategyLevermor : BotPanel
     {
         return "StrategyLevermor";
     }
+
     public override void ShowIndividualSettingsDialog()
     {
 
     }
 
-    void Strategy_DeleteEvent()
+    private void _tabPositionOpeningSuccesEvent(Position position)
     {
-        if (File.Exists(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt"))
-        {
-            File.Delete(@"Engine\" + NameStrategyUniq + @"SettingsBot.txt");
-        }
-    }
-
-    private void PositionOpeningSuccesEvent(Position position)
-    {
-        if (Regime.ValueString == "Off")
+        if (_regime.ValueString == "Off")
         {
             return;
         }
@@ -138,18 +184,23 @@ public class StrategyLevermor : BotPanel
     }
 
     // Candle Finished Event
-    private void Strateg_CandleFinishedEvent(List<Candle> candles)
+    private void _tabCandleFinishedEvent(List<Candle> candles)
     {
         // If the robot is turned off, exit the event handler
-        if (Regime.ValueString == "Off")
+        if (_regime.ValueString == "Off")
         {
             return;
         }
 
         // If there are not enough candles to build an indicator, we exit
-        if (_sma.ParametersDigit[0].Value > candles.Count ||
-            _channel.ParametersDigit[0].Value > candles.Count ||
-            _channel.ParametersDigit[1].Value > candles.Count)
+        if (_sma.ParametersDigit[0].Value >= candles.Count ||
+            _channel.ParametersDigit[0].Value + 1 >= candles.Count)
+        {
+            return;
+        }
+
+        if (_startTradeTime.Value > _tab.TimeServerCurrent ||
+            _endTradeTime.Value < _tab.TimeServerCurrent)
         {
             return;
         }
@@ -165,7 +216,7 @@ public class StrategyLevermor : BotPanel
         }
 
         // If the position closing mode, then exit the method
-        if (Regime.ValueString == "OnlyClosePosition")
+        if (_regime.ValueString == "OnlyClosePosition")
         {
             return;
         }
@@ -177,7 +228,7 @@ public class StrategyLevermor : BotPanel
     // Opening logic
     private void LogicOpenPosition(List<Candle> candles)
     {
-        decimal lastMa = _sma.DataSeries[0].Values[_sma.DataSeries[0].Values.Count - 1];
+        decimal lastMa = _sma.DataSeries[0].Last;
 
         decimal lastPrice = candles[candles.Count - 1].Close;
 
@@ -186,65 +237,64 @@ public class StrategyLevermor : BotPanel
             return;
         }
 
-        decimal maxToCandleSeries = _channel.DataSeries[0].Values[_channel.DataSeries[0].Values.Count - 1];
-        decimal minToCandleSeries = _channel.DataSeries[1].Values[_channel.DataSeries[1].Values.Count - 1];
+        //last values of price channel
+        decimal maxToCandleSeries = _channel.DataSeries[0].Last;
+        decimal minToCandleSeries = _channel.DataSeries[1].Last;
 
         List<Position> positions = _tab.PositionsOpenAll;
 
-        if (lastPrice >= lastMa && Regime.ValueString != "OnlyShort")
+        if (lastPrice >= lastMa && _regime.ValueString != "OnlyShort") //Enter Long
         {
             if (positions != null && positions.Count != 0 &&
                 positions[0].Direction == Side.Buy)
             {
-                if (positions.Count >= MaximumPosition.ValueInt)
+                if (positions.Count >= _maximumPosition.ValueInt)
                 {
                     return;
                 }
+
                 decimal lastIntro = positions[positions.Count - 1].EntryPrice;
-                if (lastIntro + lastIntro * (PersentDopSell.ValueDecimal / 100) < lastPrice)
+
+                if (lastIntro + lastIntro * (_percentDopBuy.ValueDecimal / 100) < lastPrice)
                 {
-                    if (positions.Count >= MaximumPosition.ValueInt)
-                    {
-                        return;
-                    }
-                    _tab.BuyAtLimit(GetVolume(_tab), lastPrice + (Slippage.ValueInt * _tab.Securiti.PriceStep));
+                    _tab.BuyAtLimit(GetVolume(_tab), lastPrice + (_slippage.ValueInt * _tab.Securiti.PriceStep));
                 }
             }
             else if (positions == null || positions.Count == 0)
             {
                 _tab.SellAtStopCancel();
                 _tab.BuyAtStopCancel();
-                _tab.BuyAtStop(GetVolume(_tab), maxToCandleSeries + (Slippage.ValueInt * _tab.Securiti.PriceStep), maxToCandleSeries, StopActivateType.HigherOrEqual);
+                _tab.BuyAtStop(GetVolume(_tab), maxToCandleSeries + (_slippage.ValueInt * _tab.Securiti.PriceStep), maxToCandleSeries, StopActivateType.HigherOrEqual);
             }
         }
 
-        if (lastPrice <= lastMa && Regime.ValueString != "OnlyLong")
+        if (lastPrice <= lastMa && _regime.ValueString != "OnlyLong") //Enter short
         {
             if (positions != null && positions.Count != 0 &&
                      positions[0].Direction == Side.Sell)
             {
-                if (positions.Count >= MaximumPosition.ValueInt)
+                if (positions.Count >= _maximumPosition.ValueInt)
                 {
                     return;
                 }
 
                 decimal lastIntro = positions[positions.Count - 1].EntryPrice;
 
-                if (lastIntro - lastIntro * (PersentDopSell.ValueDecimal / 100) > lastPrice)
+                if (lastIntro - lastIntro * (_percentDopSell.ValueDecimal / 100) > lastPrice)
                 {
-                    _tab.SellAtLimit(GetVolume(_tab), lastPrice - (Slippage.ValueInt * _tab.Securiti.PriceStep));
+                    _tab.SellAtLimit(GetVolume(_tab), lastPrice - (_slippage.ValueInt * _tab.Securiti.PriceStep));
                 }
             }
             else if (positions == null || positions.Count == 0)
             {
-                if (positions != null && positions.Count >= MaximumPosition.ValueInt)
+                if (positions != null && positions.Count >= _maximumPosition.ValueInt)
                 {
                     return;
                 }
 
                 _tab.SellAtStopCancel();
                 _tab.BuyAtStopCancel();
-                _tab.SellAtStop(GetVolume(_tab), minToCandleSeries - (Slippage.ValueInt * _tab.Securiti.PriceStep), minToCandleSeries, StopActivateType.LowerOrEqyal);
+                _tab.SellAtStop(GetVolume(_tab), minToCandleSeries - (_slippage.ValueInt * _tab.Securiti.PriceStep), minToCandleSeries, StopActivateType.LowerOrEqual);
             }
         }
     }
@@ -290,19 +340,15 @@ public class StrategyLevermor : BotPanel
             {
                 if (positions[i].Direction == Side.Buy)
                 {
-                    _tab.CloseAtTrailingStop(positions[i],
-                        candles[candles.Count - 1].Close -
-                        candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100,
-                        candles[candles.Count - 1].Close -
-                        candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100);
+                    decimal trailingStopPrice = candles[candles.Count - 1].Close - candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100;
+
+                    _tab.CloseAtTrailingStop(positions[i], trailingStopPrice, trailingStopPrice);
                 }
                 else
                 {
-                    _tab.CloseAtTrailingStop(positions[i],
-                        candles[candles.Count - 1].Close +
-                        candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100,
-                        candles[candles.Count - 1].Close +
-                        candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100);
+                    decimal trailingStopPrice = candles[candles.Count - 1].Close + candles[candles.Count - 1].Close * TralingStopLength.ValueDecimal / 100;
+
+                    _tab.CloseAtTrailingStop(positions[i], trailingStopPrice, trailingStopPrice);
                 }
             }
         }
