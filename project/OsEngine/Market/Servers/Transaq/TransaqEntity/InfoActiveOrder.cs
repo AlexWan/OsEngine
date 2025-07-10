5f168027115455308ccb5a2df9b91fc6c05de76e
Egor <cycahosir@gmail.com>
2025-07-10 23:47:53 +0700

    Transaq fix

---
+++ project/OsEngine/Market/Servers/Transaq/TransaqEntity/InfoActiveOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Market.Servers.Transaq.TransaqEntity
+{
+    public class InfoActiveOrder
+    {
+        public int Transactionid { get; set; }
+        public string NumberMarket { get; set; }
+    }
+}
