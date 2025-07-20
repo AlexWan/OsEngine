97b4940b07ea898e237bf255ff8a43120a886d99
JChinaM <137157160+JChinaM@users.noreply.github.com>
2025-07-20 10:29:12 +0300

    Update BotTabPairUi.xaml.cs

--- project/OsEngine/OsTrader/Panels/Tab/BotTabPairUi.xaml.cs
+++ project/OsEngine/OsTrader/Panels/Tab/BotTabPairUi.xaml.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
  * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
 */
@@ -393,6 +393,11 @@ namespace OsEngine.OsTrader.Panels.Tab
             {
                 if (_journalUi2 != null)
                 {
+                    if (_journalUi2.WindowState == System.Windows.WindowState.Minimized)
+                    {
+                        _journalUi2.WindowState = System.Windows.WindowState.Normal;
+                    }
+
                     _journalUi2.Activate();
                     return;
                 }
