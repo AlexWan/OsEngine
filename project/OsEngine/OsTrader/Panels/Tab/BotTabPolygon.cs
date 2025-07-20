ca53b54039a2c226bb54bb7051ad4b60b31d8e8c
JChinaM <137157160+JChinaM@users.noreply.github.com>
2025-07-20 10:27:14 +0300

    Update BotTabPolygon.cs

--- project/OsEngine/OsTrader/Panels/Tab/BotTabPolygon.cs
+++ project/OsEngine/OsTrader/Panels/Tab/BotTabPolygon.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Your rights to use code governed by this license https://github.com/AlexWan/OsEngine/blob/master/LICENSE
  * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
 */
@@ -1476,6 +1476,10 @@ namespace OsEngine.OsTrader.Panels.Tab
                     {
                         if (_uiList[i].NameElement == pair.Name)
                         {
+                            if (_uiList[i].WindowState == System.Windows.WindowState.Minimized)
+                            {
+                                _uiList[i].WindowState = System.Windows.WindowState.Normal;
+                            }
                             _uiList[i].Activate();
                             return;
                         }
