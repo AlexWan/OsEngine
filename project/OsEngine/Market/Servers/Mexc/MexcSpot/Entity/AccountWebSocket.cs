34944a5c611e03b2cfe62ea9a6d729e96a386078
deus4 <deus4@VLADIMIR1>
2025-07-25 18:46:11 +0300

    MexcSpot. Протобуфер

---
+++ project/OsEngine/Market/Servers/Mexc/MexcSpot/Entity/AccountWebSocket.cs
@@ -0,0 +1,22 @@
+﻿namespace OsEngine.Market.Servers.Mexc.MexcSpot.Entity
+{
+    public class AccountWebSocket
+    {
+        public string channel { get; set; }
+        public MexcSocketBalance privateAccount { get; set; }
+        public string symbol { get; set; }
+        public string sendTime { get; set; }
+    }
+
+    public class MexcSocketBalance
+    {
+        public string vcoinName { get; set; }
+        public string coinId { get; set; }
+        public string balanceAmount { get; set; }
+        public string balanceAmountChange { get; set; }
+        public string frozenAmount { get; set; }
+        public string frozenAmountChange { get; set; }
+        public string type { get; set; }
+        public string time { get; set; }
+    }
+}
---
+++ project/OsEngine/Market/Servers/Mexc/MexcSpot/Entity/DepthsWebSocket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace OsEngine.Market.Servers.Mexc.MexcSpot.Entity
+{
+    public class DepthsWebSocket
+    {
+        public string channel { get; set; }
+        public MexcDepth publicLimitDepths { get; set; }
+        public string symbol { get; set; }
+        public string sendTime { get; set; }
+    }
+
+    public class MexcDepthRow
+    {
+        public string price { get; set; }
+        public string quantity { get; set; }
+    }
+
+    public class MexcDepth
+    {
+        public List<MexcDepthRow> asks { get; set; }
+        public List<MexcDepthRow> bids { get; set; }
+        public string eventType { get; set; }
+        public string version { get; set; }
+    }
+}
