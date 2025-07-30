15c8f03df1109e1dc32fd8150eda7b4edd6fdafc
VovcheG1 <aiwa1953@yandex.ru>
2025-07-30 20:55:57 +0300

    BitMartSpot. RestSharp, правки в бумагах, рефакторинг

--- project/OsEngine/Market/Servers/BitMart/BitMartSpot/Entity/BitMartPortfolioRest.cs
+++ project/OsEngine/Market/Servers/BitMart/BitMartSpot/Entity/BitMartPortfolioRest.cs
@@ -7,9 +7,10 @@ using System.Collections.Generic;
 
 namespace OsEngine.Market.Servers.BitMart.Json
 {
-
-    public class BitMartSpotPortfolioItems : List<BitMartSpotPortfolioItem>
-    { }
+    public class PortfolioData
+    {
+        public List<BitMartSpotPortfolioItem> wallet { get; set; }
+    }
 
     public class BitMartSpotPortfolioItem
     {
@@ -17,19 +18,6 @@ namespace OsEngine.Market.Servers.BitMart.Json
         public string available { get; set; }
         public string name { get; set; }
         public string frozen { get; set; }
-
+        public string total { get; set; }
     }
-
-    public class BitMartPortfolioRest
-    {
-        public string buyingPowerAtMorning { get; set; }
-        public string buyingPower { get; set; }
-        public string profit { get; set; }
-        public string profitRate { get; set; }
-        public string portfolioEvaluation { get; set; }
-        public string portfolioLiquidationValue { get; set; }
-        public string initialMargin { get; set; }
-        public string riskBeforeForcePositionClosing { get; set; }
-    }
-
 }
