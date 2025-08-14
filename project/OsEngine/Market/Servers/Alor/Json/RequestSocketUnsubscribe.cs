bb526a643e6647bc571684c1477ff1fd116c6bf3
Nikita Buida <nikita.buida@gmail.com>
2025-08-14 21:43:33 +0300

    Alor: added unsubscription stub

---
+++ project/OsEngine/Market/Servers/Alor/Json/RequestSocketUnsubscribe.cs
@@ -0,0 +1,10 @@
+
+namespace OsEngine.Market.Servers.Alor.Json
+{
+    public class RequestSocketUnsubscribe
+    {
+        public string opcode = "unsubscribe";
+        public string token;
+        public string guid;
+    }
+}
