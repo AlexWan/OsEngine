b9f212bb4fa1066921724d3d70c3b911d74f99cb
unknown <alexey@o-s-a.net>
2025-08-15 17:49:56 +0100

    Candle starter fix

--- project/OsEngine/Candles/CandleManager.cs
+++ project/OsEngine/Candles/CandleManager.cs
@@ -428,13 +428,12 @@ namespace OsEngine.Entity
                     if (series.CandleMarketDataType == CandleMarketDataType.MarketDepth)
                     {
                         _activeSeriesBasedOnMd.Add(series);
-                        series.IsStarted = true;
                     }
                     else if (series.CandleMarketDataType == CandleMarketDataType.Tick)
                     {
                         _activeSeriesBasedOnTrades.Add(series);
                     }
-                    
+                    series.IsStarted = true;
                 }
             }
             catch (Exception error)
