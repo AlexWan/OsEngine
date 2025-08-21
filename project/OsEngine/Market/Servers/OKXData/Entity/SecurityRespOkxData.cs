90701ea6cfee15a3fdea11f9663213ceea0b912d
Sergey Tsiunel <tsiunels@yandex.ru>
2025-08-21 13:24:21 +0400

    OKX Data connector added

---
+++ project/OsEngine/Market/Servers/OKXData/Entity/SecurityRespOkxData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace OsEngine.Market.Servers.OKXData.Entity
+{
+    public class SecurityRespOkxData
+    {
+        public string code;
+        public List<OkxSecurityData> data;
+    }
+
+    public class OkxSecurityData
+    {
+        public string alias;
+        public string baseCcy;
+        public string category;
+        public string ctMult;
+        public string ctType;
+        public string ctVal;
+        public string ctValCcy;
+        public string expTime;
+        public string instId;
+        public string instType;
+        public string lever;
+        public string listTime;
+        public string lotSz;
+        public string maxIcebergSz;
+        public string maxLmtSz;
+        public string maxMktSz;
+        public string maxStopSz;
+        public string maxTriggerSz;
+        public string maxTwapSz;
+        public string minSz;
+        public string optType;
+        public string quoteCcy;
+        public string settleCcy;
+        public string state;
+        public string stk;
+        public string tickSz;
+        public string uly;
+    }
+}
---
+++ project/OsEngine/Market/Servers/OKXData/Entity/TradeComparer.cs
@@ -0,0 +1,23 @@
+﻿using OsEngine.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace OsEngine.Market.Servers.OKXData.Entity
+{
+    public class TradeComparer : IComparer<Trade>
+    {
+        public int Compare(Trade x, Trade y)
+        {
+            int timeComparison = x.Time.CompareTo(y.Time);
+            if (timeComparison != 0)
+                return timeComparison;
+
+            if (long.TryParse(x.Id, out long xId) && long.TryParse(y.Id, out long yId))
+            {
+                return xId.CompareTo(yId);
+            }
+
+            return string.Compare(x.Id, y.Id, StringComparison.Ordinal);
+        }
+    }
+}
