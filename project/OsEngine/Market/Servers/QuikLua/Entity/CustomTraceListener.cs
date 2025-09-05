a585d7bb7eb61afa948df41d6e7ad286209cfcd3
susanoo-10011 <cycahosir@gmail.com>
2025-09-05 15:56:11 +0300

    QuikLua. Добавил: логирование ошибок из библиотеки QuikSharp, статус Disconect при закрытии терминала квика, рефакторинг

---
+++ project/OsEngine/Market/Servers/QuikLua/Entity/CustomTraceListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OsEngine.Market.Servers.QuikLua.Entity
+{
+    public class CustomTraceListener : TraceListener
+    {
+        public override void Write(string message) { }
+
+        public override void WriteLine(string message) { }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            if (eventType == TraceEventType.Error)
+            {
+                OnTraceMessageReceived?.Invoke(message);
+            }
+        }
+
+        public static event Action<string> OnTraceMessageReceived;
+    }
+}
