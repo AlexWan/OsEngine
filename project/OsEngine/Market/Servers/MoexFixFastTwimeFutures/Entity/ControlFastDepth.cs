e5f6a53f2dd6e7d237e50723fc736b839bba9915
Sergey Tsiunel <tsiunels@yandex.ru>
2025-09-13 16:56:52 +0400

    MoexFixFastTwime connector upd

---
+++ project/OsEngine/Market/Servers/MoexFixFastTwimeFutures/Entity/ControlFastDepth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace OsEngine.Market.Servers.MoexFixFastTwimeFutures.Entity
+{
+    public class ControlFastDepth
+    {
+        public ControlFastDepth()
+        {
+            AsksF = new List<ControlDepthLevel>();
+            BidsF = new List<ControlDepthLevel>();
+        }
+
+        public List<ControlDepthLevel> AsksF;
+
+        public List<ControlDepthLevel> BidsF;
+    }
+
+    public class ControlDepthLevel
+    {
+        public int ImmutabilityCount { get; set; }
+
+        public decimal Ask;
+
+        public decimal Bid;
+
+        public decimal Price;
+    }
+}
