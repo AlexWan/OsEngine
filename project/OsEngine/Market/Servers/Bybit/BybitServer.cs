eb390f2edb188282bb6c09ba6fafa86c7bf3b4a6
Nikita Buida <nikita.buida@gmail.com>
2025-09-30 19:46:01 +0300

    Bybit: fixed underlyings for futures

--- project/OsEngine/Market/Servers/Bybit/BybitServer.cs
+++ project/OsEngine/Market/Servers/Bybit/BybitServer.cs
@@ -694,6 +694,7 @@ namespace OsEngine.Market.Servers.Bybit
                             DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
                             DateTime expiration = origin.AddMilliseconds(oneSec.deliveryTime.ToDouble());
                             security.Expiration = expiration;
+                            security.UnderlyingAsset = oneSec.baseCoin + oneSec.quoteCoin;
                         }
                         else
                         {
