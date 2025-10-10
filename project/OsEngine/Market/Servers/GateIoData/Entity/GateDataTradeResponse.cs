98505bd8a42d23b6678fc8ab1b741a2d596df808
Sergey Tsiunel <tsiunels@yandex.ru>
2025-10-10 12:11:44 +0400

    GateIoData connector added

---
+++ project/OsEngine/Market/Servers/GateIoData/Entity/GateDataTradeResponse.cs
@@ -0,0 +1,16 @@
+﻿
+namespace OsEngine.Market.Servers.GateIoData.Entity
+{
+    public class GateDataTradeResponse
+    {
+        public string id { get; set; }
+        public string create_time { get; set; }
+        public string create_time_ms { get; set; }
+        public string currency_pair { get; set; }
+        public string side { get; set; }
+        public string amount { get; set; }
+        public string contract { get; set; }
+        public string size { get; set; }
+        public string price { get; set; }
+    }
+}
---
+++ project/OsEngine/Market/Servers/GateIoData/Entity/GateFutCandlesResp.cs
@@ -0,0 +1,13 @@
+﻿namespace OsEngine.Market.Servers.GateIoData.Entity
+{
+    public class GateFutCandlesResp
+    {
+        public string t { get; set; }
+        public string v { get; set; }
+        public string c { get; set; }
+        public string h { get; set; }
+        public string l { get; set; }
+        public string o { get; set; }
+        public string sum { get; set; }
+    }
+}
