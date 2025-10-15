5bab53315de73db0c7f598ae2459049c91d1ce49
nikitabuida <nikita.buida@gmail.com>
2025-10-16 00:19:11 +0300

    OKX: set option strike price

--- project/OsEngine/Market/Servers/OKX/OkxServer.cs
+++ project/OsEngine/Market/Servers/OKX/OkxServer.cs
@@ -533,6 +533,7 @@ namespace OsEngine.Market.Servers.OKX
                     //security.Lot = item.ctVal.ToDecimal();
                     security.Expiration = TimeManager.GetDateTimeFromTimeStamp(long.Parse(item.expTime));
                     security.OptionType = item.optType == "P" ? OptionType.Put : OptionType.Call;
+                    security.Strike = item.stk.ToDecimal();
 
                     string baseName = item.uly + "T"; // example: BTC-USD -> BTC-USDT
 
