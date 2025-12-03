12b96870b16c12745184e3a8cc028f5c027cb8f4
VovcheG1 <aiwa1953@yandex.ru>
2025-12-03 17:00:42 +0300

    Cmo. Update indicator

--- project/OsEngine/bin/Debug/Custom/Indicators/Scripts/CMO.cs
+++ project/OsEngine/bin/Debug/Custom/Indicators/Scripts/CMO.cs
@@ -6,127 +6,98 @@ using OsEngine.Entity;
 namespace OsEngine.Indicators
 {
     [Indicator("CMO")]
-    public class CMO:Aindicator
+    public class CMO : Aindicator
     {
         private IndicatorDataSeries _series;
-
         private IndicatorParameterInt _period;
+
+        private List<decimal> _cmo1;
+        private List<decimal> _cmo2;
 
         public override void OnStateChange(IndicatorState state)
         {
             if (state == IndicatorState.Configure)
             {
                 _period = CreateParameterInt("Period", 14);
-                _series = CreateSeries("Cmo", Color.Orange, IndicatorChartPaintType.Line, true);
+                _series = CreateSeries("CMO", Color.Orange, IndicatorChartPaintType.Line, true);
+            }
+            else if (state == IndicatorState.Dispose)
+            {
+                _cmo1?.Clear();
+                _cmo2?.Clear();
             }
         }
 
         public override void OnProcess(List<Candle> candles, int index)
         {
-            _series.Values[index] = GetValue(candles, index);
+            if (index == 0)
+            {
+                _cmo1 = new List<decimal>();
+                _cmo2 = new List<decimal>();
+            }
+
+            while (_cmo1.Count <= index)
+            {
+                _cmo1.Add(0);
+            }
+
+            while (_cmo2.Count <= index)
+            {
+                _cmo2.Add(0);
+            }
+
+            decimal cmoValue = CalculateCMO(candles, index);
+            _series.Values[index] = Math.Round(cmoValue, 2);
         }
 
-        private decimal GetValue(List<Candle> candles, int index)
+        private decimal CalculateCMO(List<Candle> candles, int index)
         {
-            decimal value = 0;
-
-            decimal sum1 = 0;
-
-            decimal sum2 = 0;
-            while (index >= _cmo1.Count)
+            if (index == 0)
             {
-                _cmo1.Add(GetCmo1(candles, index));
-
-            }
-            while (index >= _cmo2.Count)
-            {
-                _cmo2.Add(GetCmo1(candles, index));
-            }
-
-            _cmo1[index] = GetCmo1(candles, index);
-            _cmo2[index] = GetCmo2(candles, index);
-
-            if (index <= _period.ValueInt)
-            {
+                _cmo1[0] = 0;
+                _cmo2[0] = 0;
                 return 0;
             }
 
-            for (int i = index - _period.ValueInt +1; i < index +1; i++)
-            {
-                sum1 = sum1 + _cmo1[i];
-                sum2 = sum2 + _cmo2[i];
-            }
+            decimal priceChange = candles[index].Close - candles[index - 1].Close;
 
-            if (sum1 + sum2 == 0)
+            if (priceChange > 0)
             {
-                return 0;
+                _cmo1[index] = priceChange;
+                _cmo2[index] = 0;
+            }
+            else if (priceChange < 0)
+            {
+                _cmo1[index] = 0;
+                _cmo2[index] = Math.Abs(priceChange);
             }
             else
             {
-                value = (sum1 - sum2) / (sum1 + sum2) * 100;
+                _cmo1[index] = 0;
+                _cmo2[index] = 0;
             }
 
-            return Math.Round(value,2);
+            if (index < _period.ValueInt)
+            {
+                return 0;
+            }
+
+            decimal sumPositive = 0;
+            decimal sumNegative = 0;
+
+            for (int i = index - _period.ValueInt + 1; i > 0 && i <= index; i++)
+            {
+                sumPositive += _cmo1[i];
+                sumNegative += _cmo2[i];
+            }
+
+            if (sumPositive + sumNegative == 0)
+            {
+                return 0;
+            }
+
+            decimal cmo = 100 * (sumPositive - sumNegative) / (sumPositive + sumNegative);
+            return cmo;
         }
-
-        private decimal GetCmo1(List<Candle> candles, int index)
-        {
-            decimal diff = 0;
-            decimal cmo1 = 0;
-
-            if (index > 1)
-            {
-                diff = candles[index].Close - candles[index - 1].Close;
-            }
-
-            if (diff > 0)
-            {
-                cmo1 = diff;
-            }
-            if (diff < 0)
-            {
-                cmo1 = 0;
-            }
-            if (diff == 0)
-            {
-                cmo1 = 0;
-            }
-
-            return cmo1;
-
-        }
-
-        private decimal GetCmo2(List<Candle> candles, int index)
-        {
-            decimal diff = 0;
-            decimal cmo2 = 0;
-
-
-            if (index > 1)
-            {
-                diff = candles[index].Close - candles[index - 1].Close;
-            }
-
-            if (diff > 0)
-            {
-                cmo2 = 0;
-            }
-            if (diff < 0)
-            {
-                cmo2 = -diff;
-            }
-            if (diff == 0)
-            {
-                cmo2 = 0;
-            }
-
-            return cmo2;
-
-        }
-
-        private List<decimal> _cmo1 = new List<decimal>();
-
-        private List<decimal> _cmo2 = new List<decimal>();
-
     }
 }
