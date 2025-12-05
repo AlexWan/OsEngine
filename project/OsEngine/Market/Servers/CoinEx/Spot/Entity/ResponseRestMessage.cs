f8a9fd6cdc64564f9804bccf3436e73415da6ee0
VovcheG1 <aiwa1953@yandex.ru>
2025-12-05 21:03:53 +0300

    CoinExSpot. Fix security

---
+++ project/OsEngine/Market/Servers/CoinEx/Spot/Entity/ResponseRestMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace OsEngine.Market.Servers.CoinEx.Spot.Entity
+{
+    public class ResponseRestMessage<T>
+    {
+        public string code { get; set; }
+        public T data { get; set; }
+        public string message { get; set; }
+    }
+
+    public class SpotMarketInfo
+    {
+        public string market { get; set; }
+        public string taker_fee_rate { get; set; }
+        public string maker_fee_rate { get; set; }
+        public string min_amount { get; set; }
+        public string base_ccy { get; set; }
+        public string quote_ccy { get; set; }
+        public string base_ccy_precision { get; set; }
+        public string quote_ccy_precision { get; set; }
+        public string status { get; set; }
+        public string delisted_at { get; set; }
+        public string is_amm_available { get; set; }
+        public string is_margin_available { get; set; }
+        public string is_pre_trading_available { get; set; }
+        public string is_api_trading_available { get; set; }
+    }
+}
