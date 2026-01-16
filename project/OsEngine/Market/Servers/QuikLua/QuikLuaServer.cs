dfb820c925b95f75b48fe6c8643e2561af61a46c
SuSaNoo <125782534+susanoo-10011@users.noreply.github.com>
2026-01-16 17:18:36 +0300

    QuikLuaServer upd

--- project/OsEngine/Market/Servers/QuikLua/QuikLuaServer.cs
+++ project/OsEngine/Market/Servers/QuikLua/QuikLuaServer.cs
@@ -1007,6 +1007,7 @@ namespace OsEngine.Market.Servers.QuikLua
 
                                 needPortf.SetNewPosition(position);
                             }
+                            else continue;
 
                             if (PortfolioEvent != null)
                             {
