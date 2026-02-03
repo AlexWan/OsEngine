0f895c5c5a5dce602654adfbd79d58d9cd123d4f
VovcheG1 <aiwa1953@yandex.ru>
2026-02-03 16:29:34 +0300

    Инструкции для главного меню

---
+++ project/OsEngine/Instructions/MainMenuInstructions.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Your rights to use code governed by this license http://o-s-a.net/doc/license_simple_engine.pdf
+ * Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using OsEngine.Language;
+using System.Collections.Generic;
+using static OsEngine.Language.OsLocalization;
+
+namespace OsEngine.Instructions
+{
+    public class MainMenuInstructions
+    {
+        public string AllInstructionsInClassDescription
+        {
+            get
+            {
+                OsLocalType currentLanguage = OsLocalization.CurLocalization;
+
+                if (currentLanguage == OsLocalType.Ru)
+                {
+                    return "Главное меню OsEngine. Сборник статей";
+                }
+                else if (currentLanguage == OsLocalType.Eng)
+                {
+                    return "OsEngine Main Menu. Post Collection";
+                }
+
+                return "";
+            }
+        }
+
+        public List<Instruction> AllInstructionsInClass
+        {
+            get
+            {
+                List<Instruction> result = new List<Instruction>();
+
+                result.Add(Link1);
+                result.Add(Link2);
+
+                return result;
+            }
+        }
+
+        public Instruction Link1 = new Instruction()
+        {
+            Type = InstructionType.Post,
+
+            Ru = new InstructionLocalized()
+            {
+                Description = "Главное меню OsEngine",
+                PostLink = "https://o-s-a.net/posts/glavnoe-menyu-osengine.html"
+            }
+        };
+
+        public Instruction Link2 = new Instruction()
+        {
+            Type = InstructionType.Post,
+
+            Ru = new InstructionLocalized()
+            {
+                Description = "Главное меню OsEngine. Общие настройки",
+                PostLink = "https://o-s-a.net/posts/osengine-main-menu-general-settings.html"
+            }
+        };
+    }
+}
