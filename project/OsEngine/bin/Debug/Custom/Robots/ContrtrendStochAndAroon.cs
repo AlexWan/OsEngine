e6dac05147ac6026377bd8a31b87b050bc1e36dd
susanoo-10011 <cycahosir@gmail.com>
2026-05-04 17:11:09 +0300

    ContrtrendStochAndAroon upd

--- project/OsEngine/bin/Debug/Custom/Robots/ContrtrendStochAndAroon.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/ContrtrendStochAndAroon.cs
@@ -38,8 +38,7 @@ namespace OsEngine.Robots
         // Basic Settings
         private StrategyParameterString _regime;
         private StrategyParameterDecimal _slippage;
-        private StrategyParameterTimeOfDay _startTradeTime;
-        private StrategyParameterTimeOfDay _endTradeTime;
+        private StrategyParameterString _orderType;
 
         // GetVolume Settings
         private StrategyParameterString _volumeType;
@@ -56,27 +55,54 @@ namespace OsEngine.Robots
         private Aindicator _aroon;
         private Aindicator _stoh;
 
+        // Non trade periods
+        private NonTradePeriods _tradePeriodsSettings;
+        private StrategyParameterButton _tradePeriodsShowDialogButton;
+
         public ContrtrendStochAndAroon(string name, StartProgram startProgram) : base(name, startProgram)
         {
+            // non trade periods
+            _tradePeriodsSettings = new NonTradePeriods(name);
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1Start = new TimeOfDay() { Hour = 0, Minute = 0 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1End = new TimeOfDay() { Hour = 10, Minute = 05 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1OnOff = true;
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2Start = new TimeOfDay() { Hour = 13, Minute = 54 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2End = new TimeOfDay() { Hour = 14, Minute = 6 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2OnOff = false;
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3Start = new TimeOfDay() { Hour = 18, Minute = 1 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3End = new TimeOfDay() { Hour = 23, Minute = 58 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3OnOff = true;
+
+            _tradePeriodsSettings.TradeInSunday = false;
+            _tradePeriodsSettings.TradeInSaturday = false;
+
+            _tradePeriodsSettings.Load();
+
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
 
             // Basic settings
             _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
             _slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
-            _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
-            _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+            _orderType = CreateParameter("Order type", "Market", new[] { "Market", "Limit" }, "Base");
 
             // GetVolume Settings
-            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
-            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
-            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" }, "Base");
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4, "Base");
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime", "Base");
             
             // Indicator settings 
             _aroonLength = CreateParameter("Aroon Length", 14, 1, 200, 1, "Indicator");
             _stochPeriod1 = CreateParameter("Stoch Period 1", 9, 3, 40, 1, "Indicator");
             _stochPeriod2 = CreateParameter("Stoch Period 2", 5, 2, 40, 1, "Indicator");
             _stochPeriod3 = CreateParameter("Stoch Period 3", 3, 2, 40, 1, "Indicator");
+
+            // non trade period button
+            _tradePeriodsShowDialogButton = CreateParameterButton("Non trade periods", "Base");
+            _tradePeriodsShowDialogButton.UserClickOnButtonEvent += _tradePeriodsShowDialogButton_UserClickOnButtonEvent;
 
             // Create indicator Aroon
             _aroon = IndicatorsFactory.CreateIndicatorByName("Aroon", name + "Aroon", false);
@@ -99,7 +125,13 @@ namespace OsEngine.Robots
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
 
             Description = OsLocalization.Description.DescriptionLabel181;
-        }   
+        }
+
+        // non trade period button click
+        private void _tradePeriodsShowDialogButton_UserClickOnButtonEvent()
+        {
+            _tradePeriodsSettings.ShowDialog();
+        }
 
         private void ContrtrendStochAndAroon_ParametrsChangeByUser()
         {
@@ -143,8 +175,7 @@ namespace OsEngine.Robots
             }
 
             // If the time does not match, we leave
-            if (_startTradeTime.Value > _tab.TimeServerCurrent ||
-                _endTradeTime.Value < _tab.TimeServerCurrent)
+            if (_tradePeriodsSettings.CanTradeThisTime(candles[^1].TimeStart) == false)
             {
                 return;
             }
@@ -195,7 +226,14 @@ namespace OsEngine.Robots
                 {
                     if (aroonUp > 70 && stoch > 30 && prevStoh < 30)
                     {
-                        _tab.BuyAtLimit(GetVolume(_tab), lastPrice + _slippage);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.BuyAtLimit(GetVolume(_tab), lastPrice + _slippage);
+                        }
+                        else
+                        {
+                            _tab.BuyAtMarket(GetVolume(_tab));
+                        }
                     }
                 }
 
@@ -204,7 +242,14 @@ namespace OsEngine.Robots
                 {
                     if (aroonDown > 70 && stoch < 80 && prevStoh > 80)
                     {
-                        _tab.SellAtLimit(GetVolume(_tab), lastPrice - _slippage);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.SellAtLimit(GetVolume(_tab), lastPrice - _slippage);
+                        }
+                        else
+                        {
+                            _tab.SellAtMarket(GetVolume(_tab));
+                        }
                     }
                 }
             }
@@ -234,14 +279,28 @@ namespace OsEngine.Robots
                 {
                     if (aroonUp < 60)
                     {
-                        _tab.CloseAtLimit(pos, lastPrice - _slippage, pos.OpenVolume);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.CloseAtLimit(pos, lastPrice - _slippage, pos.OpenVolume);
+                        }
+                        else
+                        {
+                            _tab.CloseAtMarket(pos, pos.OpenVolume);
+                        }
                     }
                 }
                 else // If the direction of the position is short
                 {
                     if (aroonDown < 60)
                     {
-                        _tab.CloseAtLimit(pos, lastPrice + _slippage, pos.OpenVolume);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.CloseAtLimit(pos, lastPrice + _slippage, pos.OpenVolume);
+                        }
+                        else
+                        {
+                            _tab.CloseAtMarket(pos, pos.OpenVolume);
+                        }
                     }
                 }
             }
