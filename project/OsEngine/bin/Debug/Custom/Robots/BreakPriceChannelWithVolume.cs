f713173084bf366316f944469bc81e7c17964b6c
susanoo-10011 <cycahosir@gmail.com>
2026-05-04 13:46:07 +0300

    BreakPriceChannelWithVolume upd

--- project/OsEngine/bin/Debug/Custom/Robots/BreakPriceChannelWithVolume.cs
+++ project/OsEngine/bin/Debug/Custom/Robots/BreakPriceChannelWithVolume.cs
@@ -46,8 +46,7 @@ namespace OsEngine.Robots.AO
         // Basic Settings
         private StrategyParameterString _regime;
         private StrategyParameterDecimal _slippage;
-        private StrategyParameterTimeOfDay _startTradeTime;
-        private StrategyParameterTimeOfDay _endTradeTime;
+        private StrategyParameterString _orderType;
 
         // GetVolume Settings
         private StrategyParameterString _volumeType;
@@ -75,27 +74,54 @@ namespace OsEngine.Robots.AO
         private StrategyParameterDecimal _coefProfit;
         private StrategyParameterInt _stopCandles;
 
+        // Non trade periods
+        private NonTradePeriods _tradePeriodsSettings;
+        private StrategyParameterButton _tradePeriodsShowDialogButton;
+
         public BreakPriceChannelWithVolume(string name, StartProgram startProgram) : base(name, startProgram)
         {
+            // non trade periods
+            _tradePeriodsSettings = new NonTradePeriods(name);
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1Start = new TimeOfDay() { Hour = 0, Minute = 0 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1End = new TimeOfDay() { Hour = 10, Minute = 05 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod1OnOff = true;
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2Start = new TimeOfDay() { Hour = 13, Minute = 54 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2End = new TimeOfDay() { Hour = 14, Minute = 6 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod2OnOff = false;
+
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3Start = new TimeOfDay() { Hour = 18, Minute = 1 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3End = new TimeOfDay() { Hour = 23, Minute = 58 };
+            _tradePeriodsSettings.NonTradePeriodGeneral.NonTradePeriod3OnOff = true;
+
+            _tradePeriodsSettings.TradeInSunday = false;
+            _tradePeriodsSettings.TradeInSaturday = false;
+
+            _tradePeriodsSettings.Load();
+
             TabCreate(BotTabType.Simple);
             _tab = TabsSimple[0];
 
             // Basic Settings
             _regime = CreateParameter("Regime", "Off", new[] { "Off", "On", "OnlyLong", "OnlyShort", "OnlyClosePosition" }, "Base");
             _slippage = CreateParameter("Slippage %", 0m, 0, 20, 1, "Base");
-            _startTradeTime = CreateParameterTimeOfDay("Start Trade Time", 0, 0, 0, 0, "Base");
-            _endTradeTime = CreateParameterTimeOfDay("End Trade Time", 24, 0, 0, 0, "Base");
+            _orderType = CreateParameter("Order type", "Market", new[] { "Market", "Limit" }, "Base");
 
             // GetVolume Settings
-            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" });
-            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4);
-            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime");
+            _volumeType = CreateParameter("Volume type", "Deposit percent", new[] { "Contracts", "Contract currency", "Deposit percent" }, "Base");
+            _volume = CreateParameter("Volume", 20, 1.0m, 50, 4, "Base");
+            _tradeAssetInPortfolio = CreateParameter("Asset in portfolio", "Prime", "Base");
 
             // Indicator Settings
             _candlesCountVolume = CreateParameter("CandlesCountVolume", 13, 10, 300, 1, "Indicator");
             _multVolume = CreateParameter("MultVolume", 10.0m, 10, 300, 10, "Indicator");
             _pcUpLength = CreateParameter("Up Line Length", 21, 7, 48, 7, "Indicator");
             _pcDownLength = CreateParameter("Down Line Length", 21, 7, 48, 7, "Indicator");
+
+            // non trade period button
+            _tradePeriodsShowDialogButton = CreateParameterButton("Non trade periods", "Base");
+            _tradePeriodsShowDialogButton.UserClickOnButtonEvent += _tradePeriodsShowDialogButton_UserClickOnButtonEvent;
 
             // Create indicator PC
             _PC = IndicatorsFactory.CreateIndicatorByName("PriceChannel", name + "PC", false);
@@ -120,6 +146,12 @@ namespace OsEngine.Robots.AO
             _tab.CandleFinishedEvent += _tab_CandleFinishedEvent;
 
             Description = OsLocalization.Description.DescriptionLabel166;
+        }
+
+        // non trade period button click
+        private void _tradePeriodsShowDialogButton_UserClickOnButtonEvent()
+        {
+            _tradePeriodsSettings.ShowDialog();
         }
 
         private void BreakPriceChannelWithVolume_ParametrsChangeByUser()
@@ -158,8 +190,7 @@ namespace OsEngine.Robots.AO
             }
 
             // If the time does not match, we leave
-            if (_startTradeTime.Value > _tab.TimeServerCurrent ||
-                _endTradeTime.Value < _tab.TimeServerCurrent)
+            if (_tradePeriodsSettings.CanTradeThisTime(candles[^1].TimeStart) == false)
             {
                 return;
             }
@@ -211,7 +242,14 @@ namespace OsEngine.Robots.AO
                 {
                     if (GetAverage(VolumeValues, _candlesCountVolume.ValueInt) * _multVolume.ValueDecimal < _lastVolume && lastPrice > _prevUpPC)
                     {
-                        _tab.BuyAtLimit(GetVolume(_tab), _tab.PriceBestAsk + _slippage);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.BuyAtLimit(GetVolume(_tab), _tab.PriceBestAsk + _slippage);
+                        }
+                        else
+                        {
+                            _tab.BuyAtMarket(GetVolume(_tab));
+                        }
                     }
                 }
 
@@ -220,7 +258,14 @@ namespace OsEngine.Robots.AO
                 {
                     if ((GetAverage(VolumeValues, _candlesCountVolume.ValueInt) * _multVolume.ValueDecimal) < _lastVolume && lastPrice < _prevDownPC)
                     {
-                        _tab.SellAtLimit(GetVolume(_tab), _tab.PriceBestBid - _slippage);
+                        if (_orderType == "Limit")
+                        {
+                            _tab.SellAtLimit(GetVolume(_tab), _tab.PriceBestBid - _slippage);
+                        }
+                        else
+                        {
+                            _tab.SellAtMarket(GetVolume(_tab));
+                        }
                     }
                 }
             }
@@ -230,7 +275,7 @@ namespace OsEngine.Robots.AO
         private void LogicClosePosition(List<Candle> candles)
         {
             List<Position> openPositions = _tab.PositionsOpenAll;
-            
+
             decimal _slippage = this._slippage.ValueDecimal * _tab.Securiti.PriceStep;
 
             decimal profitActivation;
@@ -257,8 +302,16 @@ namespace OsEngine.Robots.AO
                     price = stopActivation;
                     profitActivation = pos.EntryPrice + (pos.EntryPrice - price) * _coefProfit.ValueDecimal;
 
-                    _tab.CloseAtProfit(pos, profitActivation, profitActivation + _slippage);
-                    _tab.CloseAtStop(pos, stopActivation, stopActivation - _slippage);
+                    if (_orderType == "Limit")
+                    {
+                        _tab.CloseAtProfit(pos, profitActivation, profitActivation + _slippage);
+                        _tab.CloseAtStop(pos, stopActivation, stopActivation - _slippage);
+                    }
+                    else
+                    {
+                        _tab.CloseAtProfitMarket(pos, profitActivation);
+                        _tab.CloseAtStopMarket(pos, stopActivation);
+                    }
                 }
                 else // If the direction of the position is sale
                 {
@@ -272,8 +325,16 @@ namespace OsEngine.Robots.AO
                     price = stopActivation;
                     profitActivation = pos.EntryPrice - (price - pos.EntryPrice) * _coefProfit.ValueDecimal;
 
-                    _tab.CloseAtProfit(pos, profitActivation, profitActivation - _slippage);
-                    _tab.CloseAtStop(pos, stopActivation, stopActivation + _slippage);
+                    if (_orderType == "Limit")
+                    {
+                        _tab.CloseAtProfit(pos, profitActivation, profitActivation - _slippage);
+                        _tab.CloseAtStop(pos, stopActivation, stopActivation + _slippage);
+                    }
+                    else
+                    {
+                        _tab.CloseAtProfitMarket(pos, profitActivation);
+                        _tab.CloseAtStopMarket(pos, stopActivation);
+                    }
                 }
             }
         }
